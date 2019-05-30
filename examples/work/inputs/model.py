8460c6fe5de351e05a813a9ebdaf092b30e33a8e
Benoit Raybaud <braybaud@intven.com>
2019-05-30 14:35:48 -0700

    Add missing files.

---
+++ examples/work/inputs/model.py
@@ -0,0 +1,11 @@
+from MyExternalLibrary.functions import add
+
+if __name__ == "__main__":
+    import json
+
+    with open("config.json", 'r') as fp:
+        config = json.load(fp)
+        parameters = config["parameters"]
+
+    print(add(parameters["a"], parameters["b"]))
+    print(config["parameters"])
