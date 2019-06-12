ef2fbbf0a02a8e7436f1e8dd39813e8ff5911967
Clinton Collins <clinton.collins@gmail.com>
2019-06-12 06:58:20 -0700

    idmtools-84 Explore different backend for object storage

--- idmtools_core/idmtools/services/IPersistanceService.py
+++ idmtools_core/idmtools/services/IPersistanceService.py
@@ -1,27 +1,28 @@
 import os
-import shelve
+import diskcache
 from abc import ABCMeta
 
 
 class IPersistenceService(metaclass=ABCMeta):
-    shelve_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+    cache_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
     shelf_name = None
 
     @classmethod
-    def _open_shelf(cls):
-        os.makedirs(cls.shelve_directory, exist_ok=True)
-        return shelve.open(os.path.join(cls.shelve_directory, cls.shelf_name))
+    def _open_cache(cls):
+        cache_directory=os.path.join(cls.cache_directory, 'disk_cache', cls.shelf_name)
+        os.makedirs(cache_directory, exist_ok=True)
+        return diskcache.Cache(os.path.join(cls.cache_directory, 'disk_cache', cls.shelf_name))
 
     @classmethod
     def retrieve(cls, uid):
-        shelf = cls._open_shelf()
+        shelf = cls._open_cache()
         obj = shelf[str(uid)]
         shelf.close()
         return obj
 
     @classmethod
     def save(cls, obj):
-        shelf = cls._open_shelf()
+        shelf = cls._open_cache()
         shelf[str(obj.uid)] = obj
         shelf.close()
         return obj.uid
