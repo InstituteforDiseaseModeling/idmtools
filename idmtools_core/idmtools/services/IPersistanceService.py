e9a5812d8c7afc5d9840d306ef258bc5afcea566
Clinton Collins <clinton.collins@gmail.com>
2019-06-12 07:09:19 -0700

    #84 Explore different backend for object storage

--- idmtools_core/idmtools/services/IPersistanceService.py
+++ idmtools_core/idmtools/services/IPersistanceService.py
@@ -5,13 +5,13 @@ from abc import ABCMeta
 
 class IPersistenceService(metaclass=ABCMeta):
     cache_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
-    shelf_name = None
+    cache_name = None
 
     @classmethod
     def _open_cache(cls):
-        cache_directory=os.path.join(cls.cache_directory, 'disk_cache', cls.shelf_name)
+        cache_directory=os.path.join(cls.cache_directory, 'disk_cache', cls.cache_name)
         os.makedirs(cache_directory, exist_ok=True)
-        return diskcache.Cache(os.path.join(cls.cache_directory, 'disk_cache', cls.shelf_name))
+        return diskcache.Cache(os.path.join(cls.cache_directory, 'disk_cache', cls.cache_name))
 
     @classmethod
     def retrieve(cls, uid):
