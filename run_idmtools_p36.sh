af8de613def1498518a93d87275a15f19deb25ce
Sharon Chen <shchen@idmod.org>
2019-06-21 14:50:49 -0700

    add run test script

---
+++ run_idmtools_p36.sh
@@ -0,0 +1,20 @@
+RTUALENV_DIR="$(mktemp -d)"
+echo "env" $VIRTUALENV_DIR
+trap 'rm -r "${VIRTUALENV_DIR}"' EXIT
+virtualenv -p /usr/bin/python3.6 "${VIRTUALENV_DIR}"
+source "${VIRTUALENV_DIR}/bin/activate"
+cd dev_scripts
+./setup_virtualenv.sh
+cd ..
+cd idmtools_local_runner && \
+   docker-compose up -d
+pip install dataclasses
+LOCAL_PATH="$(realpath $(dirname '$0')/)"
+echo "auto login..."
+cd ${LOCAL_PATH}/idmtools_core/tests &&\
+   python create_auth_token_args.py --comps_url=$1 --username=$2 --password=$3
+echo "run all tests..."
+cd ${LOCAL_PATH}/idmtools_core/tests
+   python run_tests.py
+echo "deactivate..."
+deactivate
