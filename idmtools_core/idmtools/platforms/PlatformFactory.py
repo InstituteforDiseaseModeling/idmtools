d178701f7578e33c9ff6c962a5eb81d636139716
zdu <zdu@idmod.org>
2019-06-25 09:36:30 -0700

    #50: code cleanup for PlatformFactory

--- idmtools_core/idmtools/platforms/PlatformFactory.py
+++ idmtools_core/idmtools/platforms/PlatformFactory.py
@@ -8,11 +8,10 @@ class PlatformFactory:
         if isinstance(platform_type, str):
             platform_type = PlatformType[platform_type]
 
-        platform = platform_type.value(**kwargs)  # if defined by enum.Enum
-        # platform = platform_type(**kwargs)          # if defined by type
+        # get platform class
+        platform_cls = platform_type.value
+
+        # create platform
+        platform = platform_cls(**kwargs)
 
         return platform
-
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
