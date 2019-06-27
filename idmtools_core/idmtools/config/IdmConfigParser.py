f39c610b1f8df8ffc41f8ee7a4af8c7cd08b988b
zdu <zdu@idmod.org>
2019-06-27 13:51:06 -0700

    #50: add description and fix strong type issue

--- idmtools_core/idmtools/config/IdmConfigParser.py
+++ idmtools_core/idmtools/config/IdmConfigParser.py
@@ -1,23 +1,36 @@
 import copy
 import os
 from configparser import ConfigParser
+from typing import Dict
 
 default_config = 'idmtools.ini'
 
 
 class IdmConfigParser:
+    """
+    Parse INI file
+    """
     _config = None
     _instance = None
     _config_path = None
 
-    def __new__(cls, dir_path='.', file_name=default_config):
+    def __new__(cls, dir_path: str = '.', file_name: str = default_config) -> 'IdmConfigParser':
+        """
+
+        Args:
+            dir_path: idmtools INI configuration file directory
+            file_name: idmtools INI file name
+
+        Returns: IdmConfigParser instance
+
+        """
         if not cls._instance:
             cls._instance = super(IdmConfigParser, cls).__new__(cls)
             cls._instance._load_config_file(dir_path, file_name)
         return cls._instance
 
     @classmethod
-    def retrieve_settings(cls, section=None, field_type={}):
+    def retrieve_settings(cls, section: str = None, field_type: Dict[str, str] = {}) -> Dict[str, str]:
         import ast
 
         cls.ensure_init()
@@ -39,7 +52,7 @@ class IdmConfigParser:
         return field_config_updated
 
     @classmethod
-    def _find_config(cls, dir_path, file_name=default_config):
+    def _find_config(cls, dir_path: str = None, file_name: str = default_config) -> None:
         full_dir_path = os.path.abspath(dir_path)
         if os.path.exists(os.path.join(full_dir_path, file_name)):
             cls._config_path = os.path.join(full_dir_path, file_name)
@@ -53,7 +66,7 @@ class IdmConfigParser:
                 return cls._config_path
 
     @classmethod
-    def _load_config_file(cls, dir_path='.', file_name=default_config):
+    def _load_config_file(cls, dir_path: str = '.', file_name: str = default_config) -> None:
         ini_file = cls._find_config(dir_path, file_name)
         if ini_file is None:
             print("/!\\ WARNING: File '{}' Not Found!".format(file_name))
@@ -65,7 +78,7 @@ class IdmConfigParser:
         cls._config.read(ini_file)
 
     @classmethod
-    def _get_section(cls, section=None):
+    def _get_section(cls, section: str = None) -> Dict[str, str]:
         cls.ensure_init()
         if cls._config is None:
             return {}
@@ -79,17 +92,17 @@ class IdmConfigParser:
         return dict(section)
 
     @classmethod
-    def get_config_path(cls):
+    def get_config_path(cls) -> str:
         cls.ensure_init()
         return cls._config_path
 
     @classmethod
-    def display_config_path(cls):
+    def display_config_path(cls) -> None:
         cls.ensure_init()
         print(cls.get_config_path())
 
     @classmethod
-    def view_config_file(cls):
+    def view_config_file(cls) -> None:
         cls.ensure_init()
         print("View Config INI: \n{}".format(cls._config_path))
         print('-' * len(cls._config_path), '\n')
@@ -98,11 +111,11 @@ class IdmConfigParser:
             print(read_data)
 
     @classmethod
-    def get_option(cls, section, option):
+    def get_option(cls, section: str = None, option: str = None):
         cls.ensure_init()
         return cls._config.get(section.upper(), option)
 
     @classmethod
-    def ensure_init(cls, dir_path='.', file_name=default_config):
+    def ensure_init(cls, dir_path: str = '.', file_name: str = default_config):
         if cls._instance is None:
             cls(dir_path, file_name)
