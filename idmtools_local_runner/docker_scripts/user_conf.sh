388411d3aeb421e15eb020a338ba40f92cfa99b1
Clinton Collins <devclinton@users.noreply.github.com>
2019-07-02 08:34:47 -0700

    #68, #69, and #102 (#179)
    * #69 - Local database implementation
    
    * #69 - Refactor local runner
    
    * #69 - Final structure of local runner. Cleaning up the cli now and status reporting
    
    * Additional updates
    
    * Add docker scripts
    
    * Move to name to avoid gitignore
    
    * #69 - add tags to local db
    
    * #69 - Extra documentation on the dockerfile and improvement to the size
    Add UI service start script
    
    * #69 - Add postgres
    Switch docker-compose version to support local constraints(memory limits)
    Add a image name for the workers. In future, we will should have the user download the build image instead of building themselves
    Add some helpful dev tools
    
    * #69 - Update versions to all be compatible release formatted
    Remove diskcache and backoff
    Add sqlalchemy, pandas, psycopg2, tabulate, and flask
    
    * #69 - Updated readme for local_runner
    
    * #69 - Restructure and improve the CLI
    
    * #69 - Update db to postgres due to limits of quering in diskcache on most likely the need for an API-Like interface in the future
    
    * #69 - Update the service scripts
    
    * #69 - Define the UI for the local runner
    
    * #69 - Add comment about platform switching just to make it clear to users in the example(if it isn't already clear)
    
    * #69 - Update readme and script to use index-url vs extra as most users will be installing
    within IDM and this gives us the benefit of on-site caching
    
    * #102 - Refactor local_runner so that a minimal install doesn't require postgres or flask
    Add simple API for experiments/simulations
    CLI now uses the API for statusing
    
    * Update documentation to reflect the CLI no longer needs container
    
    * Documentation updates
    
    * #69 - Move all worker related items under workers module for more development clarity
    Define an API client
    Add support for canceling simulations
    
    * Add error handling to Clients
    Update CLI to use Clients
    
    * Fix worker scripts paths
    Add permanent postgres storage
    Change metadata to extra_details(to avoid conflict with SqlAlchemy)
    Add flask_restful
    
    * Ensure EOL's are always LF for shell files

---
+++ idmtools_local_runner/docker_scripts/user_conf.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/with-contenv bash
+
+## Set defaults for environmental variables in case they are undefined
+USER=${USER:=idmtools}
+PASSWORD=${PASSWORD:=idmtools}
+USERID=${USERID:=1000}
+GROUPID=${GROUPID:=1000}
+ROOT=${ROOT:=FALSE}
+UMASK=${UMASK:=022}
+
+echo "$USER"
+if [ "$USERID" -ne 1000 ]
+## Configure user with a different USERID if requested.
+  then
+    echo "deleting user idmtools"
+    userdel idmtools
+    echo "creating new $USER with UID $USERID"
+    useradd -m $USER -u $USERID
+    mkdir /home/$USER
+    chown -R $USER /home/$USER /data /app
+    usermod -a -G staff $USER
+elif [ "$USER" != "idmtools" ]
+  then
+    echo "Renaming idmtools to $USER"
+    ## cannot move home folder when it's a shared volume, have to copy and change permissions instead
+    cp -r /home/idmtools /home/$USER
+    ## RENAME the user
+    usermod -l $USER -d /home/$USER idmtools
+    groupmod -n $USER idmtools
+    usermod -a -G staff $USER
+    chown -R $USER:$USER /home/$USER /data /app
+    echo "USER is now $USER"
+else
+    echo "Ensuring proper permissions on data directories"
+    chown -R $USER:$USER /home/$USER /data /app
+fi
+
+if [ "$GROUPID" -ne 1000 ]
+## Configure the primary GID (whether rstudio or $USER) with a different GROUPID if requested.
+  then
+    echo "Modifying primary group $(id $USER -g -n)"
+    groupmod -g $GROUPID $(id $USER -g -n)
+    echo "Primary group ID is now custom_group $GROUPID"
+fi
+
+## Add a password to user
+echo "$USER:$PASSWORD" | chpasswd
+
+# Use Env flag to know if user should be added to sudoers
+if [[ ${ROOT,,} == "true" ]]
+  then
+    adduser $USER sudo && echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+    echo "$USER added to sudoers"
+fi
