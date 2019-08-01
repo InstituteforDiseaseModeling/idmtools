876221d7eed8d42c287c58e8a67705a766b7e96c
Clinton Collins <clinton.collins@gmail.com>
2019-08-01 00:08:42 -0700

    Fix the setup_virtualenv.bat

--- dev_scripts/setup_virtualenv.bat
+++ dev_scripts/setup_virtualenv.bat
@@ -6,4 +6,4 @@ set sources[3]="idmtools_platform_comps"
 set sources[4]="idmtools_models_collection"
 set sources[5]="idmtools_test"
 
-for /F "tokens=2 delims==" %%s in ('set sources[') do cd %%s && pip install -e .[test] --index-url=https://packages.idmod.org/api/pypi/pypi-production/simple
+for /F "tokens=2 delims==" %%s in ('set sources[') do cd ../%%s && pip install -e .[test] --index-url=https://packages.idmod.org/api/pypi/pypi-production/simple
