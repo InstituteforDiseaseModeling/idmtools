22ba844b8230ea9c7a51a36d83d6863fb3d6fdf2
Clinton Collins <clinton.collins@gmail.com>
2019-08-01 22:00:56 -0700

    Bump version: 0.1.0-nightly.0 → 0.1.0-nightly.1

--- idmtools_cli/setup.py
+++ idmtools_cli/setup.py
@@ -11,7 +11,7 @@ with open('README.md') as readme_file:
 with open('requirements.txt') as requirements_file:
     requirements = requirements_file.read().split("\n")
 
-build_requirements = ['flake8', 'coverage']
+build_requirements = ['flake8', 'coverage', 'py-make', 'bump2version']
 setup_requirements = []
 test_requirements = ['pytest', 'pytest-runner', 'numpy==1.16.4', 'xmlrunner'] + build_requirements
 
@@ -50,6 +50,6 @@ setup(
     entry_points={"console_scripts": ["idmtools=idmtools_cli.main:main"]},
     extras_require=extras,
     url='https://github.com/InstituteforDiseaseModeling/idmtools',
-    version='0.1.0',
+    version='0.1.0-nightly',
     zip_safe=True
 )
