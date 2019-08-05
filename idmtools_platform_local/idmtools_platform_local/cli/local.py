f6a6f70e002099decd1b138ffda2b89d7e703413
Clinton Collins <clinton.collins@gmail.com>
2019-08-05 15:40:59 -0700

    Pretty status

--- idmtools_platform_local/idmtools_platform_local/cli/local.py
+++ idmtools_platform_local/idmtools_platform_local/cli/local.py
@@ -1,5 +1,5 @@
 import click
-
+from colorama import Fore
 from idmtools_cli.cli import cli
 from idmtools_platform_local.docker.DockerOperations import DockerOperations
 
@@ -12,28 +12,46 @@ def local():
 @local.command()
 @click.option("--delete-data/--no-delete-data", default=False)
 def down(delete_data):
+    """Shutdown the local execution platform(and optionally delete data"""
     do = DockerOperations()
     click.confirm(f'Do you want to remove all data associated with the local platform?({do.host_data_directory})', abort=True)
     do.cleanup(delete_data)
 
 
 @local.command()
+def start():
+    """Start the local execution platform"""
+    do = DockerOperations()
+    do.restart_all()
+
+
+@local.command()
 def restart():
+    """Restart the local execution platform"""
     do = DockerOperations()
     do.restart_all()
 
 
 @local.command()
 def status():
+    """
+    Check the status of the local execution platform
+    """
     do = DockerOperations()
     redis = do.get_redis()
     if redis:
-        print(f'Redis running in container: {redis.id}')
+        click.echo(f'Redis:      [{Fore.GREEN}{redis.status}{Fore.RESET}] [{redis.id}] ')
+    else:
+        click.echo(f'Redis:      [{Fore.RED}down{Fore.RESET}]')
 
     postgres = do.get_postgres()
     if postgres:
-        print(f'Postgres running in container: {postgres.id}')
+        click.echo(f'Postgres    [{Fore.GREEN}{postgres.status}{Fore.RESET}] [{redis.id}] ')
+    else:
+        click.echo(f'Postgres:   [{Fore.RED}down{Fore.RESET}]')
 
     workers = do.get_workers()
     if workers:
-        print(f'Worker running in container: {workers.id}')
+        click.echo(f'Workers:    [{Fore.GREEN}{workers.status}{Fore.RESET}] [{redis.id}] ')
+    else:
+        click.echo(f'Workers:    [{Fore.RED}down{Fore.RESET}]')
