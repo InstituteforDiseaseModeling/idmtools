b813fddaa568adf523082a979b466672e0b8e139
Clinton Collins <clinton.collins@gmail.com>
2019-08-16 07:19:27 -0700

    Remove test-with-loogin Update build scripts to use bootstrap and pymake test

---
+++ idmtools_models/idmtools_models/python/DockerizedPythonExperiment.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass, field
+import os
+from idmtools.entities import CommandLine
+from idmtools_models.python import PythonExperiment, PythonSimulation
+
+
+@dataclass(repr=False)
+class DockerizedPythonExperiment(PythonExperiment):
+    docker_image: str = field(default=None)
+    #extra_volume_mounts: str = field(default=None)
+
+    def __post_init__(self, simulation_type):
+        super().__post_init__(simulation_type=PythonSimulation)
+        if self.docker_image is None:
+            raise ValueError("Docker image is required when running a dockerized python simulation")
+
+    def pre_creation(self):
+        super().pre_creation()
+
+        # Create the command line according to the location of the model
+        # the data path will be updated by the platform
+        self.command = CommandLine("docker", "run", "-v", "{data_path}:/workdir", f"{self.docker_image}", "python",
+                                   f"./Assets/{os.path.basename(self.model_path)}", "config.json")
