6c7323c2e6fedd918bc6ca74162807b2421c9d09
zdu <zdu@idmod.org>
2019-09-10 09:38:57 -0700

    #297: implement inputs validation

--- idmtools_core/idmtools/entities/IPlatform.py
+++ idmtools_core/idmtools/entities/IPlatform.py
@@ -1,8 +1,10 @@
+import ast
 import uuid
 import typing
 from abc import ABCMeta, abstractmethod
 from logging import getLogger
 from idmtools.core.interfaces.IEntity import IEntity
+from dataclasses import fields
 
 if typing.TYPE_CHECKING:
     from idmtools.core.types import TExperiment, TSimulation, TSimulationBatch
@@ -57,7 +59,7 @@ class IPlatform(IEntity, metaclass=ABCMeta):
         Work to be done after object creation
         Returns: None
         """
-        pass
+        self.validate_inputs_types()
 
     @abstractmethod
     def create_experiment(self, experiment: 'TExperiment') -> None:
@@ -136,3 +138,27 @@ class IPlatform(IEntity, metaclass=ABCMeta):
 
     def __repr__(self):
         return f"<Platform {self.__class__.__name__} - id: {self.uid}>"
+
+    def validate_inputs_types(self) -> None:
+        """
+        Validate user inputs and case attr with the correct data types
+        Returns: None
+        """
+        # retrieve field values, default values and types
+        fds = fields(self)
+        field_value = {f.name: getattr(self, f.name) for f in fds}
+        field_type = {f.name: f.type for f in fds}
+
+        # Make sure the user values have the requested type
+        fs_kwargs = set(field_type.keys()).intersection(set(field_value.keys()))
+        for fn in fs_kwargs:
+            ft = field_type[fn]
+            if ft in (int, float, str):
+                field_value[fn] = ft(field_value[fn]) if field_value[fn] is not None else field_value[fn]
+            elif ft is bool:
+                field_value[fn] = ast.literal_eval(field_value[fn]) if isinstance(field_value[fn], str) else \
+                field_value[fn]
+
+        # Update attr with validated data types
+        for fn in fs_kwargs:
+            setattr(self, fn, field_value[fn])
