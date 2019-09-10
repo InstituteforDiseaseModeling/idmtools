370171daf97464aa5e54008f5baeb2de8a488aaf
zdu <zdu@idmod.org>
2019-09-10 09:23:23 -0700

    #297: add import Platform

--- idmtools_core/idmtools/core/__init__.py
+++ idmtools_core/idmtools/core/__init__.py
@@ -3,7 +3,6 @@ from idmtools.core.CacheEnabled import CacheEnabled
 from idmtools.core.interfaces.EntityContainer import EntityContainer
 from idmtools.core.enums import *
 from idmtools.core.exceptions import *
-from idmtools.core.ExperimentFactory import experiment_factory
-from idmtools.core.PlatformFactory import Platform
 from idmtools.core.SystemInformation import SystemInformation
+from idmtools.core.PlatformFactory import Platform
 from idmtools.core.types import *
