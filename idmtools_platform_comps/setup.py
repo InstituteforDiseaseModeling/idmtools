5eea1621fdbd0310d1be548b2e798381e2ceb23b
Clinton Collins <clinton.collins@gmail.com>
2019-09-11 12:28:22 -0700

    Begin consolidate  version into one config Bump to 0.2.1

--- idmtools_platform_comps/setup.py
+++ idmtools_platform_comps/setup.py
@@ -53,6 +53,6 @@ setup(
                       ),
     extras_require=extras,
     url='https://github.com/InstituteforDiseaseModeling/idmtools',
-    version='0.2.0+nightly',
+    version='0.2.1+nightly',
     zip_safe=False,
 )
