70f1fd4bb941060efbc5d29409762871a55adf04
INTERNAL\mfisher <mfisher@idmod.org>
2019-09-11 12:59:50 -0700

    Analyzer changes and custom dtk exp test

--- idmtools_platform_comps/tests/inputs/analyzers/PopulationAnalyzer.py
+++ idmtools_platform_comps/tests/inputs/analyzers/PopulationAnalyzer.py
@@ -1,24 +1,59 @@
 import os
+import pandas as pd
 
 from idmtools.analysis.AnalyzeManager import AnalyzeManager
 from idmtools.entities import IAnalyzer
 from idmtools.core.PlatformFactory import PlatformFactory
+from idmtools_test.utils.group import default_select_fn, default_group_fn
 
 
 class PopulationAnalyzer(IAnalyzer):
-    def __init__(self, filenames=None):
-        super().__init__()
-        self.filenames = filenames or []
+
+    data_group_names = ['group', 'sim_id', 'channel']
+    ordered_levels = ['channel', 'group', 'sim_id']
+    output_file = 'population.csv'
+
+    def __init__(self, filename=os.path.join('output', 'InsetChart.json'), channels=(['Statistical Population']), save_output=True, working_dir="."):
+        super(PopulationAnalyzer, self).__init__(filenames=[filename], working_dir=working_dir)
+        self.channels = set(channels)
+        self.save_output = save_output
+
+    def get_channel_data(self, data_by_channel, selected_channels, header):
+        channel_series = [default_select_fn(data_by_channel[channel]["Data"]) for channel in selected_channels]
+        return pd.concat(channel_series, axis=1, keys=selected_channels)
 
     def map(self, data, simulation):
-        # Apply is called for every simulations included into the experiment
-        # We are simply storing the population data in the pop_data dictionary
-        return data[self.filenames[0]]["Channels"]["Statistical Population"]["Data"]
+        cdata = data[self.filenames[0]]['Channels']
+        selected_channels = self.channels.intersection(cdata.keys()) if self.channels else cdata.keys()
+        return self.get_channel_data(cdata, selected_channels, data[self.filenames[0]]["Header"])
 
     def reduce(self, all_data):
         import matplotlib
         matplotlib.use("Agg")
         import matplotlib.pyplot as plt
+
+        selected = []
+        for sim, data in all_data.items():
+            # TODO: AttributeError: 'UUID' object has no attribute 'id'
+            # Enrich the data with info
+            data.group = default_group_fn(sim.id, sim.tags)
+            data.sim_id = sim.id
+            selected.append(data)
+
+        if len(selected) == 0:
+            print("No data have been returned... Exiting...")
+            return
+
+        # Combining selected data...
+        combined = pd.concat(selected, axis=1,
+                             keys=[(d.group, d.sim_id) for d in selected],
+                             names=self.data_group_names)
+
+        # Re-ordering multi-index levels...
+        data = combined.reorder_levels(self.ordered_levels, axis=1).sort_index(axis=1)
+
+        if self.save_output:
+            data.to_csv(self.output_file)
 
         fig = plt.figure()
         for pop in list(all_data.values()):
@@ -34,12 +69,11 @@ if __name__ == "__main__":
 
     platform = PlatformFactory.create(key='COMPS')
 
-    exp_id = 'a5421d87-5ece-e911-a2bb-f0921c167866' #'86099829-6ecb-e911-a2bb-f0921c167866'
+    exp_id = 'eba5b47b-f7d3-e911-a2bb-f0921c167866' #comps2 staging
 
-    filenames = ['output/InsetChart.json']
-    analyzers = [PopulationAnalyzer(filenames=filenames)]
+    analyzers = [PopulationAnalyzer()]
 
     experiment = platform.get_item(id=exp_id)
 
-    manager = AnalyzeManager(configuration={}, platform=platform, items=experiment.children(), analyzers=analyzers)
+    manager = AnalyzeManager(configuration={}, platform=platform, ids=[exp_id], analyzers=analyzers)
     manager.analyze()
--- idmtools_test/idmtools_test/utils/utils.py
+++ idmtools_test/idmtools_test/utils/utils.py
@@ -16,3 +16,15 @@ def del_file(filename, dir=None):
     if os.path.exists(filepath):
         print(filepath)
         os.remove(filepath)
+
+
+def load_csv_file(filename, dir=None):
+    df = None
+    if dir:
+        filepath = os.path.join(dir, filename)
+    else:
+        filepath = os.path.join(os.path.curdir, filename)
+
+    if os.path.exists(filepath):
+        df = pd.read_csv(filepath)
+    return df
