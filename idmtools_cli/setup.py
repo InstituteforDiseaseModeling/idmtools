87cda509b1363cdbbe7a49227fb65069e744e04a
Clinton Collins <clinton.collins@gmail.com>
2019-09-12 13:57:38 -0700

    Bump version: 0.2.0 → 0.2.1+nightly.0

--- idmtools_cli/setup.py
+++ idmtools_cli/setup.py
@@ -50,6 +50,6 @@ setup(
     entry_points={"console_scripts": ["idmtools=idmtools_cli.main:main"]},
     extras_require=extras,
     url='https://github.com/InstituteforDiseaseModeling/idmtools',
-    version='0.2.0',
+    version='0.2.1+nightly',
     zip_safe=False
 )
