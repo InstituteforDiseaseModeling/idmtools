0f5a594dfd26de3c4b3b674c473ed6159908867c
Clinton Collins <clinton.collins@gmail.com>
2019-09-12 13:39:02 -0700

    Bump version: 0.2.0+nightly.0 → 0.2.0

--- idmtools_core/setup.py
+++ idmtools_core/setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     extras_require=extras,
     url='https://github.com/InstituteforDiseaseModeling/idmtools',
-    version='0.2.0+nightly',
+    version='0.2.0',
     zip_safe=False
 )
