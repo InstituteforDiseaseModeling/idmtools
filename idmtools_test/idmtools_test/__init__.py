89a1068c69050e9061222bfe2e15b3c3d86c2050
Clinton Collins <clinton.collins@gmail.com>
2019-09-12 13:02:22 -0700

    Revert to old version to make everything use one file

--- idmtools_test/idmtools_test/__init__.py
+++ idmtools_test/idmtools_test/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '0.2.1+nightly.0'
+__version__ = '0.2.0+nightly.0'
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 COMMON_INPUT_PATH = os.path.join(current_directory, "inputs")
