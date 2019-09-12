f21086a939ef4c2a9efae0f0158a755f44eeec44
Clinton Collins <clinton.collins@gmail.com>
2019-09-12 13:42:38 -0700

    Bump version: 0.2.0+nightly.0 → 0.2.0

--- idmtools_model_dtk/idmtools_model_dtk/__init__.py
+++ idmtools_model_dtk/idmtools_model_dtk/__init__.py
@@ -3,4 +3,4 @@ from idmtools_model_dtk.DTKExperiment import DTKExperiment
 from idmtools_model_dtk.DTKSimulation import DTKSimulation
 from idmtools_model_dtk.defaults.IDTKDefault import IDTKDefault
 
-__version__ = "0.2.0+nightly.0"
+__version__ = "0.2.0.0"
