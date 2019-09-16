4ca79cd24dcf94dcb4014c855065c89b9c1b2933
shchen-idmod <38014764+shchen-idmod@users.noreply.github.com>
2019-09-16 09:56:12 -0700

    Fix example_abalysusis.py

--- examples/example_analysis.py
+++ examples/example_analysis.py
@@ -1,21 +1,22 @@
-from idmtools.entities import IAnalyzer
+# from idmtools.entities.IAnalyzer import IAnalyzer
 from idmtools.analysis.AnalyzeManager import AnalyzeManager
 from idmtools.core.PlatformFactory import PlatformFactory
+from idmtools.entities import IAnalyzer
 
 
 class ExampleAnalyzer(IAnalyzer):
     def __init__(self, uid=None, working_dir=None, parse=True):
-        super().__init__(uid, working_dir, parse, filenames=["output.json"])
+        super().__init__(uid, working_dir, parse, filenames=["output/result.json"])
 
     def map(self, data, simulation):
-        result = data["result"]
+        result = data[self.filenames[0]]
         return result
 
     def reduce(self, all_data):
         for simulation, result in all_data.items():
             print(simulation)
             print(result)
-            print("---")
+            print("ExampleAnalyzer---")
 
 
 class ExampleAnalyzer2(IAnalyzer):
@@ -23,30 +24,27 @@ class ExampleAnalyzer2(IAnalyzer):
         super().__init__(uid, working_dir, parse, filenames=["config.json"])
 
     def filter(self, simulation: 'TSimulation') -> bool:
-        return int(simulation.tags.get("a")) > 5
+        return int(simulation.tags.get("b")) > 5
 
     def map(self, data, simulation):
-        result = data["result"]
+        result = data[self.filenames[0]]
         return result
 
     def reduce(self, all_data):
         for simulation, result in all_data.items():
             print(simulation)
             print(result)
-            print("---")
+            print("ExampleAnalyzer2---")
 
 
 if __name__ == "__main__":
-
     platform = PlatformFactory.create(key='COMPS')
 
     analyzers = [ExampleAnalyzer(), ExampleAnalyzer2()]
     # analyzers = [ExampleAnalyzer2()]
 
-    experiment_id = "91ec1e91-9fca-e911-a2bb-f0921c167866"  # "185eb7fa-8f97-e911-a2bb-f0921c167866"  # comps2 staging
+    experiment_id = "91ec1e91-9fca-e911-a2bb-f0921c167866"  ## comps2 staging
 
-    experiment = platform.get_item(id=experiment_id)
-
-    manager = AnalyzeManager(configuration={}, platform=platform, items=experiment.children(), analyzers=analyzers)
+    manager = AnalyzeManager(configuration={}, platform=platform, ids=[experiment_id], analyzers=analyzers)
 
     manager.analyze()
