6fb8bf29c632d15f8db4d792e0a96415dcc619fd
Benoit Raybaud <braybaud@intven.com>
2019-10-10 23:16:43 -0700

    restore model

--- examples/work/inputs/python_model_with_deps/Assets/model.py
+++ examples/work/inputs/python_model_with_deps/Assets/model.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 if __name__ == "__main__":
     current_directory = os.path.abspath(os.getcwd())
@@ -12,3 +13,8 @@ if __name__ == "__main__":
         json.dump({"a":run_number}, fp)
 
     print(f"Simulation running with Run_Number = {run_number}")
+
+    # The local platform needs to know the resulting status of a work item. We provide it through a return code	    print(f"Simulation running with Run_Number = {run_number}")
+    # 0 means succeeded. Any other value is an error. It is optional if you want to provide specific return codes
+    # for specific errors
+    sys.exit(0)
