d21db6df40da7dd827efd1f4b6feddecde63ea32
Clinton Collins <clinton.collins@gmail.com>
2019-11-04 02:52:11 -0800

    Windows fixes

--- idmtools_platform_local/setup.py
+++ idmtools_platform_local/setup.py
@@ -19,8 +19,8 @@ extras = dict(test=test_requirements, dev=['Pympler'],
               # Requirements for running workers server
               workers=['sqlalchemy~=1.3.5', 'psycopg2-binary~=2.8.3'],
               # these are only needed when not running UI
-              ui=['flask~=1.0.3', 'Flask-AutoIndex~=0.6.4', 'flask_restful~=0.3.7', 'Flask-SQLAlchemy~=2.4.0', 'uwsgi'])
-
+              ui=['flask~=1.0.3', 'Flask-AutoIndex~=0.6.4', 'flask_restful~=0.3.7', 'Flask-SQLAlchemy~=2.4.0'],
+              servers=['uwsgi==2.0.18'])
 # check for python 3.6
 if sys.version_info[1] == 6:
     requirements.append('dataclasses')
