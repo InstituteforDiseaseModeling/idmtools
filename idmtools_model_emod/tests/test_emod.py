6369a363f47b3724b90455fd09bf8d9799ff2593
Clinton Collins <clinton.collins@gmail.com>
2019-11-06 09:53:56 -0800

    Make tests runnable per platform test tag

--- idmtools_model_emod/tests/test_emod.py
+++ idmtools_model_emod/tests/test_emod.py
@@ -1,7 +1,7 @@
 import json
 import os
 import pytest
-from typing import Dict
+from abc import ABC, abstractmethod
 
 from idmtools.builders import ExperimentBuilder, StandAloneSimulationsBuilder
 from idmtools.core.platform_factory import Platform
@@ -22,136 +22,122 @@ DEFAULT_DEMOGRAPHICS_JSON = os.path.join(COMMON_INPUT_PATH, "files", "demographi
 DEFAULT_ERADICATION_PATH = os.path.join(COMMON_INPUT_PATH, "emod", "Eradication.exe")
 
 emod_version = '2.20.0'
-EMOD_TYPES = dict(COMPS=EMODExperiment,
-                  Local=DockerEMODExperiment)
-ERADICATION_BINARIES = dict(COMPS=os.path.join(COMMON_INPUT_PATH, "emod", "Eradication.exe"),
-                            Local=get_github_eradication_url(emod_version))
 
 
 @pytest.mark.comps
 @pytest.mark.docker
 @pytest.mark.emod
-class TestEMOD(ITestWithPersistence):
+class EMODPlatformTest(ABC):
 
     @classmethod
-    @restart_local_platform(silent=True, stop_before=True, stop_after=False, **get_test_local_env_overrides())
-    def setUpClass(cls):
-        cls.platforms: Dict[str, IPlatform] = dict(
-            COMPS=Platform('COMPS'),
-            Local=Platform('Local')
-        )
-
-    @classmethod
-    @restart_local_platform(silent=True, stop_before=False, stop_after=True, **get_test_local_env_overrides())
-    def tearDownClass(cls) -> None:
+    @abstractmethod
+    def get_emod_experiment(cls,) -> IEMODExperiment:
         pass
 
     @classmethod
-    def get_emod_experiment(cls, platform: str) -> IEMODExperiment:
-        return EMOD_TYPES[platform]
+    @abstractmethod
+    def get_emod_binary(cls, ) -> str:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_platform(cls) -> IPlatform:
+        pass
 
     def setUp(self) -> None:
         self.case_name = os.path.basename(__file__) + "--" + self._testMethodName
         print(self.case_name)
 
     def test_sir_with_StandAloneSimulationsBuilder(self):
-        for platform in self.platforms:
-            with self.subTest(f"test_sir_with_StandAloneSimulationsBuilder_{platform}"):
-                e = self.get_emod_experiment(platform).from_default(self.case_name, default=EMODSir,
-                                                                    eradication_path=ERADICATION_BINARIES[platform])
+            e = self.get_emod_experiment().from_default(self.case_name, default=EMODSir,
+                                                                eradication_path=self.get_emod_binary())
 
-                e.tags = {"idmtools": "idmtools-automation", "string_tag": "test", "number_tag": 123}
-                sim = e.simulation()
-                sim.set_parameter("Enable_Immunity", 0)
-                b = StandAloneSimulationsBuilder()
-                b.add_simulation(sim)
-                e.builder = b
+            e.tags = {"idmtools": "idmtools-automation", "string_tag": "test", "number_tag": 123}
+            sim = e.simulation()
+            sim.set_parameter("Enable_Immunity", 0)
+            b = StandAloneSimulationsBuilder()
+            b.add_simulation(sim)
+            e.builder = b
 
-                em = ExperimentManager(experiment=e, platform=self.platforms[platform])
-                em.run()
-                em.wait_till_done()
-                self.assertTrue(e.succeeded)
-                # get the files in a platform agnostic way
-                for sim in e.simulations:
-                    files = self.platforms[platform].get_files(sim, ["config.json"])
-                    config_parameters = json.loads(files["config.json"])['parameters']
-                    self.assertEqual(config_parameters["Enable_Immunity"], 0)
+            em = ExperimentManager(experiment=e, platform=self.platform)
+            em.run()
+            em.wait_till_done()
+            self.assertTrue(e.succeeded)
+            # get the files in a platform agnostic way
+            for sim in e.simulations:
+                files = self.platform.get_files(sim, ["config.json"])
+                config_parameters = json.loads(files["config.json"])['parameters']
+                self.assertEqual(config_parameters["Enable_Immunity"], 0)
 
     def test_sir_with_ExperimentBuilder(self):
-        for platform in self.platforms:
-            with self.subTest(f"test_sir_with_ExperimentBuilder_{platform}"):
-                e = self.get_emod_experiment(platform).from_default(self.case_name, default=EMODSir,
-                                                                    eradication_path=ERADICATION_BINARIES[platform])
-                e.tags = {"idmtools": "idmtools-automation", "string_tag": "test", "number_tag": 123}
+        e = self.get_emod_experiment().from_default(self.case_name, default=EMODSir,
+                                                            eradication_path=self.get_emod_binary())
+        e.tags = {"idmtools": "idmtools-automation", "string_tag": "test", "number_tag": 123}
 
-                e.base_simulation.set_parameter("Enable_Immunity", 0)
+        e.base_simulation.set_parameter("Enable_Immunity", 0)
 
-                def param_a_update(simulation, value):
-                    simulation.set_parameter("Run_Number", value)
-                    return {"Run_Number": value}
+        def param_a_update(simulation, value):
+            simulation.set_parameter("Run_Number", value)
+            return {"Run_Number": value}
 
-                builder = ExperimentBuilder()
-                # Sweep parameter "Run_Number"
-                builder.add_sweep_definition(param_a_update, range(0, 2))
-                e.builder = builder
-                em = ExperimentManager(experiment=e, platform=self.platforms[platform])
-                em.run()
-                em.wait_till_done()
-                self.assertTrue(e.succeeded)
-                run_number = 0
-                for sim in e.simulations:
-                    files = self.platforms[platform].get_files(sim, ["config.json"])
-                    config_parameters = json.loads(files["config.json"])['parameters']
-                    self.assertEqual(config_parameters["Enable_Immunity"], 0)
-                    self.assertEqual(config_parameters["Run_Number"], run_number)
-                    run_number = run_number + 1
+        builder = ExperimentBuilder()
+        # Sweep parameter "Run_Number"
+        builder.add_sweep_definition(param_a_update, range(0, 2))
+        e.builder = builder
+        em = ExperimentManager(experiment=e, platform=self.platform)
+        em.run()
+        em.wait_till_done()
+        self.assertTrue(e.succeeded)
+        run_number = 0
+        for sim in e.simulations:
+            files = self.platform.get_files(sim, ["config.json"])
+            config_parameters = json.loads(files["config.json"])['parameters']
+            self.assertEqual(config_parameters["Enable_Immunity"], 0)
+            self.assertEqual(config_parameters["Run_Number"], run_number)
+            run_number = run_number + 1
 
     def test_batch_simulations_StandAloneSimulationsBuilder(self):
-        for platform in self.platforms:
-            with self.subTest(f"test_batch_simulations_StandAloneSimulationsBuilder_{platform}"):
-                e = self.get_emod_experiment(platform).from_default(self.case_name, default=EMODSir,
-                                                                    eradication_path=ERADICATION_BINARIES[platform])
-                e.tags = {"idmtools": "idmtools-automation", "string_tag": "test", "number_tag": 123}
-                b = StandAloneSimulationsBuilder()
+            e = self.get_emod_experiment().from_default(self.case_name, default=EMODSir,
+                                                                eradication_path=self.get_emod_binary())
+            e.tags = {"idmtools": "idmtools-automation", "string_tag": "test", "number_tag": 123}
+            b = StandAloneSimulationsBuilder()
 
-                for i in range(20):
-                    sim = e.simulation()
-                    sim.set_parameter("Enable_Immunity", 0)
-                    b.add_simulation(sim)
+            for i in range(20):
+                sim = e.simulation()
+                sim.set_parameter("Enable_Immunity", 0)
+                b.add_simulation(sim)
 
-                e.builder = b
+            e.builder = b
 
-                em = ExperimentManager(experiment=e, platform=self.platforms[platform])
-                em.run()
-                em.wait_till_done()
-                self.assertTrue(e.succeeded)
-                for sim in e.simulations:
-                    files = self.platforms[platform].get_files(sim, ["config.json"])
-                    config_parameters = json.loads(files["config.json"])['parameters']
-                    self.assertEqual(config_parameters["Enable_Immunity"], 0)
+            em = ExperimentManager(experiment=e, platform=self.platform)
+            em.run()
+            em.wait_till_done()
+            self.assertTrue(e.succeeded)
+            for sim in e.simulations:
+                files = self.platform.get_files(sim, ["config.json"])
+                config_parameters = json.loads(files["config.json"])['parameters']
+                self.assertEqual(config_parameters["Enable_Immunity"], 0)
 
     def test_batch_simulations_ExperimentBuilder(self):
-        for platform in self.platforms:
-            with self.subTest(f"test_batch_simulations_ExperimentBuilder_{platform}"):
-                e = self.get_emod_experiment(platform).from_default(self.case_name, default=EMODSir,
-                                                                    eradication_path=ERADICATION_BINARIES[platform])
-                e.tags = {"idmtools": "idmtools-automation", "string_tag": "test", "number_tag": 123}
-                # s = Suite(name="test suite")
-                # s.experiments.append(e)
-                e.base_simulation.set_parameter("Enable_Immunity", 0)
+            e = self.get_emod_experiment().from_default(self.case_name, default=EMODSir,
+                                                                eradication_path=self.get_emod_binary())
+            e.tags = {"idmtools": "idmtools-automation", "string_tag": "test", "number_tag": 123}
+            # s = Suite(name="test suite")
+            # s.experiments.append(e)
+            e.base_simulation.set_parameter("Enable_Immunity", 0)
 
-                def param_a_update(simulation, value):
-                    simulation.set_parameter("Run_Number", value)
-                    return {"Run_Number": value}
+            def param_a_update(simulation, value):
+                simulation.set_parameter("Run_Number", value)
+                return {"Run_Number": value}
 
-                builder = ExperimentBuilder()
-                # Sweep parameter "Run_Number"
-                builder.add_sweep_definition(param_a_update, range(0, 20))
-                e.builder = builder
-                em = ExperimentManager(experiment=e, platform=self.platforms[platform])
-                em.run()
-                em.wait_till_done()
-                self.assertTrue(e.succeeded)
+            builder = ExperimentBuilder()
+            # Sweep parameter "Run_Number"
+            builder.add_sweep_definition(param_a_update, range(0, 20))
+            e.builder = builder
+            em = ExperimentManager(experiment=e, platform=self.platform)
+            em.run()
+            em.wait_till_done()
+            self.assertTrue(e.succeeded)
 
     def test_duplicated_eradication(self):
         """
@@ -175,3 +161,65 @@ class TestEMOD(ITestWithPersistence):
         self.assertEqual("", exe_eradication.relative_path)
         self.assertEqual("Eradication", exe_eradication.filename)
         self.assertEqual(os.path.join(duplicated_model_path, "exe", "Eradication"), exe_eradication.absolute_path)
+
+
+@pytest.mark.comps
+@pytest.mark.emod
+class TestCompsEMOOD(ITestWithPersistence, EMODPlatformTest):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.platform: IPlatform = cls.get_platform()
+
+    def setUp(self) -> None:
+        self.case_name = os.path.basename(__file__) + "--" + self._testMethodName
+        print(self.case_name)
+
+    @classmethod
+    def get_emod_experiment(cls) -> IEMODExperiment:
+        return EMODExperiment
+
+    @classmethod
+    def get_platform(cls) -> IPlatform:
+        return Platform('COMPS')
+
+    @classmethod
+    def get_emod_binary(cls, ) -> str:
+        return os.path.join(COMMON_INPUT_PATH, "emod", "Eradication.exe")
+
+
+@pytest.mark.docker
+@pytest.mark.emod
+class TestLocalPlatformEMOD(ITestWithPersistence, EMODPlatformTest):
+
+    def setUp(self) -> None:
+        self.case_name = os.path.basename(__file__) + "--" + self._testMethodName
+        print(self.case_name)
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        from idmtools_platform_local.internals.infrastructure.service_manager import DockerServiceManager
+        from idmtools_platform_local.internals.docker_io import DockerIO
+        import docker
+        cls.do = DockerIO()
+        cls.sdm = DockerServiceManager(docker.from_env())
+        cls.sdm.cleanup(True, True)
+        cls.do.cleanup(True)
+        cls.platform: IPlatform = cls.get_platform()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.sdm.cleanup(True, True)
+        cls.do.cleanup(True)
+
+    @classmethod
+    def get_emod_experiment(cls,) -> IEMODExperiment:
+        return DockerEMODExperiment
+
+    @classmethod
+    def get_platform(cls) -> IPlatform:
+        return Platform('Local')
+
+    @classmethod
+    def get_emod_binary(cls, ) -> str:
+        return get_github_eradication_url(emod_version)
