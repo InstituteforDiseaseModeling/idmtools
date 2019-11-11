6edf0b7751541d0623fb750b812f4aab85237493
Clinton Collins <clinton.collins@gmail.com>
2019-11-11 15:40:12 -0800

    fix image

--- examples/r_model/user_init.sh
+++ examples/r_model/user_init.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 if (( EUID == 0 )); then
-    echo "Runing s6 init scripts"
+    echo "Running s6 init scripts"
     /init &
     sleep 1
-    su idmtools -c "$@"
+    echo Running $@
+    su idmtools -c '$@'
 else
     echo "Skipping s6"
      $@
