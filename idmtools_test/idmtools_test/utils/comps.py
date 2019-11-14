241868382bef1d2a08679f45c13c3c07958e018f
shchen-idmod <38014764+shchen-idmod@users.noreply.github.com>
2019-11-14 12:32:52 -0800

    Add back example to run experiment in comps slurm cluster

--- idmtools_test/idmtools_test/utils/comps.py
+++ idmtools_test/idmtools_test/utils/comps.py
@@ -18,15 +18,19 @@ def get_asset_collection_by_id(collection_id, query_criteria=None):
     except (RuntimeError, ValueError):
         return None
 
+
 def sims_from_experiment(e):
     return e.get_simulations(QueryCriteria().select(['id', 'state']).select_children('hpc_jobs'))
+
 
 def workdirs_from_simulations(sims):
     return {str(sim.id): sim.hpc_jobs[-1].working_directory for sim in sims if sim.hpc_jobs}
 
+
 def get_simulation_path(simulation):
     path = workdirs_from_simulations([simulation])[str(simulation.id)]
     return path
+
 
 def get_simulation_by_id(sim_id, query_criteria=None):
     return Simulation.get(id=sim_id, query_criteria=query_criteria)
