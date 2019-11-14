ffbf61db9c3784c3ac375ff5035a042cd9737a95
Clinton Collins <clinton.collins@gmail.com>
2019-11-14 12:42:00 -0800

    Don't include usgi but in container

--- idmtools_platform_local/setup.py
+++ idmtools_platform_local/setup.py
@@ -21,11 +21,11 @@ with open('ui_requirements.txt') as requirements_file:
 
 
 setup_requirements = []
+server_requirements = ['uwsgi==2.0.18']
 test_requirements = ['pytest', 'pytest-runner', 'pytest-timeout', 'pytest-cache']
 
-if os.name != "nt":
-    ui_requirements.append('uwsgi==2.0.18')
-else:
+# Only install uwsgi on python 3.7
+if os.name == "nt":
     # TODO remove workaround. This is needed because 226 break windows virtual envs when calling processes
     # which we need
     test_requirements.append('pywin32==225')
@@ -33,6 +33,7 @@ else:
 extras = dict(test=test_requirements, dev=['Pympler'],
               # Requirements for running workers server
               workers=worker_requirements,
+              server=server_requirements,
               # these are only needed when not running UI
               ui=ui_requirements)
 # check for python 3.6
