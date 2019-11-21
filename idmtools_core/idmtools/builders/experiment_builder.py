75daeacf7df26e8b2301ba7c92db59b349d3525e
Clinton Collins <clinton.collins@gmail.com>
2019-11-20 18:52:32 -0800

    Undo changes

--- idmtools_core/idmtools/builders/experiment_builder.py
+++ idmtools_core/idmtools/builders/experiment_builder.py
@@ -1,6 +1,5 @@
 import inspect
 import typing
-from dataclasses import dataclass, field
 from functools import partial
 from inspect import signature
 from itertools import product
@@ -16,15 +15,10 @@ class ExperimentBuilder:
     """
     # The keyword searched in the function used for sweeps
     SIMULATION_ATTR = 'simulation'
-    sweeps: List[Any] = field(default_factory=list)
-    count: int = 1
 
     def __init__(self):
         self.sweeps = []
         self.count = 1
-
-    def __hash__(self):
-        return hash((self.SIMULATION_ATTR, str(self.sweeps), self.count))
 
     def add_sweep_definition(self, function: 'Callable', values: 'Union[List[Any], Iterable]'):
         """
