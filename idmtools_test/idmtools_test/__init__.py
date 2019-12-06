c603da53f061d1085bd7cb563cd563287c04cc46
Sharon Chen <shchen@idmod.org>
2019-12-06 15:00:01 -0800

    Bump version: 0.2.0+nightly.0 → 0.3.0

--- idmtools_test/idmtools_test/__init__.py
+++ idmtools_test/idmtools_test/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '0.2.0+nightly.0'
+__version__ = '0.3.0.0'
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 COMMON_INPUT_PATH = os.path.join(current_directory, "inputs")
