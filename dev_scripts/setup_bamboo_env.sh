33949b7c8113fc4b61a9b5a17cd81613445752b7
Clinton Collins <devclinton@users.noreply.github.com>
2019-12-09 23:27:10 -0800

    Update setup_bamboo_env.sh

--- dev_scripts/setup_bamboo_env.sh
+++ dev_scripts/setup_bamboo_env.sh
@@ -3,9 +3,6 @@
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Takes in the username and password for packages.idmod.org and creates a pypirc in users home directory
 
-echo "Setting up pip conf"
-cp "${SRC_DIR}/pip.conf" ~/pip.conf
-
 echo "Setting up pypirc"
 cp "${SRC_DIR}/.pypirc" ~/.pypirc
 echo "Replacing <username> with ${bamboo_UserArtifactory}"
