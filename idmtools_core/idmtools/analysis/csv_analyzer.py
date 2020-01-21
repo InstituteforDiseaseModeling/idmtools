967239513160fce34a0f21aecc7026ffc678cebf
INTERNAL\mfisher <mfisher@idmod.org>
2020-01-21 08:47:38 -0800

    add comments to csv and tags analyzers

--- idmtools_core/idmtools/analysis/csv_analyzer.py
+++ idmtools_core/idmtools/analysis/csv_analyzer.py
@@ -1,31 +1,41 @@
+# Example of a csv analyzer to concatenate csv results into one csv from your experiment simulations
+
+# First, import some necessary system and idmtools packages.
 import os
 import pandas as pd
 from idmtools.entities import IAnalyzer
-from idmtools.core import EntityStatus
 
 
+# Create a class for the analyzer
 class CSVAnalyzer(IAnalyzer):
+    # Arg option for analyzer init are uid, working_dir, parse (True to leverage the :class:`OutputParser`;
+    # False to get the raw data in the :meth:`select_simulation_data`), and filenames
+    # In this case, we want parse=True, and the filename(s) to analyze
     def __init__(self, filenames, parse=True):
         super().__init__(parse, filenames=filenames)
+        # Raise exception early if files are not csv files
         if not all(['csv' in os.path.splitext(f)[1].lower() for f in self.filenames]):
             raise Exception('Please ensure all filenames provided to CSVAnalyzer have a csv extension.')
 
+    # Map is called to get for each simulation a data object (all the metadata of the simulations) and simulation object
     def map(self, data, simulation):
-
+        # If there are 1 to many csv files, concatenate csv data columns into one dataframe
         concatenated_df = pd.concat(list(data.values()), axis=0, ignore_index=True, sort=True)
         return concatenated_df
 
+    # In reduce, we are printing the simulation and result data filtered in map
     def reduce(self, all_data):
 
         # Let's hope the first simulation is representative
-        first_sim = next(iter(all_data.keys()))
-        exp_id = str(first_sim.experiment.uid)
+        first_sim = next(iter(all_data.keys()))  # Iterate over the dataframe keys
+        exp_id = str(first_sim.experiment.uid)  # Set the exp id from the first sim data
 
-        results = pd.concat(list(all_data.values()), axis=0,
-                            keys=[str(k.uid) for k in all_data.keys()],
-                            names=['SimId'])
+        results = pd.concat(list(all_data.values()), axis=0,  # Combine a list of all the sims csv data column values
+                            keys=[str(k.uid) for k in all_data.keys()],  # Add a hierarchical index with the keys option
+                            names=['SimId'])  # Label the index keys you create with the names option
         results.index = results.index.droplevel(1)  # Remove default index
 
+        # Make a directory labeled the exp id to write the csv results to
         os.makedirs(exp_id, exist_ok=True)
         # NOTE: If running twice with different filename, the output files will collide
         results.to_csv(os.path.join(exp_id, self.__class__.__name__+'.csv'))
--- idmtools_core/idmtools/analysis/tags_analyzer.py
+++ idmtools_core/idmtools/analysis/tags_analyzer.py
@@ -1,22 +1,30 @@
-from idmtools.analysis.analyze_manager import AnalyzeManager
+# Example of a tags analyzer to get all the tags from your experiment simulations into one csv file
+
+# First, import some necessary system and idmtools packages.
 import os
 import pandas as pd
 from idmtools.entities import IAnalyzer
 
 
+# Create a class for the analyzer
 class TagsAnalyzer(IAnalyzer):
+    # Arg option for analyzer init are uid, working_dir, parse (True to leverage the :class:`OutputParser`;
+    # False to get the raw data in the :meth:`select_simulation_data`), and filenames
+    # In this case, we want uid, working_dir, and parse=True
     def __init__(self, uid=None, working_dir=None, parse=True):
         super().__init__(uid, working_dir, parse)
         self.exp_id = None
 
+    # Map is called to get for each simulation a data object (all the metadata of the simulations) and simulation object
     def map(self, data, simulation):
-        df = pd.DataFrame(columns=simulation.tags.keys())
-        df.loc[str(simulation.uid)] = list(simulation.tags.values())
-        df.index.name = 'SimId'
+        df = pd.DataFrame(columns=simulation.tags.keys())  # Create a dataframe with the simulation tag keys
+        df.loc[str(simulation.uid)] = list(simulation.tags.values())  # Get a list of the sim tag values
+        df.index.name = 'SimId'  # Label the index keys you create with the names option
         return df
 
+    # In reduce, we are printing the simulation and result data filtered in map
     def reduce(self, all_data):
-        exp_id = str(next(iter(all_data.keys())).experiment.uid)
-        results = pd.concat(list(all_data.values()), axis=0)
-        os.makedirs(exp_id, exist_ok=True)
-        results.to_csv(os.path.join(exp_id, 'tags.csv'))
+        exp_id = str(next(iter(all_data.keys())).experiment.uid)  # Set the exp id from the first sim data
+        results = pd.concat(list(all_data.values()), axis=0)  # Combine a list of all the sims tag values
+        os.makedirs(exp_id, exist_ok=True)  # Make a directory labeled the exp id to write the tags to a csv
+        results.to_csv(os.path.join(exp_id, 'tags.csv'))  # Write the sim tags to a csv
