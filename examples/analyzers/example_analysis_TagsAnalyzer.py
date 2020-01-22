ab4cf25eff6e5f7da9eecb264b4d8758f89cff02
mfisher-idmod <37818827+mfisher-idmod@users.noreply.github.com>
2020-01-21 16:48:04 -0800

    New general purpose analyzers with examples and tests (#581)
    * analyzer changes, examples, and tests
    
    * fixes to analyzer tests
    
    * change platform to COMPS2
    
    * change tags analyzer platform to COMPS2
    
    * add comments to csv and tags analyzers
    
    * change to simple sim and csv example
    
    * point examples to comps2 staging

---
+++ examples/analyzers/example_analysis_TagsAnalyzer.py
@@ -0,0 +1,28 @@
+# Example TagsAnalyzer for any experiment
+# In this example, we will demonstrate how to use a TagsAnalyzer to put your sim tags in a csv file
+
+# First, import some necessary system and idmtools packages.
+from idmtools.analysis.analyze_manager import AnalyzeManager
+from idmtools.analysis.tags_analyzer import TagsAnalyzer
+from idmtools.core import ItemType
+from idmtools.core.platform_factory import Platform
+
+if __name__ == '__main__':
+
+    # Set the platform where you want to run your analysis
+    # In this case we are running in COMPS since the Work Item we are analyzing was run on COMPS
+    platform = Platform('COMPS2')
+
+    # Arg option for analyzer init are uid, working_dir, data in the method map (aka select_simulation_data),
+    # and filenames
+    # Initialize the analyser class which just requires an experiment id
+    analyzers = [TagsAnalyzer()]
+
+    # Set the experiment id you want to analyze
+    experiment_id = '36d8bfdc-83f6-e911-a2be-f0921c167861'  # staging exp id JSuresh's Magude exp
+
+    # Specify the id Type, in this case an Experiment on COMPS
+    manager = AnalyzeManager(configuration={}, partial_analyze_ok=True, platform=platform,
+                             ids=[(experiment_id, ItemType.EXPERIMENT)],
+                             analyzers=analyzers)
+    manager.analyze()
