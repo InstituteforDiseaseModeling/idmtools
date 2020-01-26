3ea2758f9179eb0ee852b84dbc3fcb02aac4ea3c
Clinton Collins <clinton.collins@gmail.com>
2020-01-25 16:50:24 -0800

    Remove #142 regression test. Batching is a generator now so it always return the next item. Internally there is a grouping batch of 10 but that isn't related to user expose batching and only includes parameters

--- idmtools_core/tests/test_regression.py
+++ idmtools_core/tests/test_regression.py
@@ -2,14 +2,12 @@ import os
 import unittest
 
 from idmtools.assets import AssetCollection
-from idmtools.builders import SimulationBuilder, StandAloneSimulationsBuilder
+from idmtools.builders import StandAloneSimulationsBuilder
 from idmtools.core.platform_factory import Platform
-from idmtools.entities.templated_simulation import TemplatedSimulations
 from idmtools.managers import ExperimentManager
-from idmtools_test.utils.itest_with_persistence import ITestWithPersistence
 from idmtools_models.python import PythonExperiment, PythonSimulation
 from idmtools_test import COMMON_INPUT_PATH
-from idmtools_test.utils.test_task import TestTask
+from idmtools_test.utils.itest_with_persistence import ITestWithPersistence
 
 
 class TestPersistenceServices(ITestWithPersistence):
@@ -46,28 +44,6 @@ class TestPersistenceServices(ITestWithPersistence):
         ac.add_directory(assets_directory=os.path.join(COMMON_INPUT_PATH, "regression", "125", "Assets2"),
                          relative_path="MyExternalLibrary")
         self.assertTrue(all([a.relative_path.startswith("MyExternalLibrary") for a in ac]))
-
-    def test_fix_142(self):
-        # https://github.com/InstituteforDiseaseModeling/idmtools/issues/142
-        ts = TemplatedSimulations(base_task=TestTask())
-        b = SimulationBuilder()
-        b.add_sweep_definition(lambda simulation, v: {"p": v}, range(500))
-        ts.builder = b
-
-        counter = 0
-        for item in ts:
-            self.assertEqual(len(batch), 100)
-            counter += 1
-        self.assertEqual(counter, 5)
-
-        b = SimulationBuilder()
-        b.add_sweep_definition(lambda simulation, v: {"p": v}, range(500))
-        ts.builder = b
-        counter = 0
-        for batch in ts.batch_simulations(200):
-            self.assertTrue(len(batch) in (100, 200))
-            counter += 1
-        self.assertEqual(counter, 3)
 
     def test_fix_138(self):
         # https://github.com/InstituteforDiseaseModeling/idmtools/issues/138
