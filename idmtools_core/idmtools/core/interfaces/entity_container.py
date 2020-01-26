ecf82c0a8f5b15843be8e5966e5f16475ed9998a
Clinton Collins <clinton.collins@gmail.com>
2020-01-26 07:54:12 -0800

    Remove parent from EntityContainer

--- idmtools_core/idmtools/core/interfaces/entity_container.py
+++ idmtools_core/idmtools/core/interfaces/entity_container.py
@@ -5,28 +5,9 @@ if typing.TYPE_CHECKING:
     from typing import List
 
 
-
-class ParentIterator:
-    def __init__(self, lst: typing.Iterator, parent: 'IEntity'):
-        self.items = lst
-        self.parent = parent
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        i = next(self.items)
-        i._parent = self.parent
-        if hasattr(i, 'parent_id') and self.parent.uid is not None:
-            i.parent_id = self.parent.uid
-        return i
-
-
-
 class EntityContainer(list):
-    def __init__(self, children: 'List[IEntity]' = None, parent: 'IEntity' = None):
+    def __init__(self, children: 'List[IEntity]' = None):
         super().__init__()
-        self.parent = parent
         self.extend(children or [])
 
     def set_status(self, status):
@@ -41,8 +22,3 @@ class EntityContainer(list):
 
         raise Exception(f"Item with id {item_id} not found in the container")
 
-    def __iter__(self):
-        if self.parent is not None:
-            return ParentIterator(super().__iter__(), self.parent)
-        else:
-            return super().__iter__()
