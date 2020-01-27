e0d6416233428d5f4f6a73d062df08ef2d26bf9a
zdu <zdu@idmod.org>
2020-01-27 14:15:41 -0800

    SSMT: update three samples to use COMPSPlatform instead of SSMTPlatform

--- examples/ssmt/hello_world/run_ssmt_task.py
+++ examples/ssmt/hello_world/run_ssmt_task.py
@@ -6,9 +6,10 @@ from idmtools.ssmt.ssmt_work_item import SSMTWorkItem
 wi_name = "SSMT Hello 1"
 command = "python hello.py"
 user_files = FileList(root='files')
+tags = {'test': 123}
 
 if __name__ == "__main__":
-    platform = Platform('SSMT')
-    wi = SSMTWorkItem(item_name=wi_name, command=command, user_files=user_files)
+    platform = Platform('COMPS2')
+    wi = SSMTWorkItem(item_name=wi_name, command=command, user_files=user_files, tags=tags)
     wim = WorkItemManager(wi, platform)
     wim.process(check_status=True)
