b122a7bce3e684457483af443e5a34445cfb8081
Clinton Collins <clinton.collins@gmail.com>
2020-01-28 16:17:39 -0800

    Remove python/r experiments Remove emod from models in default install of core

--- idmtools_core/setup.py
+++ idmtools_core/setup.py
@@ -25,10 +25,9 @@ extras = {
     # to support notebooks we need docker
     'notebooks': ['docker==4.0.1'],
     'packaging': build_requirements,
-    'idm': ['idmtools_platform_comps', 'idmtools_cli', 'idmtools_model_emod', 'idmtools_models'],
+    'idm': ['idmtools_platform_comps', 'idmtools_cli', 'idmtools_models'],
     # our full install include all common plugins
-    'full': ['idmtools_platform_comps', 'idmtools_platform_local', 'idmtools_cli', 'idmtools_model_emod',
-             'idmtools_models']
+    'full': ['idmtools_platform_comps', 'idmtools_platform_local', 'idmtools_cli', 'idmtools_models']
 }
 
 authors = [
