a0e07aee2c9dedf58f3072e90088c87e96b6991f
Clinton Collins <clinton.collins@gmail.com>
2020-02-04 12:29:47 -0800

    Add logging to run commands to troubleshoot environment issues

--- dev_scripts/run.py
+++ dev_scripts/run.py
@@ -1,6 +1,23 @@
 import argparse
 import os
+import subprocess
 import sys
+from logging import getLogger
+import logging
+
+logger = getLogger(__name__)
+
+
+def setup_logging(working_dir):
+    logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+    rootLogger = logging.getLogger()
+    fileHandler = logging.FileHandler("%s/make.log" % os.path.abspath(working_dir))
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -11,21 +28,31 @@ if __name__ == '__main__':
     parser.add_argument('-ex', help='Add items to the path')
 
     args = parser.parse_args()
+    setup_logging(args.working_dir)
     if args.environment:
         env = dict()
         for e in args.environment:
             parts = e.split("=")
             env[parts[0]] = parts[1]
-        print(f"Environment opts: {env}")
+        logger.info(f"Environment opts: {env}")
 
     if args.path:
         for p in args.path:
-            print(f'Adding {os.path.abspath(p)} to the path')
+            logger.info(f'Adding {os.path.abspath(p)} to the path')
             sys.path.insert(0, os.path.abspath(p))
 
+    logger.debug('Environment: %s', str(os.environ))
+
     if args.working_dir:
-        print(f'Changing working directory to {os.path.abspath(args.working_dir)}')
+        logger.info(f'Changing working directory to {os.path.abspath(args.working_dir)}')
         os.chdir(os.path.abspath(args.working_dir))
-    print(f'Running {args.ex}')
-    result = os.system(args.ex)
-    sys.exit(result if args.return_code is None else 0)
+    logger.info(f'Running {args.ex}')
+    try:
+        output = subprocess.check_output(args.ex)
+        result = 0
+    except subprocess.CalledProcessError as e:
+        result = e.returncode
+        output = e.output
+        logger.debug(f'Return Code: {result}')
+    logger.debug('Command output: %s', output)
+    sys.exit(result)
