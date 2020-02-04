8b201a6bcf7b99c393790e6652e337ca68f35026
zdu <zdu@idmod.org>
2020-02-04 09:23:34 -0800

    SSMT: add example for InputDataWorker type

---
+++ examples/ssmt/inputdata example/run_input_data_task.py
@@ -0,0 +1,17 @@
+import os
+from idmtools.core.platform_factory import Platform
+from idmtools.managers.work_item_manager import WorkItemManager
+from idmtools.ssmt.ssmt_work_item import SSMTWorkItem
+
+wi_name = "InputDataWorker Hello 1"
+sim_id = "8db8ae8f-793c-ea11-a2be-f0921c167861"
+tags = {'SimulationId': sim_id}
+node_type = 'Points'
+
+if __name__ == "__main__":
+    platform = Platform('COMPS2')
+    wi = SSMTWorkItem(item_name=wi_name, tags=tags, work_item_type='InputDataWorker')
+    wi.load_work_order(os.path.join('..', 'files', 'inputdataworker_workorder.json'))
+
+    wim = WorkItemManager(wi, platform)
+    wim.process(check_status=True)
