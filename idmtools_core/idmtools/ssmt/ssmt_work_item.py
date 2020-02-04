4fa6712bc224561538044bcdae2e9cde4dd3348b
zdu <zdu@idmod.org>
2020-02-04 09:06:20 -0800

    SSMT: SSMTWorkItem support InputData and vistools and more related object

--- idmtools_core/idmtools/ssmt/ssmt_work_item.py
+++ idmtools_core/idmtools/ssmt/ssmt_work_item.py
@@ -1,4 +1,5 @@
 import typing
+import json
 from idmtools.core import TTags
 from dataclasses import dataclass, field
 from idmtools.core import ItemType
@@ -21,8 +22,12 @@ class SSMTWorkItem(IWorkflowItem):
     asset_collection_id: UUID = field(default=None)
     asset_files: FileList = field(default=None)
     user_files: FileList = field(default=None)
-    wo_kwargs: dict = field(default_factory=lambda: {})
+    work_order: dict = field(default_factory=lambda: {})
     related_experiments: list = field(default=None)
+    related_simulations: list = field(default=None)
+    related_suites: list = field(default=None)
+    related_work_items: list = field(default=None)
+    related_asset_collections: list = field(default=None)
 
     work_item_type: str = field(default=None)
     docker_image: str = field(default=None)
@@ -32,12 +37,31 @@ class SSMTWorkItem(IWorkflowItem):
 
     def __post_init__(self):
         self.tags = self.tags or {}
-        self.wo_kwargs = self.wo_kwargs or {}
+        self.work_order = self.work_order or {}
         self.asset_files = self.asset_files or FileList()
         self.user_files = self.user_files or FileList()
 
     def __repr__(self):
         return f"<WorkItem {self.uid}>"
+
+    def get_base_work_order(self, platform):
+        wi_type = self.work_item_type or platform.work_item_type
+
+        base_wo = {"WorkItem_Type": wi_type}
+
+        if wi_type == 'DockerWorker':
+            base_wo = {
+                "WorkItem_Type": wi_type,
+                "Execution": {
+                    "ImageName": self.docker_image or platform.docker_image,
+                    "Command": self.command
+                }
+            }
+
+        return base_wo
+
+    def load_work_order(self, wo_file):
+        self.work_order = json.load(open(wo_file, 'rb'))
 
     def gather_assets(self) -> NoReturn:
         """
@@ -55,7 +79,16 @@ class SSMTWorkItem(IWorkflowItem):
         """
         self.user_files.add_asset_file(af)
 
-    def add_wo_arg(self, name, value):
+    def set_work_order(self, wo):
+        """
+        Update wo for the name with value
+        Args:
+            wo: user wo
+        Returns: None
+        """
+        self.work_order = wo
+
+    def update_work_order(self, name, value):
         """
         Update wo for the name with value
         Args:
@@ -64,7 +97,7 @@ class SSMTWorkItem(IWorkflowItem):
 
         Returns: None
         """
-        self.wo_kwargs[name] = value
+        self.work_order[name] = value
 
     def clear_user_files(self):
         """
@@ -81,7 +114,7 @@ class SSMTWorkItem(IWorkflowItem):
         Returns: None
 
         """
-        self.wo_kwargs = {}
+        self.work_order = {}
 
 
 IWorkflowItemClass = typing.Type[SSMTWorkItem]
