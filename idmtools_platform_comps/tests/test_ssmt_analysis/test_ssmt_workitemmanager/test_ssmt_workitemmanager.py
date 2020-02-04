a3b65fee644c061bbf2656dec8c88346b7c6c125
INTERNAL\mfisher <mfisher@idmod.org>
2020-02-04 11:48:03 -0800

    clean up SSMT work item tests and typos

--- idmtools_platform_comps/tests/test_ssmt_analysis/test_ssmt_workitemmanager/test_ssmt_workitemmanager.py
+++ idmtools_platform_comps/tests/test_ssmt_analysis/test_ssmt_workitemmanager/test_ssmt_workitemmanager.py
@@ -3,6 +3,7 @@ import unittest
 import xmlrunner
 import json
 import sys
+import pytest
 
 from idmtools.assets.FileList import FileList
 from idmtools.core.platform_factory import Platform
@@ -16,13 +17,15 @@ par_par_dir = os.path.normpath(os.path.join('..', os.pardir))
 sys.path.append(os.path.join(par_par_dir,'General'))
 from idmtools_test.utils.utils import del_file
 
-
+@pytest.mark.skip
+# TODO: these won't work until a docker image with idmtools is deployed to the comps ssmt server
 class RunAnalyzeTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.par_par_dir = par_par_dir
-        #Comment out if you do not want to generate exp and sims
+        # TODO: these tests depend on EMOD experiments which will be disabled; to write tests for Python experiments
+        # Comment out if you do not want to generate exp and sims
         # exp = BuildExperiment()
         # exp.build_experiment(exp_name="Experiment for ssmt")
         # cls.exp_id = exp.exp_id
@@ -32,7 +35,7 @@ class RunAnalyzeTest(unittest.TestCase):
 
     def setUp(self):
         print(self._testMethodName)
-        self.wi_name = "SSMT WorkItemManager Test"
+        self.wi_name = "idmtools SSMT WorkItemManager Test"
         self.tags = {'idmtools': self._testMethodName, 'WorkItem type': 'Docker'}
 
     #------------------------------------------
@@ -43,9 +46,8 @@ class RunAnalyzeTest(unittest.TestCase):
         expid = self.exp_id
         command = "python run_PopulationAnalyzer.py " + expid
 
-        # upload 3 files to docker's current folder as linked file
+        # upload files to docker's current folder as linked file
         user_files = FileList(root='.', files_in_root=['PopulationAnalyzer.py', 'run_PopulationAnalyzer.py'])
-        # user_files.add_file(os.path.join(self.par_par_dir, "simtools.ini"))
 
         platform = Platform('COMPS2')
         wi = SSMTWorkItem(item_name=self.wi_name, command=command, user_files=user_files,
@@ -65,12 +67,8 @@ class RunAnalyzeTest(unittest.TestCase):
         asset_files.add_file('PopulationAnalyzer.py')
         asset_files.add_file('run_PopulationAnalyzer.py')
 
-        # upload simtools.ini to current dir in docker
-        user_files = FileList()
-        user_files.add_file(os.path.join(self.par_par_dir, "simtools.ini"))
-
         platform = Platform('COMPS2')
-        wi = SSMTWorkItem(item_name=self.wi_name, command=command, asset_files=asset_files, user_files=user_files,
+        wi = SSMTWorkItem(item_name=self.wi_name, command=command, asset_files=asset_files,
                           related_experiments=[self.exp_id], tags=self.tags)
         wim = WorkItemManager(wi, platform)
         wim.process(check_status=True)
@@ -89,14 +87,13 @@ class RunAnalyzeTest(unittest.TestCase):
         current_dir = os.path.abspath(os.path.dirname(__file__))
         assets_dir = os.path.join(current_dir, 'analyzer')
         # uncomment following line if you want to upload everything under 'analyzer' folder to remote
-        #asset_files.add_path(assets_dir, relative_path="analyzer", recursive=True)
+        # asset_files.add_path(assets_dir, relative_path="analyzer", recursive=True)
 
         # only upload 'PopulationAnalyzer.py' under analyzer folder to remote docker's Assets\analyzer folder
         asset_files.add_file(os.path.join(assets_dir, 'PopulationAnalyzer.py'), relative_path="analyzer")
 
-        # upload simtools.ini and run_anaysis1.py files to current dir in docker
+        # upload run_PopulationAnalyzer1.py file to current dir in docker
         user_files = FileList()
-        user_files.add_file(os.path.join(self.par_par_dir, "simtools.ini"))
         user_files.add_file('run_PopulationAnalyzer1.py')
 
         platform = Platform('COMPS2')
@@ -122,9 +119,8 @@ class RunAnalyzeTest(unittest.TestCase):
         # only upload 'PopulationAnalyzer.py' under analyzer folder to remote docker's Assets\analyzer folder
         asset_files.add_file(os.path.join(assets_dir, 'PopulationAnalyzer_with_output_folder.py'), relative_path="analyzer")
 
-        # upload simtools.ini and run_anaysis1.py files to current dir in docker
+        # upload run_PopulationAnalyzer_with_output_folder.py file to current dir in docker
         user_files = FileList()
-        user_files.add_file(os.path.join(self.par_par_dir, "simtools.ini"))
         user_files.add_file('run_PopulationAnalyzer_with_output_folder.py')
 
         platform = Platform('COMPS2')
@@ -141,11 +137,11 @@ class RunAnalyzeTest(unittest.TestCase):
     def test_ssmt_workitemmanager_with_multiple_analyzers(self):
         import pandas
         expid = self.exp_id
-        command = "python run_2analyers.py " + expid
+        command = "python run_2analyzers.py " + expid
 
         # add all files under 'analyzer' folder and put folder (relative_path) in docker
-        user_files = FileList(root='.', files_in_root=['PopulationAnalyzer.py', 'TimeseriesAnalyzer.py', 'run_2analyers.py'])
-        user_files.add_file(os.path.join(self.par_par_dir, "simtools.ini"))
+        user_files = FileList(root='.',
+                              files_in_root=['PopulationAnalyzer.py', 'TimeseriesAnalyzer.py', 'run_2analyzers.py'])
 
         platform = Platform('COMPS2')
         wi = SSMTWorkItem(item_name=self.wi_name, command=command, user_files=user_files,
