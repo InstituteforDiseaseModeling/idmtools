c45a2ef7116b357fb622f7fe5a124d3d8bf8bbd8
Clinton Collins <clinton.collins@gmail.com>
2020-02-06 10:59:25 -0800

    Add test for examples for of python models

---
+++ idmtools_test/tests/test_example_python_model.py
@@ -0,0 +1,15 @@
+import os
+from unittest import TestCase
+
+import pytest
+
+from idmtools_test.utils.utils import test_example_folder
+
+examples_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'examples'))
+python_model = os.path.join(examples_folder, 'python_model')
+
+
+@pytest.mark.example
+class TestPythonModelDirectory(TestCase):
+    def test_python_model_directory(self):
+        test_example_folder(self, python_model)
