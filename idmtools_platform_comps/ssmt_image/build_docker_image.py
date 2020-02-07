35c2056d99d9c7a8aca33aae89b56861d3f6bb64
Clinton Collins <clinton.collins@gmail.com>
2020-02-07 08:57:11 -0800

    SSMT docker image

---
+++ idmtools_platform_comps/ssmt_image/build_docker_image.py
@@ -0,0 +1,22 @@
+####
+# This script is currently a workaround so that we can use bump2version with docker since the nightly versions
+# don't work with docker registry
+import os
+import subprocess
+import sys
+from urllib.parse import urlparse
+
+version = open('../VERSION').read().strip()
+if '+nightly' in version:
+    version = version.replace('+nightly', '.nightly')
+uri = urlparse(sys.argv[1])
+
+cmd = ['docker', 'build', '--network=host', '--build-arg', f'PYPIURL={sys.argv[1]}', '--build-arg',
+       f'PYPIHOST={uri.hostname}', '--tag',
+       f'idm-docker-staging.packages.idmod.org/idmtools_comps_ssmt_worker:{version}', '.']
+if len(sys.argv) == 3 and sys.argv[2] == "no-cache":
+    cmd.insert(2, "--no-cache")
+print(f'Running: {" ".join(cmd)}')
+p = subprocess.Popen(" ".join(cmd), cwd=os.path.abspath(os.path.dirname(__file__)), shell=True)
+p.wait()
+sys.exit(p.returncode)
