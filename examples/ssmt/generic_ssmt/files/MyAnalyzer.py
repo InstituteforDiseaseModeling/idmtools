6ad18d8d8cad35d82828951a75f83f42f81a98f9
shchen-idmod <38014764+shchen-idmod@users.noreply.github.com>
2020-02-19 10:39:57 -0800

    fix ssmt examples, TODO, still need to fix all integration tests

--- examples/ssmt/generic_ssmt/files/MyAnalyzer.py
+++ examples/ssmt/generic_ssmt/files/MyAnalyzer.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 try:
     # use idmtools image
@@ -8,7 +9,6 @@ except ImportError:
     from simtools.Analysis.BaseAnalyzers.BaseAnalyzer import BaseAnalyzer
 
 import matplotlib as mpl
-
 mpl.use('Agg')
 
 
@@ -21,7 +21,10 @@ class PopulationAnalyzer(BaseAnalyzer):
         return data[self.filenames[0]]["Channels"]["Statistical Population"]["Data"]
 
     def finalize(self, all_data):
-        with open("results.json", "w") as fp:
+        # output directory to store json and image
+        output_dir = os.path.join(self.working_dir, "output")
+
+        with open(os.path.join(output_dir, "population.json"), "w") as fp:
             json.dump({s.id: v for s, v in all_data.items()}, fp)
 
         import matplotlib.pyplot as plt
@@ -32,4 +35,24 @@ class PopulationAnalyzer(BaseAnalyzer):
         for pop in list(all_data.values()):
             ax.plot(pop)
         ax.legend([s.id for s in all_data.keys()])
-        fig.savefig("Figure.png")
+        fig.savefig(os.path.join(output_dir, "population.png"))
+
+    def initialize(self):
+        if not os.path.exists(os.path.join(self.working_dir, "output")):
+            os.mkdir(os.path.join(self.working_dir, "output"))
+
+    # idmtools analyzer
+    def map(self, data: 'Any', item: 'IItem') -> 'Any':
+        return data[self.filenames[0]]["Channels"]["Statistical Population"]["Data"]
+
+    def reduce(self, all_data: dict) -> 'Any':
+        output_dir = os.path.join(self.working_dir, "output")
+        with open(os.path.join(output_dir, "population.json"), "w") as fp:
+            json.dump({str(s.uid): v for s, v in all_data.items()}, fp)
+        import matplotlib.pyplot as plt
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for pop in list(all_data.values()):
+            ax.plot(pop)
+        ax.legend([str(s.uid) for s in all_data.keys()])
+        fig.savefig(os.path.join(output_dir, "population.png"))
