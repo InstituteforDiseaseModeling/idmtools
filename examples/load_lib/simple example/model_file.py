81643ccad9602803368d245ba5ef3af3a226725b
zdu <zdu@idmod.org>
2020-02-25 14:16:35 -0800

    #603: update example

--- examples/load_lib/simple example/model_file.py
+++ examples/load_lib/simple example/model_file.py
@@ -1,32 +1,19 @@
-import json
 import os
 import sys
 import ast
 import inspect
-# from COMPS.Data import Experiment
 
-# dir_path = os.path.dirname(os.path.realpath(__file__))    # working
-from idmtools.entities.experiment import Experiment
-
-dir_path = os.path.dirname(__file__)  # working
+dir_path = os.path.dirname(__file__)
 sys.path.append(os.path.join(dir_path, "Assets", "lib", "site-packages"))  # Need to site-packages level!!!
-
-# print('dir_path: \n', dir_path)
-# print(os.path.join(dir_path, "Assets", "lib", "site-packages"))
 
 import astor
 
 
 def dump_tree():
-    # src = inspect.getsource(SimpleVaccine)
-    src = inspect.getsource(Experiment)
-    # src = inspect.getsource(A)
+    src = inspect.getsource(ast)
     expr_ast = ast.parse(src)
 
-    # print(ast.dump(expr_ast))  # one line
-    print(astor.dump_tree(expr_ast))  # one line
-    # print(codegen.dump(expr_ast))                 # one line ast dump
-    # print(astunparse.dump(expr_ast))                # JSON format
+    print(astor.dump_tree(expr_ast))
 
 
 if __name__ == "__main__":
