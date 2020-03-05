277f4d9c6ab20117250edd3a29b400f8e93c6b21
zdu <zdu@idmod.org>
2020-03-05 12:50:37 -0800

    #639: add sample to support wheels

---
+++ examples/load_lib/simple_wheel_example/model_file.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import ast
+import inspect
+
+CURRENT_DIRECTORY = os.path.dirname(__file__)
+LIBRARY_PATH = os.path.join(CURRENT_DIRECTORY, 'site_packages')  # Need to site_packages level!!!
+
+"""
+sys.path.insert(0, LIBRARY_PATH) will search packages from experiment's 'Assets/site_packages' first,
+then default HPC python site_packages.
+"astor" package does not exist in comps, so we use custom_lib to upload to experiment's Assets/site_packages directory
+Please go to examples/load_lib/example-load-lib.py to see how we upload 'astor' package to comps
+"""
+sys.path.insert(0, LIBRARY_PATH)  # Very Important!
+print(sys.path)
+
+import astor
+
+
+def dump_tree():
+    print('astor version: ', astor.__version__)
+    print('astor path: ', astor.__path__)
+    print(astor)
+
+    src = inspect.getsource(ast)
+    expr_ast = ast.parse(src)
+
+    print(astor.dump_tree(expr_ast))
+
+
+if __name__ == "__main__":
+    dump_tree()
