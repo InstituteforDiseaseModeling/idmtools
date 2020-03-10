e0f508bb3faae29c8fab3cc6504bc54eef47bceb
INTERNAL\mfisher <mfisher@idmod.org>
2020-03-10 13:34:49 -0700

    fix dropbox loc

--- idmtools_core/idmtools/utils/dropbox_location.py
+++ idmtools_core/idmtools/utils/dropbox_location.py
@@ -9,6 +9,5 @@ def get_current_user():
 
 def get_dropbox_location():
     user = get_current_user()
-    dropbox_filepath = os.path.join("C:/Users/", "mfisher", "Dropbox (IDM)")
-    # dropbox_filepath = os.path.join("C:/Users/", user, "Dropbox (IDM)/Malaria Team Folder/")
+    dropbox_filepath = os.path.join("C:/Users/", user, "Dropbox (IDM)")
     return dropbox_filepath
