8bbcccd4ab235b1f438bf93631285feeaa96f50d
shchen-idmod <38014764+shchen-idmod@users.noreply.github.com>
2020-04-15 13:24:07 -0700

    revert previous change

--- idmtools_platform_comps/tests/inputs/run_multiple_exps.py
+++ idmtools_platform_comps/tests/inputs/run_multiple_exps.py
@@ -4,9 +4,7 @@ from sys import argv
 from idmtools.analysis.analyze_manager import AnalyzeManager
 from idmtools.core import ItemType
 from idmtools.core.platform_factory import Platform
-
-sys.path.append(os.path.dirname(__file__))
-from population_analyzer import PopulationAnalyzer  # noqa
+from emodpy.analyzers.population_analyzer import PopulationAnalyzer
 
 sys.path.append(os.path.dirname(__file__))
 
