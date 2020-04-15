4c35513dc14ec0f6bf4b736b1e24913389ac9309
Clinton Collins <clinton.collins@gmail.com>
2020-04-15 09:55:18 -0700

    Add items back to repo

---
+++ idmtools_platform_comps/tests/inputs/adult_vectors_analyzer.py
@@ -0,0 +1,39 @@
+import json
+import os
+from typing import Any
+
+from idmtools.core.interfaces.iitem import IItem
+
+from idmtools.entities.ianalyzer import IAnalyzer as BaseAnalyzer
+import matplotlib as mpl
+
+mpl.use('Agg')
+
+
+class AdultVectorsAnalyzer(BaseAnalyzer):
+
+    def __init__(self, name='hi'):
+        super().__init__(filenames=["output\\InsetChart.json"])
+        print(name)
+
+    def initialize(self):
+        if not os.path.exists(os.path.join(self.working_dir, "output")):
+            os.mkdir(os.path.join(self.working_dir, "output"))
+
+    def map(self, data: Any, item: IItem) -> Any:
+        return data[self.filenames[0]]["Channels"]["Adult Vectors"]["Data"]
+
+    def reduce(self, all_data: dict) -> Any:
+        output_dir = os.path.join(self.working_dir, "output")
+        with open(os.path.join(output_dir, "adult_vectors.json"), "w") as fp:
+            json.dump({str(s.uid): v for s, v in all_data.items()}, fp)
+
+        import matplotlib.pyplot as plt
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+        for pop in list(all_data.values()):
+            ax.plot(pop)
+        ax.legend([str(s.uid) for s in all_data.keys()])
+        fig.savefig(os.path.join(output_dir, "adult_vectors.png"))
---
+++ idmtools_platform_comps/tests/inputs/run_multiple_exps.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from sys import argv
+from idmtools.analysis.analyze_manager import AnalyzeManager
+from idmtools.core import ItemType
+from idmtools.core.platform_factory import Platform
+from emodpy.analyzers.population_analyzer import PopulationAnalyzer
+
+sys.path.append(os.path.dirname(__file__))
+
+if __name__ == "__main__":
+    platform = Platform('COMPS2')
+
+    analyzers = [PopulationAnalyzer()]
+    if len(sys.argv) > 1:
+        expid = []
+        expid.append((argv[1], ItemType.EXPERIMENT))
+        expid.append((argv[2], ItemType.EXPERIMENT))
+    else:
+        expid = [('8bb8ae8f-793c-ea11-a2be-f0921c167861', ItemType.EXPERIMENT),
+                 ('4ea96af7-1549-ea11-a2be-f0921c167861', ItemType.EXPERIMENT)]
+    am = AnalyzeManager(platform=platform, ids=expid, analyzers=analyzers)
+    am.analyze()
