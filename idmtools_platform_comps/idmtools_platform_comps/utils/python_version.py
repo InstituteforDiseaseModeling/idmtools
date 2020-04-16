4ca6028c973941ded3e8e890b7e6be2a54facd60
zdu <zdu@idmod.org>
2020-04-16 11:02:04 -0700

    #716: address CommonTask case.

--- idmtools_platform_comps/idmtools_platform_comps/utils/python_version.py
+++ idmtools_platform_comps/idmtools_platform_comps/utils/python_version.py
@@ -3,26 +3,27 @@ PYTHON_EXECUTABLE = 'python3'
 
 
 def platform_task_hooks(task, platform):
+    """
+    Update task with new python command: python3
+    Args:
+        task: PythonTask or CommandTask
+        platform: the platform user uses
+
+    Returns: re-build task
+    """
     try:
         from idmtools_models.python.python_task import PythonTask
+        from idmtools.entities.command_task import CommandTask
         if isinstance(task, PythonTask):
-            # check if we are on slurm and python_path has not been changed
-            if platform.environment.lower() in SLURM_ENVIRONMENTS and task.python_path == 'python':
+            if platform.environment.lower() in SLURM_ENVIRONMENTS and task.python_path.lower() == 'python':
                 task.python_path = PYTHON_EXECUTABLE
+        elif isinstance(task, CommandTask):
+            if platform.environment.lower() in SLURM_ENVIRONMENTS:
+                cmd_list = task.command.executable.split(' ')
+                if cmd_list[0].lower() == 'python':
+                    cmd_list[0] = PYTHON_EXECUTABLE
+                    task.command.executable = ' '.join(cmd_list)
     except ImportError:
         pass
 
     return task
-
-
-def platform_command_hooks(command, platform):
-    try:
-        from idmtools.entities import CommandLine
-        if isinstance(command, CommandLine):
-            # check if we are on slurm and python_path has not been changed
-            if platform.environment.lower() in SLURM_ENVIRONMENTS and command.executable.lower() == 'python':
-                command.executable = PYTHON_EXECUTABLE
-    except ImportError:
-        pass
-
-    return command
