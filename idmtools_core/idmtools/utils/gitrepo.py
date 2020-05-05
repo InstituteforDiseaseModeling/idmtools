94cb2f111e815dda286f48a3e9faaf21345dc76d
zdu <zdu@idmod.org>
2020-05-05 16:12:08 -0700

    #818: refactor code

--- idmtools_core/idmtools/utils/gitrepo.py
+++ idmtools_core/idmtools/utils/gitrepo.py
@@ -6,32 +6,33 @@ import signal
 import argparse
 from colorama import Fore, Style, init
 
+REPO_OWNER = 'InstituteforDiseaseModeling'
+REPO_NAME = 'idmtools'
+API_HOME = 'https://api.github.com'
+
 
 class GitRepo:
     class UnknownRepository(Exception):
         pass
 
-    REPO_OWNER = 'InstituteforDiseaseModeling'
-    REPO_NAME = 'idmtools'
-
     def __init__(self, repo_owner=None, repo_name=None):
-        self.repo_owner = repo_owner or self.REPO_OWNER
-        self.repo_name = repo_name or self.REPO_NAME
-        self._path_to_repo = './'
+        self.repo_owner = repo_owner or REPO_OWNER
+        self.repo_name = repo_name or REPO_NAME
+        self._path_to_repo = ''
         self._branch = 'master'
 
     @property
-    def repo_path(self):
+    def repo_url(self):
         return f'https://{self.repo_owner}/{self.repo_name}'
 
     @property
-    def api_path(self):
-        return f'https://api.github.com/repos/{self.repo_owner}/{self.repo_name}/contents/{self._path_to_repo}?ref={self._branch}'
+    def api_url(self):
+        return f'{API_HOME}/repos/{self.repo_owner}/{self.repo_name}/contents/{self._path_to_repo}?ref={self._branch}'
 
     def list_public_repos(self, repo_owner=None, raw=False):
         import requests
 
-        api_url = f'https://api.github.com/users/{repo_owner if repo_owner else self.repo_owner}/repos'
+        api_url = f'{API_HOME}/users/{repo_owner if repo_owner else self.repo_owner}/repos'
 
         resp = requests.get(api_url)
         if resp.status_code != 200:
@@ -45,33 +46,26 @@ class GitRepo:
         else:
             return [r['full_name'] for r in repo_list]
 
-    def download(self, path_to_repo='./', output_dir="./", branch='master'):
+    def download(self, path_to_repo='', output_dir="./", branch='master'):
         """ Downloads the files and directories in repo_url. If flatten is specified, the contents of any and all
          sub-directories will be pulled upwards into the root folder. """
 
         self._path_to_repo = path_to_repo
         self._branch = branch
 
-        # generate the url which returns the JSON data
-        api_url = self.api_path
-
-        # To handle file names.
-        dir_out = output_dir
+        if not os.path.exists(output_dir):
+            raise Exception(f"output_dir does not exist: {output_dir}")
 
         try:
             opener = urllib.request.build_opener()
             opener.addheaders = [('User-agent', 'Mozilla/5.0')]
             urllib.request.install_opener(opener)
-            response = urllib.request.urlretrieve(api_url)  # Retrieve a URL into a temporary location on disk.
+            response = urllib.request.urlretrieve(self.api_url)  # Retrieve a URL into a temporary location on disk.
         except KeyboardInterrupt:
             # when CTRL+C is pressed during the execution of this script,
             # bring the cursor to the beginning, erase the current line, and dont make a new line
             # print_text("✘ Got interrupted", "red", in_place=True)
             sys.exit()
-
-        # make a directory with the name which is taken from
-        # the actual repo
-        os.makedirs(dir_out, exist_ok=True)
 
         # total files count
         total_files = 0
@@ -89,7 +83,7 @@ class GitRepo:
                     opener = urllib.request.build_opener()
                     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
                     urllib.request.install_opener(opener)
-                    urllib.request.urlretrieve(data["download_url"], os.path.join(dir_out, data["name"]))
+                    urllib.request.urlretrieve(data["download_url"], os.path.join(output_dir, data["name"]))
                     # bring the cursor to the beginning, erase the current line, and dont make a new line
                     # print_text("Downloaded: " + Fore.WHITE + "{}".format(data["name"]), "green", in_place=True)
 
@@ -104,9 +98,7 @@ class GitRepo:
                 file_url = file["download_url"]
                 path = file["path"]
 
-                # os.makedirs(os.path.dirname(path), exist_ok=True)   # zdu: bug: not consider dir_out
-                os.makedirs(os.path.dirname(os.path.join(dir_out, path)),
-                            exist_ok=True)  # almost right but add sxtra level githug
+                os.makedirs(os.path.dirname(os.path.join(output_dir, path)), exist_ok=True)
 
                 if file_url is not None:
                     try:
@@ -114,8 +106,7 @@ class GitRepo:
                         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
                         urllib.request.install_opener(opener)
                         # download the file
-                        # urllib.request.urlretrieve(file_url, path)  # zdu: bug: not consider dir_out
-                        urllib.request.urlretrieve(file_url, os.path.join(dir_out, path))
+                        urllib.request.urlretrieve(file_url, os.path.join(output_dir, path))
 
                         # bring the cursor to the beginning, erase the current line, and dont make a new line
                         # print_text("Downloaded: " + Fore.WHITE + "{}".format(file_name), "green", in_place=False,
@@ -128,7 +119,7 @@ class GitRepo:
                         # print_text("✘ Got interrupted", 'red', in_place=False)
                         sys.exit()
                 else:
-                    self.download(path, dir_out, branch)
+                    self.download(path, output_dir, branch)
 
         return total_files
 
