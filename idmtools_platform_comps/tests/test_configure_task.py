fd968255f377ce70d3e3738a586583a3722797aa
Clinton Collins <clinton.collins@gmail.com>
2020-06-18 10:25:17 -0700

    Fix configure task

--- idmtools_platform_comps/tests/test_configure_task.py
+++ idmtools_platform_comps/tests/test_configure_task.py
@@ -9,7 +9,7 @@ from idmtools_models.json_configured_task import JSONConfiguredTask
 class TestExperimentOperations(unittest.TestCase):
     def test_reload_from_simulation_task1(self):
         with Platform("COMPS2") as p:
-            command: str = "python -m json.tool --config my_config.json"
+            command: str = "python -m json.tool my_config.json"
             config_file_name: str = 'my_config.json'
 
             task = JSONConfiguredTask(command=command, config_file_name=config_file_name, parameters=dict(a=1, b=2, c=3))
