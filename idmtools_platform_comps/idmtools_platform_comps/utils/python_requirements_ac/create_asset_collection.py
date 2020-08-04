635d722882fd68f0b07ef42192a7678d463cc855
Clinton Collins <clinton.collins@gmail.com>
2020-08-03 18:36:32 -0700

    Fix print of list of files

--- idmtools_platform_comps/idmtools_platform_comps/utils/python_requirements_ac/create_asset_collection.py
+++ idmtools_platform_comps/idmtools_platform_comps/utils/python_requirements_ac/create_asset_collection.py
@@ -126,7 +126,7 @@ def main():
                 ac2.add_asset(acf)
 
         print("\n\n\n=====================\nUploading files not in comps: " + "\n".join(
-            [f"{a.relative_path}/{a.file_name}" for a in ac2.assets[0:max_files] if a.md5_checksum is None]))
+            [f"{a.relative_path}/{a.file_name}" for a in ac2.assets if a.md5_checksum in missing_files or a.md5_checksum is None]))
 
         sys.stdout.flush()
         ac2.save()
