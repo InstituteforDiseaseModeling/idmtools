57f01fe44c443ae7ca072d58323e9e884b3ae2b3
Clinton Collins <clinton.collins@gmail.com>
2020-08-06 16:41:54 -0700

    Add demo of existing sims

---
+++ examples/cookbook/experiments/add_simulation_to_existing_experiment.py
@@ -0,0 +1,49 @@
+import os
+
+from idmtools.builders import SimulationBuilder
+from idmtools.core.platform_factory import Platform
+from idmtools.entities.experiment import Experiment
+from idmtools.entities.templated_simulation import TemplatedSimulations
+from idmtools_models.python.json_python_task import JSONConfiguredPythonTask
+
+# load up an existing experiment with completed simulations
+with Platform('COMPS2'):
+    # Create First Experiment
+    builder = SimulationBuilder()
+    builder.add_sweep_definition(JSONConfiguredPythonTask.set_parameter_partial("a"),
+                                 [i * i for i in range(5)])
+    model_path = os.path.join("inputs", "python_model_with_deps", "Assets", "model.py")
+    sims_template = TemplatedSimulations(base_task=JSONConfiguredPythonTask(script_path=model_path))
+    sims_template.add_builder(builder=builder)
+
+    experiment = Experiment.from_template(sims_template)
+    experiment.run(wait_until_done=True)
+
+    # create a new sweep for new simulations
+    builder = SimulationBuilder()
+    builder.add_sweep_definition(JSONConfiguredPythonTask.set_parameter_partial("a"),
+                                 [i * i for i in range(6, 10)])
+    sims_template.add_builder(builder=builder)
+    experiment.simulations.extend(sims_template)
+
+    # Changing the Common Assets
+    experiment.assets = experiment.assets.copy()
+    # Add new simulations to the experiment
+    model_path = os.path.join("inputs", "python_model_with_deps", "Assets", "newmodel2.py")
+    sims_template = TemplatedSimulations(base_task=JSONConfiguredPythonTask(script_path=model_path))
+    sims_template.add_builder(builder=builder)
+    experiment.simulations.extend(sims_template)
+
+    # More than one batch of simulations can be added to the experiment before running again
+    model_path = os.path.join("inputs", "python_model_with_deps", "Assets", "newmodel3.py")
+    sims_template = TemplatedSimulations(base_task=JSONConfiguredPythonTask(script_path=model_path))
+    sims_template.add_builder(builder=builder)
+    experiment.simulations.extend(sims_template)
+
+    # If you are adding a large amount of simulations through a Template, it is recommended to do the following to keep generator use and lower memory footprint
+    #
+    # sims_template.add_simulations(experiment.simulations)
+    # experiment.simulations = simulations
+
+    # run all simulations in the experiment that have not run before and wait
+    experiment.run(wait_until_done=True)
