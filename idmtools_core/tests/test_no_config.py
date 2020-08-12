a0fb166a9e538cb8e5627db17cf00ff3ab33c95d
Clinton Collins <clinton.collins@gmail.com>
2020-08-12 09:20:16 -0700

    Add tests for no configuration

---
+++ idmtools_core/tests/test_no_config.py
@@ -0,0 +1,57 @@
+import os
+import tempfile
+import pytest
+from idmtools.config import IdmConfigParser
+from idmtools_test.utils.itest_with_persistence import ITestWithPersistence
+from idmtools.core.platform_factory import Platform
+from idmtools.entities.experiment import Experiment
+
+
+class TestNoConfig(ITestWithPersistence):
+
+    def setUp(self) -> None:
+        # store current work directory
+        super().setUp()
+        IdmConfigParser.clear_instance()
+        self.current_directory = os.getcwd()
+        self.temp_directory = tempfile.TemporaryDirectory()
+        os.chdir(self.temp_directory.name)
+
+    def tearDown(self) -> None:
+        self.temp_directory.cleanup()
+        os.chdir(self.current_directory)
+
+    @pytest.mark.comps
+    def test_success(self):
+
+        sim_root_dir = os.path.join('$COMPS_PATH(USER)', 'output')
+        plat_obj = Platform('COMPS',
+                            endpoint='https://comps.idmod.org',
+                            environment='Calculon',
+                            priority='Normal',
+                            simulation_root=sim_root_dir,
+                            node_group='idm_abcd',
+                            num_cores='1',
+                            num_retries='0',
+                            exclusive='False', missing_ok=True)
+
+        # we don't care if we find experiment, only that the platform initialized
+        try:
+            experiment = Experiment.from_id('a7ea2ac2-a068-ea11-a2c5-c4346bcb1550')
+        except:  # noqa: E722
+            pass
+
+    @pytest.mark.comps
+    def test_failure(self):
+        sim_root_dir = os.path.join('$COMPS_PATH(USER)', 'output')
+        with self.assertRaises(ValueError) as a:
+            plat_obj = Platform('COMPS',
+                                endpoint='https://comps.idmod.org',
+                                environment='Calculon',
+                                priority='Normal',
+                                simulation_root=sim_root_dir,
+                                node_group='idm_abcd',
+                                num_cores='abc',
+                                num_retries='0',
+                                exclusive='False', missing_ok=True)
+            experiment = Experiment.from_id('a7ea2ac2-a068-ea11-a2c5-c4346bcb1550')
