df47ec970ab899e77dba1eb78910c1f5c01173c6
Clinton Collins <clinton.collins@gmail.com>
2020-08-31 12:29:13 -0700

    Removed comment about commission_batch_size

--- idmtools_platform_comps/idmtools_platform_comps/comps_platform.py
+++ idmtools_platform_comps/idmtools_platform_comps/comps_platform.py
@@ -62,8 +62,7 @@ class COMPSPlatform(IPlatform, CacheEnabled):
     batch_size: int = field(default=10, metadata={"help": "How many simulations per batch",
                                                   "validate": partial(validate_range, min=1, max=100)})
     min_time_between_commissions: int = field(default=15, metadata={
-        "help": "How many seconds between commission calls on an experiment. "
-                "Used in conjunction with commission_batch_size to commission simulations in batches",
+        "help": "How many seconds between commission calls on an experiment. ",
         "validate": partial(validate_range, min=10, max=300)})
     exclusive: bool = field(default=False,
                             metadata={"help": "Enable exclusive mode? (one simulation per node on the cluster)"})
