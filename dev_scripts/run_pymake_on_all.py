f194f194a61a426554d72e96d02ca1abd21c9eee
Clinton Collins <clinton.collins@gmail.com>
2020-09-24 13:29:48 -0700

    Remove all make

--- dev_scripts/run_pymake_on_all.py
+++ dev_scripts/run_pymake_on_all.py
@@ -22,9 +22,6 @@ if __name__ == '__main__':
     if which("make"):
         make_command = "make"
     # check for almost make next
-    elif which("almake"):
-        print("almake is currently being tested as a replacement for pymake on Windows. There could be issues. It is recommended you use make or pymake for now.")
-        make_command = "almake"
     elif which("pymake"):
         make_command = "pymake"
     # fallback to os default
