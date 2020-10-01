27807395a96f93554354b3ecceaad0c67c952cd4
Clinton Collins <clinton.collins@gmail.com>
2020-09-30 22:10:33 -0700

    Add verbose option

--- dev_scripts/bootstrap.py
+++ dev_scripts/bootstrap.py
@@ -25,28 +25,6 @@ if sys.platform == "win32" and 'VIRTUAL_ENV' in os.environ:
 # To use simply run
 # python bootstrap.py
 
-logger = getLogger("bootstrap")
-logger.setLevel(logging.DEBUG)
-log_formatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s]  %(message)s")
-file_handler = logging.FileHandler("bootstrap.buildlog")
-file_handler.setFormatter(log_formatter)
-file_handler.setLevel(logging.DEBUG)
-logger.addHandler(file_handler)
-# use colorful logs except the first time
-console_log_level = logging.DEBUG if 'BUILD_DEBUG' in os.environ else logging.INFO
-try:
-    import coloredlogs
-
-    coloredlogs.install(logger=logger, level=console_log_level, fmt="%(asctime)s [%(levelname)-8.8s]  %(message)s")
-    logging.addLevelName(15, 'VERBOSE')
-    logging.addLevelName(35, 'SUCCESS')
-    logging.addLevelName(50, 'CRITICAL')
-except ImportError:
-    console_handler = logging.StreamHandler(stream=sys.stdout)
-    console_handler.setFormatter(log_formatter)
-    console_handler.setLevel(console_log_level)
-    logger.addHandler(console_handler)
-
 base_directory = abspath(join(dirname(__file__), '..'))
 
 default_install = ['test']
@@ -65,6 +43,7 @@ packages = dict(
     idmtools_platform_slurm=data_class_default,
     idmtools_test=[]
 )
+logger = getLogger("bootstrap")
 
 
 def execute(cmd: List['str'], cwd: str = base_directory, ignore_error: bool = False) -> Generator[str, None, None]:
@@ -82,6 +61,7 @@ def execute(cmd: List['str'], cwd: str = base_directory, ignore_error: bool = Fa
     Raises:
         CalledProcessError if the return code was not 0
     """
+    logger.debug(f'Running {" ".join(cmd)}')
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, cwd=cwd)
     for stdout_line in iter(process.stdout.readline, ""):
         yield stdout_line
@@ -137,15 +117,39 @@ def install_base_environment(pip_url):
     for line in execute(["pip", "uninstall", "-y", "py-make"], ignore_error=True):
         process_output(line)
 
-    for line in execute(["pip", "install", "mozilla-pymake", f"--extra-index-url={pip_url}"]):
+    for line in execute(["pip", "install", "mozilla-pymake", f"--index-url={pip_url}"]):
         process_output(line)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Bootstrap the development environment")
     parser.add_argument("--index-url", default='https://packages.idmod.org/api/pypi/pypi-production/simple', help="Pip url to install dependencies from")
+    parser.add_argument("--verbose", default=False, action='store_true')
 
     args = parser.parse_args()
+
+
+    logger.setLevel(logging.DEBUG)
+    log_formatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s]  %(message)s")
+    file_handler = logging.FileHandler("bootstrap.buildlog")
+    file_handler.setFormatter(log_formatter)
+    file_handler.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+    # use colorful logs except the first time
+    console_log_level = logging.DEBUG if 'BUILD_DEBUG' in os.environ or args.verbose else logging.INFO
+    try:
+        import coloredlogs
+
+        coloredlogs.install(logger=logger, level=console_log_level, fmt="%(asctime)s [%(levelname)-8.8s]  %(message)s")
+        logging.addLevelName(15, 'VERBOSE')
+        logging.addLevelName(35, 'SUCCESS')
+        logging.addLevelName(50, 'CRITICAL')
+    except ImportError:
+        console_handler = logging.StreamHandler(stream=sys.stdout)
+        console_handler.setFormatter(log_formatter)
+        console_handler.setLevel(console_log_level)
+        logger.addHandler(console_handler)
+
 
     install_base_environment(args.index_url)
     sys.exit(install_dev_packages(args.index_url))
