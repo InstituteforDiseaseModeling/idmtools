b47871ab1616953f8b287f59f68823b1628d2531
Clinton Collins <clinton.collins@gmail.com>
2020-10-09 14:41:19 -0700

    Fix test so that is doesn't filter out txt files

--- idmtools_platform_comps/tests/test_assetize_output.py
+++ idmtools_platform_comps/tests/test_assetize_output.py
@@ -142,7 +142,7 @@ class TestAssetizeOutput(unittest.TestCase):
 
         with self.subTest("test_workitem_prefix"):
 
-            ao2 = AssetizeOutput(related_work_items=['a6d11db7-1603-e911-80ca-f0921c167866', ao], file_patterns=["*.txt"], verbose=True, work_item_prefix_format_str="{work_item.name}")
+            ao2 = AssetizeOutput(related_work_items=['a6d11db7-1603-e911-80ca-f0921c167866', ao], file_patterns=["**.txt"], exclude_patterns=['WorkOrder.json'], verbose=True, work_item_prefix_format_str="{work_item.name}")
             ac = ao2.run(wait_on_done=True, platform=Platform("COMPS2"))
             self.assertTrue(ao2.succeeded)
             self.assertIsNotNone(ac)
