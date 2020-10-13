80b3b854632f15372c43ed88b9fc1f8c8ed5ecb5
shchen <shchen@idmod.org>
2020-10-13 14:00:05 -0700

    Added example to use ImageBuilderWorker for covasim

---
+++ examples/workitem/image_builder/covasim/create_covasim_sif_ac.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+from idmtools.core.platform_factory import Platform
+from idmtools_platform_comps.ssmt_work_items.comps_workitems import InputDataWorkItem
+
+
+if __name__ == "__main__":
+    platform = Platform('SLURM2')
+    wi = InputDataWorkItem(name=os.path.split(sys.argv[0])[1], tags={'idmtools': 'ImageBuilderWorker'}, work_item_type="ImageBuilderWorker")
+    wi.load_work_order(os.path.join('inputs', 'covasim_wo.json'))
+    wi.run(wait_on_done=True)
