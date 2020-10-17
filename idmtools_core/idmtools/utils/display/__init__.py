5288f258a4b2538a760d3c25641b7c800839fe8b
Clinton Collins <clinton.collins@gmail.com>
2020-10-16 19:38:49 -0700

    Remove unused files

--- idmtools_core/idmtools/utils/display/__init__.py
+++
@@ -1,7 +0,0 @@
-from idmtools.utils.display.settings import *  # noqa: F401, F403
-from idmtools.utils.display.displays import *  # noqa: F401, F403
-
-
-def display(obj, settings):
-    for setting in settings:
-        print(setting.display(obj))
