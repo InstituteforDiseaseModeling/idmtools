0fc339ea09931ee92fbe5f00eafac6458bb8dca1
Clinton Collins <clinton.collins@gmail.com>
2020-10-23 10:39:24 -0700

    Updates from comments

--- idmtools_platform_comps/idmtools_platform_comps/utils/package_version.py
+++ idmtools_platform_comps/idmtools_platform_comps/utils/package_version.py
@@ -43,6 +43,10 @@ def get_latest_package_version_from_pypi(pkg_name, display_all=False):
         return None
 
     all_releases = sorted(releases, key=parse_version, reverse=True)
+
+    if display_all:
+        print(all_releases)
+
     return all_releases
 
 
@@ -101,9 +105,10 @@ def fetch_versions_from_server(pkg_url: str) -> List[str]:
     return all_releases
 
 
+@functools.lru_cache(3)
 def get_latest_version_from_site(pkg_url, base_version: Optional[str] = None, display_all=False):
     """
-    Fetch all versions from server
+    Utility to get the latest version for a given package name
 
     Args:
         pkg_url: package name given
@@ -124,9 +129,10 @@ def get_latest_version_from_site(pkg_url, base_version: Optional[str] = None, di
 
     if base_version:
         # only use the longest match latest
-        version_compatible_portion = base_version[:3]
+        version_compatible_portion = ".".join(base_version.split(".")[:2])
+
         for ver in release_versions:
-            if ver[:3] == version_compatible_portion:
+            if ".".join(ver.split('.')[:2]) == version_compatible_portion:
                 return ver
         return None
     else:
