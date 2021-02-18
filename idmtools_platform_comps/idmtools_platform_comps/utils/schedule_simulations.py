fdc64bd0617d730fdb3d0ca0e133e42f2c5802fb
zdu <zdu@idmod.org>
2021-02-18 13:53:57 -0800

    add utility file to support COMPS scheduling

---
+++ idmtools_platform_comps/idmtools_platform_comps/utils/schedule_simulations.py
@@ -0,0 +1,96 @@
+import json
+from os import PathLike
+from typing import List, Union
+from idmtools.assets import Asset
+from idmtools.entities.experiment import Experiment
+from idmtools.entities.simulation import Simulation
+from idmtools.entities.templated_simulation import TemplatedSimulations
+from logging import DEBUG
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def _add_work_order_asset(_simulation: Simulation, _file_name: str = "WorkOrder.json",
+                          _file_path: Union[str, PathLike] = "./WorkOrder.json", _update: bool = True):
+
+    with open(str(_file_path), "r") as jsonFile:
+        data = json.loads(jsonFile.read())
+
+    if _update:
+        data["Command"] = _simulation.task.command.cmd
+
+    _simulation.task.transient_assets.add_asset(Asset(filename=_file_name, content=json.dumps(data)))
+
+    setattr(_simulation, 'scheduling', True)
+
+
+def add_work_order(item: Union[Experiment, Simulation, TemplatedSimulations], file_name: str = "WorkOrder.json",
+                   file_path: Union[str, PathLike] = "./WorkOrder.json", update: bool = True,
+                   scheduling: bool = True):
+    if isinstance(item, Simulation):
+        _add_work_order_asset(item, _file_name=file_name, _file_path=file_path, _update=update)
+    elif isinstance(item, TemplatedSimulations):
+        _add_work_order_asset(item.base_simulation, _file_name=file_name, _file_path=file_path, _update=update)
+    elif isinstance(item, Experiment):
+        if isinstance(item.simulations.items, TemplatedSimulations):
+            if len(item.simulations.items) == 0:
+                raise ValueError("You cannot run an empty experiment")
+            if logger.isEnabledFor(DEBUG):
+                logger.debug("Using Base task from template for WorkOrder.json assets")
+            _add_work_order_asset(item.simulations.items.base_simulation, _file_name=file_name, _file_path=file_path,
+                                  _update=update)
+            for sim in item.simulations.items.extra_simulations():
+                _add_work_order_asset(sim, _file_name=file_name, _file_path=file_path, _update=update)
+        elif isinstance(item.simulations.items, List):
+            if len(item.simulations.items) == 0:
+                raise ValueError("You cannot run an empty experiment")
+            if logger.isEnabledFor(DEBUG):
+                logger.debug("Using all tasks to gather assets")
+            for sim in item.simulations.items:
+                _add_work_order_asset(sim, _file_name=file_name, _file_path=file_path, _update=update)
+        elif isinstance(item.simulations.items, List) and len(item.simulations.items) == 0:
+            raise ValueError("You cannot run an empty experiment")
+    else:
+        raise ValueError("The method only support object type: Experiment, Simulation, TemplatedSimulations!")
+
+
+def _add_schedule_config_asset(simulation: Simulation, _config: dict, _update: bool = True):
+    if _update:
+        _config["Command"] = simulation.task.command.cmd
+
+    ctn = json.dumps(_config, indent=3)
+    simulation.task.transient_assets.add_asset(Asset(filename="Work_order.json", content=ctn))
+
+
+def add_schedule_config(item: Union[Experiment, Simulation, TemplatedSimulations], command: str = None,
+                        node_group_name: str = 'idm_cd', num_cores: int = 1, update: bool = True, **config_opts):
+    config = dict(Command=command, NodeGroupName=node_group_name, NumCores=num_cores)
+    config.update(config_opts)
+
+    if isinstance(item, Simulation):
+        _add_schedule_config_asset(item, _config=config, _update=update)
+    elif isinstance(item, TemplatedSimulations):
+        _add_schedule_config_asset(item.base_simulation, _config=config, _update=update)
+    elif isinstance(item, Experiment):
+        if isinstance(item.simulations.items, TemplatedSimulations):
+            if len(item.simulations.items) == 0:
+                raise ValueError("You cannot run an empty experiment")
+            if logger.isEnabledFor(DEBUG):
+                logger.debug("Using Base task from template for WorkOrder.json assets")
+            _add_schedule_config_asset(item.simulations.items.base_simulation, _config=config, _update=update)
+            for sim in item.simulations.items.extra_simulations():
+                _add_schedule_config_asset(sim, _config=config, _update=update)
+        elif isinstance(item.simulations.items, List):
+            if len(item.simulations.items) == 0:
+                raise ValueError("You cannot run an empty experiment")
+            if logger.isEnabledFor(DEBUG):
+                logger.debug("Using all tasks to gather assets")
+            for sim in item.simulations.items:
+                _add_schedule_config_asset(sim, _config=config, _update=update)
+        elif isinstance(item.simulations.items, List) and len(item.simulations.items) == 0:
+            raise ValueError("You cannot run an empty experiment")
+    else:
+        raise ValueError("The method only support object type: Experiment, Simulation, TemplatedSimulations!")
+
+
