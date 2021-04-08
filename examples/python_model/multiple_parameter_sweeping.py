66922ebb1cfdcc03352d96c33b1518f3866a2626
shchen <shchen@idmod.org>
2021-04-08 12:48:44 -0700

    add examples for multiple_parameter_sweeps

--- examples/python_model/multiple_parameter_sweeping.py
+++ examples/python_model/multiple_parameter_sweeping.py
@@ -10,13 +10,12 @@ from idmtools.entities.templated_simulation import TemplatedSimulations
 from idmtools_platform_comps.utils.scheduling import default_add_workerorder_sweep_callback
 
 
-# Update each sweep parameter in simulation and add to command line argument to command
-def set_value(simulation, name, value):
-    fix_value = round(value, 2) if isinstance(value, float) else value
-    # add argument
-    simulation.task.command.add_raw_argument(fix_value)
-    # add tag with our value
-    simulation.tags[name] = fix_value
+def update_parameter_callback(simulation, pop_size, pop_infected, n_days, rand_seed):
+    simulation.task.command.add_argument(pop_size)
+    simulation.task.command.add_argument(pop_infected)
+    simulation.task.command.add_argument(n_days)
+    simulation.task.command.add_argument(rand_seed)
+    return {"pop_size": pop_size, "pop_infected": pop_infected, "n_days": n_days, "rand_seed": rand_seed}
 
 
 # create command line
@@ -26,18 +25,18 @@ task = CommandTask(command=command)
 ts = TemplatedSimulations(base_task=task)
 
 sb = SimulationBuilder()
-sb.add_sweep_definition(partial(set_value, name="pop_size"), [10000, 20000])
-sb.add_sweep_definition(partial(set_value, name="pop_infected"), [10, 100])
-sb.add_sweep_definition(partial(set_value, name="n_days"), [100, 110])
-sb.add_sweep_definition(partial(set_value, name="rand_seed"), [1234, 4567])
+sb.add_multiple_parameter_sweep_definition(update_parameter_callback, pop_size=[10000, 20000], pop_infected=[10, 100],
+                                           n_days=[100, 110], rand_seed=[1234, 4567])
+# has to add workorder.json with simulation to update cmd arguments
+# for command line task sweep with workorder, DO NOT use since it will not update arguments:
+# add_work_order(experiment, file_name="WorkOrder.json", file_path=os.path.join("inputs", "scheduling", "WorkOrder_orig.json"))
+
 sb.add_sweep_definition(partial(default_add_workerorder_sweep_callback, file_name="WorkOrder.json"),
                         os.path.join("inputs", "scheduling", "WorkOrder_orig.json"))
-
 ts.add_builder(sb)
 
 experiment = Experiment.from_template(ts, name=os.path.split(sys.argv[0])[1])
 experiment.add_asset(os.path.join("inputs", "scheduling", "commandline_model.py"))
-
 with Platform('CALCULON') as platform:
     experiment.run(wait_on_done=True, scheduling=True)
     # use system status as the exit code
