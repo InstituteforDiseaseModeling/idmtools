08f61dfc60c5e3e5bc5d8ef4b38827f38d3c6e71
shchen <shchen@idmod.org>
2021-09-02 09:44:07 -0700

    update

--- examples/cookbook/download/download_files_from_comps.py
+++ examples/cookbook/download/download_files_from_comps.py
@@ -1,6 +1,6 @@
 # Example to download files to local from COMPS with DownloadWorkItem
-# in this example, it downloads all files from COMPS simulation's 'output' folder. you can filter specific files with
-# file_patterns, for example: file_patterns=["output/*.csv"] to only download csv files from output dir
+# In this example, it downloads all files from COMPS simulation's 'output' folder. You can filter specific files with
+# file_patterns, for example: file_patterns=["output/*.csv"] to only download csv files from 'output' directory.
 
 # You can also download the same files from COMPS with idmtools cli command under current dir. i.e:
 # idmtools comps CALCULON  download --experiment 7a7b7c10-f1f3-eb11-a9ed-b88303911bc1 --name example_test --output-path output_dir --pattern output\** --no-delete-after-download --extract-after-download
@@ -13,11 +13,10 @@ from idmtools_platform_comps.utils.download.download import DownloadWorkItem
 
 platform = Platform("CALCULON")
 cwd = os.getcwd()
-os.makedirs(os.path.join(cwd, "output_dir"), exist_ok=True)
 dl_wi = DownloadWorkItem(name=os.path.split(sys.argv[0])[1],
                          related_experiments=['7a7b7c10-f1f3-eb11-a9ed-b88303911bc1'],
                          file_patterns=["output/**"], delete_after_download=False,
-                         simulation_prefix_format_str='{simulation.id}', verbose=True,
+                         verbose=True,
                          output_path=os.path.join(cwd, "output_dir"),
                          )
 dl_wi.run(wait_on_done=True, platform=platform)
