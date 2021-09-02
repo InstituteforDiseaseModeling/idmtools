031a00e840bf5c43d2150cb7f34cd2569d87a232
shchen <shchen@idmod.org>
2021-09-02 10:23:31 -0700

    update

--- examples/cookbook/download/download_files_from_comps.py
+++ examples/cookbook/download/download_files_from_comps.py
@@ -12,11 +12,10 @@ from idmtools_core.idmtools.core.platform_factory import Platform
 from idmtools_platform_comps.utils.download.download import DownloadWorkItem
 
 platform = Platform("CALCULON")
-cwd = os.getcwd()
 dl_wi = DownloadWorkItem(name=os.path.split(sys.argv[0])[1],
                          related_experiments=['7a7b7c10-f1f3-eb11-a9ed-b88303911bc1'],
                          file_patterns=["output/**"], delete_after_download=False,
                          verbose=True,
-                         output_path=os.path.join(cwd, "output_dir"),
+                         output_path=os.path.join(os.getcwd(), "output_dir"),
                          )
 dl_wi.run(wait_on_done=True, platform=platform)
