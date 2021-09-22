f429daa492a6fcf8a81928e364978ad3d0d56976
zdu <zdu@idmod.org>
2021-09-22 07:02:14 -0700

    update SSMTPlatformSimulationOperations

--- idmtools_platform_comps/idmtools_platform_comps/ssmt_operations/simulation_operations.py
+++ idmtools_platform_comps/idmtools_platform_comps/ssmt_operations/simulation_operations.py
@@ -2,9 +2,16 @@
 
 Copyright 2021, Bill & Melinda Gates Foundation. All rights reserved.
 """
-from typing import List, Dict
+from uuid import UUID
+from typing import List, Dict, Optional
 from idmtools.entities.simulation import Simulation
 from idmtools_platform_comps.comps_operations.simulation_operations import CompsPlatformSimulationOperations
+from COMPS.Data.Simulation import Simulation as COMPSSimulation
+from COMPS.Data import QueryCriteria
+from logging import getLogger, DEBUG
+from pathlib import Path
+
+logger = getLogger(__name__)
 
 
 class SSMTPlatformSimulationOperations(CompsPlatformSimulationOperations):
@@ -13,6 +20,33 @@ class SSMTPlatformSimulationOperations(CompsPlatformSimulationOperations):
 
     In this case, we only have to redefine get_assets to optimize file usage.
     """
+
+    def get(self, simulation_id: UUID, columns: Optional[List[str]] = None, load_children: Optional[List[str]] = None,
+            query_criteria: Optional[QueryCriteria] = None, **kwargs) -> COMPSSimulation:
+        """
+        Get Simulation from Comps.
+
+        Args:
+            simulation_id: ID
+            columns: Optional list of columns to load. Defaults to "id", "name", "experiment_id", "state"
+            load_children: Optional children to load. Defaults to "tags", "configuration"
+            query_criteria: Optional query_criteria object to use your own custom criteria object
+            **kwargs:
+
+        Returns:
+            COMPSSimulation
+        """
+        # ensure hpc_jobs is in children
+        if load_children:
+            load_children.append('hpc_jobs')
+        # when query criteria is specified, we need to ensure our desired criteria are followed
+        elif query_criteria is not None:
+            if 'hpc_jobs' not in query_criteria._children:
+                query_criteria._children.append('hpc_jobs')
+        else:
+            load_children = ['hpc_jobs']
+
+        return super().get(simulation_id, load_children=load_children, query_criteria=query_criteria)
 
     def get_assets(self, simulation: Simulation, files: List[str], **kwargs) -> Dict[str, bytearray]:
         """
@@ -26,4 +60,19 @@ class SSMTPlatformSimulationOperations(CompsPlatformSimulationOperations):
         Returns:
             Files fetched
         """
-        raise NotImplementedError("Fetching files for simulation is not currently implemented for SSMT")
+        po: COMPSSimulation = simulation.get_platform_object(
+            load_children=["files", "configuration", "hpc_jobs"])
+        working_directory = po.hpc_jobs[0].working_directory
+        results = dict()
+        for file in files:
+            full_path = Path(working_directory).joinpath(file)
+            if not full_path.exists():
+                msg = f"Cannot find the file {file} at {full_path}"
+                logger.error(msg)
+                raise FileNotFoundError(msg)
+            if logger.isEnabledFor(DEBUG):
+                logger.debug(full_path)
+            with open(full_path, 'rb') as fin:
+                results[file] = fin.read()
+        return results
+
