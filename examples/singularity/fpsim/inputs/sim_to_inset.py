414fba49533576dad98874f562cf4ea2449ba1ed
CWiswell-IDM <cwiswell@idmod.org>
2021-10-21 15:49:55 -0700

    Fixing image generation and enabling insetchart view

--- examples/singularity/fpsim/inputs/sim_to_inset.py
+++ examples/singularity/fpsim/inputs/sim_to_inset.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 
-def create_insetchart(sim_json, n=0):
-    sim_results = { k: sim_json['results'][k] for k in sim_json['results']['timeseries_keys']}
+def create_insetchart(results_json, n=0):
+    sim_results = {k: results_json[k] for k in ['births', 'maternal_deaths', 'infant_deaths','t','on_method','total_women_fecund']}
     df = pd.DataFrame.from_dict(sim_results)
     if n == 0:
         df.to_csv('InsetChart.csv', index=True)
--- examples/singularity/fpsim/inputs/simple.py
+++ examples/singularity/fpsim/inputs/simple.py
@@ -4,6 +4,8 @@ Simple example usage for FPsim
 
 import fpsim as fp
 import fp_analyses.senegal_parameters as sp
+import sciris as sc
+import sim_to_inset
 
 # Set options
 do_plot = True
@@ -14,6 +16,8 @@ pars['exposure_correction'] = 1.0 # Overall scale factor on probability of becom
 
 sim = fp.Sim(pars=pars)
 sim.run()
+sim_to_inset.create_insetchart(sc.jsonify(sim.results))
+
 
 if do_plot:
     sim.plot()
