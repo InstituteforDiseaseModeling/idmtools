c36afbadc0ffe349ff9dd4f6f920fea118380507
CWiswell-IDM <cwiswell@idmod.org>
2021-11-03 10:42:45 -0700

    Clean up comments

--- examples/singularity/poliosim/inputs/sim_to_inset.py
+++ examples/singularity/poliosim/inputs/sim_to_inset.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import sciris as sc
 
 channels = [
     'n_naive',
@@ -15,31 +14,9 @@ channels = [
     'doubling_time'
 ]
 
-'''
-    "new_infections": 0.0,
-    "new_sus_para_infs": 0.0,
-    "new_tests": 2.0,
-    "new_diagnoses": 0.0,
-    "new_recoveries": 0.0,
-    "new_symptomatic": 0.0,
-    "new_quarantined": 0.0,
-    "new_symp_triggered_surveilled": 0.0,
-'''
+
 def create_insetchart(results, n=0):
     sim_results = {k: results[k].values.tolist() for k in channels}
-
-    # TODO: remove this commented out loop. Here for debugging help.
-    # sim_results = {}
-    # for k in channels:
-    #     print(k)
-    #     channel = results[k]
-    #     print(channel)
-    #     values = channel.values
-    #     print(values)
-    #     values_list = values.tolist()
-    #     sim_results[k] = values_list
-    # sim_results_json = sc.jsonify(sim_results)
-    # sim_results_json['t'] = results['t']
 
     sim_results['t'] = results['t']
     df = pd.DataFrame.from_dict(sim_results)
