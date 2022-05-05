f4d58e45217e257f928921005999ffe4db549d26
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2022-05-05 11:07:37 -0700

    Update output path

--- examples/analyzers/example_analysis_self_parse_analyzer.py
+++ examples/analyzers/example_analysis_self_parse_analyzer.py
@@ -15,8 +15,13 @@ from idmtools.entities import IAnalyzer
 
 
 class MyCSVAnalyzer(IAnalyzer):
-    def __init__(self, filenames, parse=True):
+    def __init__(self, filenames, parse=True, output_path="output"):
         super().__init__(parse=parse, filenames=filenames)
+        self.output_path = output_path
+
+    def initialize(self):
+        self.output_path = os.path.join(self.working_dir, self.output_path)
+        os.makedirs(self.output_path, exist_ok=True)
 
     def map(self, data, simulation) -> pd.DataFrame:
         # parse csv file ourself, 'data' contains a key as filename and value as csv byte string
@@ -33,7 +38,7 @@ class MyCSVAnalyzer(IAnalyzer):
         # Make a directory labeled the exp id to write the csv results to
         first_sim = list(all_data.keys())[0]  # get first Simulation
         exp_id = first_sim.experiment.id  # get experiment id from all_data
-        output_folder = os.path.join(exp_id)
+        output_folder = os.path.join(self.output_path, exp_id)
         os.makedirs(output_folder, exist_ok=True)
         results.to_csv(os.path.join(output_folder, self.__class__.__name__ + '.csv'))
 
