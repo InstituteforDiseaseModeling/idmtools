f5ed0ca2006d7585123e41c475f1b908799716ff
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2022-05-13 13:17:15 -0700

    update

--- examples/ssmt/partial_analyzer/run_ssmt_analysis.py
+++ examples/ssmt/partial_analyzer/run_ssmt_analysis.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     analysis = PlatformAnalysis(
         platform=platform, experiment_ids=["76c4f919-f0d2-ec11-a9f8-b88303911bc1"],
         analyzers=[MyCSVAnalyzer], analyzers_args=[{'filenames': ['output/a.csv'], 'output_path': 'output'}],
-        analysis_name="SSMT Analysis Simple 1",
+        analysis_name="SSMT partial analyzer",
         # Only analyzer succeed sims
         extra_args=dict(partial_analyze_ok=True)
     )
