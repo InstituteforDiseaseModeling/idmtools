b8a55ff0d8e5beeac8616b88becb20d328151887
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2022-05-16 15:51:39 -0700

    add mkdir tests

--- idmtools_platform_slurm/tests/test_slurm_platform.py
+++ idmtools_platform_slurm/tests/test_slurm_platform.py
@@ -1,4 +1,10 @@
 import os
+import shutil
+import tempfile
+from uuid import UUID, uuid4
+
+import pytest
+
 from idmtools.core.platform_factory import Platform
 from idmtools.entities import Suite
 from idmtools.entities.experiment import Experiment
@@ -7,7 +13,9 @@ from idmtools_platform_slurm.slurm_operations import LocalSlurmOperations
 
 from idmtools_test.utils.itest_with_persistence import ITestWithPersistence
 
+cwd = os.path.dirname(__file__)
 
+@pytest.mark.smoke
 class TestPythonSimulation(ITestWithPersistence):
 
     def setUp(self) -> None:
@@ -33,7 +41,6 @@ class TestPythonSimulation(ITestWithPersistence):
         suite = Suite()
         local = LocalSlurmOperations(platform=self.platform)
         actual_entity_path = local.get_entity_dir(suite)
-        cwd = os.path.dirname(__file__)
         expected_path = os.path.join(cwd, suite.id)
         self.assertEqual(str(actual_entity_path), expected_path)
 
@@ -44,7 +51,6 @@ class TestPythonSimulation(ITestWithPersistence):
         experiment.parent = suite
         local = LocalSlurmOperations(platform=self.platform)
         actual_entity_path = local.get_entity_dir(experiment)
-        cwd = os.path.dirname(__file__)
         expected_path = os.path.join(cwd, suite.id, experiment.id)
         self.assertEqual(str(actual_entity_path), expected_path)
 
@@ -58,7 +64,6 @@ class TestPythonSimulation(ITestWithPersistence):
         exp3 = Experiment()
         exp3.parent = suite
         local = LocalSlurmOperations(platform=self.platform)
-        cwd = os.path.dirname(__file__)
         actual_suite_path = local.get_entity_dir(suite)
         expected_suite_path = os.path.join(cwd, suite.id)
         self.assertEqual(str(actual_suite_path), expected_suite_path)
@@ -84,7 +89,6 @@ class TestPythonSimulation(ITestWithPersistence):
         simulation.parent = experiment
         local = LocalSlurmOperations(platform=self.platform)
         actual_entity_path = local.get_entity_dir(simulation)
-        cwd = os.path.dirname(__file__)
         expected_path = os.path.join(cwd, suite.id, experiment.id, simulation.id)
         self.assertEqual(str(actual_entity_path), expected_path)
 
@@ -96,3 +100,54 @@ class TestPythonSimulation(ITestWithPersistence):
             actual_entity_path = local.get_entity_dir(simulation)
         self.assertEqual(ex.exception.args[0], "Simulation missing parent!")
 
+    # create dir if no dest defined
+    def test_localSlurmOperations_make_dir_no_dest_suite(self):
+        suite = Suite()
+        local = LocalSlurmOperations(platform=self.platform)
+        local.mk_directory(suite)
+        expected_dir = os.path.join(cwd, suite.id)
+        self.assertTrue(os.path.isdir(expected_dir))
+        # delete dir after test
+        shutil.rmtree(expected_dir)
+        self.assertFalse(os.path.isdir(expected_dir))
+
+    # create dir with dest defined
+    def test_localSlurmOperations_make_dir_dest_suite(self):
+        suite = Suite()
+        local = LocalSlurmOperations(platform=self.platform)
+        dest_dir = tempfile.mkdtemp()
+        local.mk_directory(suite, dest=dest_dir)
+        expected_dir = os.path.join(dest_dir)
+        self.assertTrue(os.path.isdir(expected_dir))
+
+        # make sure we do not create suite id folder under dest folder in this case
+        self.assertFalse(os.path.isdir(os.path.join(dest_dir, suite.id)))
+
+    # test mk_directory method without pass item and dest parameters
+    def test_localSlurmOperations_make_dir_no_params(self):
+        local = LocalSlurmOperations(platform=self.platform)
+        with self.assertRaises(RuntimeError) as ex:
+            local.mk_directory()
+        self.assertEqual(ex.exception.args[0], "Only support Suite/Experiment/Simulation or not None dest.")
+
+    # create dirs for suite/experiment/simulations
+    def test_localSlurmOperations_make_dir_dest_suite(self):
+        local = LocalSlurmOperations(platform=self.platform)
+        suite = Suite()
+        experiment = Experiment()
+        experiment.parent = suite
+        simulation1 = Simulation(_uid=uuid4())  # create uniq simulation
+        simulation2 = Simulation(_uid=uuid4())
+        simulation1.parent = experiment
+        simulation2.parent = experiment
+        local.mk_directory(simulation1)
+        local.mk_directory(simulation2)
+        expected_dir1 = os.path.join(cwd, suite.id, experiment.id, simulation1.id)
+        expected_dir2 = os.path.join(cwd, suite.id, experiment.id, simulation2.id)
+        self.assertTrue(os.path.isdir(expected_dir1))
+        self.assertTrue(os.path.isdir(expected_dir2))
+        # delete dir after test
+        shutil.rmtree(expected_dir1)
+        self.assertFalse(os.path.isdir(expected_dir1))
+        shutil.rmtree(expected_dir2)
+        self.assertFalse(os.path.isdir(expected_dir2))
