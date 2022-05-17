fe287aed61c0c05d77b6a02b863542ad04163c30
zdu <zhaowei.du@gatesfoundation.org>
2022-05-17 15:16:50 -0700

    Code cleanup: removed not-used code or import

--- idmtools_platform_slurm/idmtools_platform_slurm/slurm_operations.py
+++ idmtools_platform_slurm/idmtools_platform_slurm/slurm_operations.py
@@ -4,11 +4,8 @@ Here we implement the SlurmPlatform operations.
 Copyright 2021, Bill & Melinda Gates Foundation. All rights reserved.
 """
 import os
-import copy
-import dataclasses
 from enum import Enum
 from pathlib import Path
-from datetime import datetime
 from logging import getLogger
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
@@ -24,77 +21,6 @@ SIMULATION_SH_FILE = '_run.sh'
 EXPERIMENT_SH_FILE = 'job_submit.sh'
 
 logger = getLogger(__name__)
-
-
-def asdict(obj, *, dict_factory=dict):
-    """Return the fields of a dataclass instance as a new dictionary mapping
-    field names to field values.
-
-    Example usage:
-
-      @dataclass
-      class C:
-          x: int
-          y: int
-
-      c = C(1, 2)
-      assert asdict(c) == {'x': 1, 'y': 2}
-
-    If given, 'dict_factory' will be used instead of built-in dict.
-    The function applies recursively to field values that are
-    dataclass instances. This will also look into built-in containers:
-    tuples, lists, and dicts.
-    """
-    if not dataclasses.is_dataclass(obj):
-        raise TypeError("asdict() should be called on dataclass instances")
-    return _asdict_inner(obj, dict_factory)
-
-
-def _asdict_inner(obj, dict_factory):
-    if dataclasses.is_dataclass(obj):
-        result = []
-        for f in dataclasses.fields(obj):
-            if 'pickle_function' in f.metadata:
-                value = _asdict_inner(f.metadata['pickle_function'](getattr(obj, f.name)), dict_factory)
-            elif 'pickle_ignore' in f.metadata and f.metadata['pickle_ignore']:
-                pass
-            else:
-                value = _asdict_inner(getattr(obj, f.name), dict_factory)
-            result.append((f.name, value))
-        return dict_factory(result)
-    elif isinstance(obj, tuple) and hasattr(obj, '_fields'):
-        # obj is a namedtuple.  Recurse into it, but the returned
-        # object is another namedtuple of the same type.  This is
-        # similar to how other list- or tuple-derived classes are
-        # treated (see below), but we just need to create them
-        # differently because a namedtuple's __init__ needs to be
-        # called differently (see bpo-34363).
-
-        # I'm not using namedtuple's _asdict()
-        # method, because:
-        # - it does not recurse in to the namedtuple fields and
-        #   convert them to dicts (using dict_factory).
-        # - I don't actually want to return a dict here.  The the main
-        #   use case here is json.dumps, and it handles converting
-        #   namedtuples to lists.  Admittedly we're losing some
-        #   information here when we produce a json list instead of a
-        #   dict.  Note that if we returned dicts here instead of
-        #   namedtuples, we could no longer call asdict() on a data
-        #   structure where a namedtuple was used as a dict key.
-
-        return type(obj)(*[_asdict_inner(v, dict_factory) for v in obj])
-    elif isinstance(obj, (list, tuple)):
-        # Assume we can create an object of this type by passing in a
-        # generator (which is not true for namedtuples, handled
-        # above).
-        return type(obj)(_asdict_inner(v, dict_factory) for v in obj)
-    elif isinstance(obj, dict):
-        return type(obj)((_asdict_inner(k, dict_factory),
-                          _asdict_inner(v, dict_factory))
-                         for k, v in obj.items())
-    else:
-        return copy.deepcopy(obj)
-
 
 SLURM_STATES = dict(
     BOOT_FAIL=EntityStatus.FAILED,
