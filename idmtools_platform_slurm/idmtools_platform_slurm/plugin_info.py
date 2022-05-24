7f2bb0414d661e26ce5e13732041ed21bfb7d4ef
zdu <zhaowei.du@gatesfoundation.org>
2022-05-24 13:58:56 -0700

    Fix Path issue

--- idmtools_platform_slurm/idmtools_platform_slurm/plugin_info.py
+++ idmtools_platform_slurm/idmtools_platform_slurm/plugin_info.py
@@ -67,7 +67,7 @@ class SlurmPlatformSpecification(PlatformSpecification):
         config_aliases = dict(
             SLURM_LOCAL=dict(
                 mode="local",
-                job_directory=Path.cwd()
+                job_directory=str(Path.cwd())
             )
         )
         return config_aliases
