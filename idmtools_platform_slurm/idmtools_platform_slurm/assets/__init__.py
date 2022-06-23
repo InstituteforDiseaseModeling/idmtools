16d874ac702c1c2e057e3164002611926a30f287
zdu <zhaowei.du@gatesfoundation.org>
2022-06-23 06:05:02 -0700

    Add docstrings

--- idmtools_platform_slurm/idmtools_platform_slurm/assets/__init__.py
+++ idmtools_platform_slurm/idmtools_platform_slurm/assets/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from typing import TYPE_CHECKING, Optional, Union
 from jinja2 import Template
+from typing import TYPE_CHECKING, Optional, Union
 from idmtools.entities.experiment import Experiment
 
 if TYPE_CHECKING:
@@ -10,7 +10,18 @@ DEFAULT_TEMPLATE_FILE = Path(__file__).parent.joinpath("sbatch.sh.jinja2")
 
 
 def generate_script(platform: 'SlurmPlatform', experiment: Experiment, max_running_jobs: Optional[int] = None,
-                    template: Union[Path, str] = DEFAULT_TEMPLATE_FILE, **kwargs):
+                    template: Union[Path, str] = DEFAULT_TEMPLATE_FILE, **kwargs) -> None:
+    """
+    Generate batch file sbatch.sh
+    Args:
+        platform: Slurm Platform
+        experiment: idmtools Experiment
+        max_running_jobs: int
+        template: template to be used to build batch file
+        kwargs: keyword arguments used to expand functionality
+    Returns:
+        None
+    """
     from idmtools_platform_slurm.slurm_platform import CONFIG_PARAMETERS
     template_vars = dict(njobs=experiment.simulation_count)
     # populate from our platform config vars
@@ -40,7 +51,16 @@ def generate_script(platform: 'SlurmPlatform', experiment: Experiment, max_runni
     platform._op_client.update_script_mode(output_target)
 
 
-def generate_simulation_script(platform: 'SlurmPlatform', simulation, retries: Optional[int] = None):
+def generate_simulation_script(platform: 'SlurmPlatform', simulation, retries: Optional[int] = None) -> None:
+    """
+    Generate batch file _run.sh
+    Args:
+        platform: Slurm Platform
+        simulation: idmtools Simulation
+        retries: int
+    Returns:
+        None
+    """
     sim_script = platform._op_client.get_directory(simulation).joinpath("_run.sh")
     with open(sim_script, "w") as tout:
         with open(Path(__file__).parent.parent.joinpath("assets/_run.sh.jinja2")) as tin:
