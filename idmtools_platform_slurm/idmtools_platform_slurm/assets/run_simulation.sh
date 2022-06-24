6b8726bdc71252ad4e912b9ac484353a66aa79b5
zdu <zhaowei.du@gatesfoundation.org>
2022-06-24 06:42:45 -0700

    Update script process

--- idmtools_platform_slurm/idmtools_platform_slurm/assets/run_simulation.sh
+++ idmtools_platform_slurm/idmtools_platform_slurm/assets/run_simulation.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 JOB_DIRECTORY=$(find . -type d -maxdepth 1 -mindepth 1  | grep -v Assets | head -${SLURM_ARRAY_TASK_ID} | tail -1)
-$JOB_DIRECTORY/_run.sh
+cd $JOB_DIRECTORY
+srun _run.sh
