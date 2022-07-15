14f6f36306f941e9437e00ac691012c9eda3efc2
zdu <zhaowei.du@gatesfoundation.org>
2022-07-15 12:10:02 -0700

    Add docstring

--- idmtools_platform_slurm/idmtools_platform_slurm/platform_operations/json_metadata_operations.py
+++ idmtools_platform_slurm/idmtools_platform_slurm/platform_operations/json_metadata_operations.py
@@ -5,7 +5,7 @@ Copyright 2021, Bill & Melinda Gates Foundation. All rights reserved.
 """
 import json
 from pathlib import Path
-from typing import Dict, List, Type, Union
+from typing import TYPE_CHECKING, Dict, List, Type, Union
 from dataclasses import dataclass, field
 from idmtools.core import ItemType
 from idmtools.core.interfaces import imetadata_operations
@@ -14,10 +14,13 @@ from idmtools.entities.experiment import Experiment
 from idmtools.entities.simulation import Simulation
 from idmtools.utils.json import IDMJSONEncoder
 
+if TYPE_CHECKING:
+    from idmtools_platform_slurm.slurm_platform import SlurmPlatform
+
 
 @dataclass
 class JSONMetadataOperations(imetadata_operations.IMetadataOperations):
-    platform: 'platform'  # noqa: F821
+    platform: 'SlurmPlatform'  # noqa: F821
     platform_type: Type = field(default=None)
     metadata_filename: str = field(default='metadata.json')
 
