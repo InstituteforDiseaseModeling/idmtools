0e1a655542628aa59bf703366a404d090acadb53
Clinton Collins <clinton.collins@gmail.com>
2022-08-24 09:37:31 -0700

    Fix default paths

--- idmtools_platform_slurm/idmtools_platform_slurm/slurm_operations/bridged_operations.py
+++ idmtools_platform_slurm/idmtools_platform_slurm/slurm_operations/bridged_operations.py
@@ -14,10 +14,10 @@ from idmtools_platform_slurm.slurm_operations.local_operations import LocalSlurm
 logger = getLogger(__name__)
 
 
-def create_bridged_job(jobs_directory, working_directory):
+def create_bridged_job(working_directory, jobs_directory, results_directory):
     bridged_id = str(uuid4())
     jn = Path(jobs_directory).joinpath(f'{bridged_id}.json')
-    rf = Path(f'{jn}.result')
+    rf = Path(results_directory).joinpath(f'{bridged_id}.json.result')
     with open(jn, "w") as jout:
         info = dict(working_directory=str(working_directory))
         json.dump(info, jout)
@@ -36,7 +36,7 @@ def create_bridged_job(jobs_directory, working_directory):
 @dataclass
 class BridgedLocalSlurmOperations(LocalSlurmOperations):
     jobs_directory = Path.home().joinpath(".idmtools").joinpath("singularity-bridge")
-    results_directory = Path.home().joinpath(".idmtools").joinpath("singularity-bridge")
+    results_directory = Path.home().joinpath(".idmtools").joinpath("singularity-bridge").joinpath("results")
 
     def __post_init__(self):
         if not self.jobs_directory.exists():
@@ -58,9 +58,7 @@ class BridgedLocalSlurmOperations(LocalSlurmOperations):
             if not dry_run:
                 working_directory = self.get_directory(item)
 
-                result = subprocess.run(['sbatch', 'sbatch.sh'], stdout=subprocess.PIPE, cwd=str(working_directory))
-                stdout = result.stdout.decode('utf-8').strip()
-                return stdout
+                return create_bridged_job(working_directory, self.jobs_directory, self.results_directory)
         elif isinstance(item, Simulation):
             pass
         else:
