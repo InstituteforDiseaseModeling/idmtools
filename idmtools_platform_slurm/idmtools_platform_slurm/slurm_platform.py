067e1600565c977c804bd6a4ae165f3d43720a70
zdu <zdu@idmod.org>
2022-09-14 13:38:10 -0700

    Improve perf with new branch

--- idmtools_platform_slurm/idmtools_platform_slurm/slurm_platform.py
+++ idmtools_platform_slurm/idmtools_platform_slurm/slurm_platform.py
@@ -3,25 +3,31 @@ Here we implement the SlurmPlatform object.
 
 Copyright 2021, Bill & Melinda Gates Foundation. All rights reserved.
 """
-from typing import Optional, Any, Dict
+from typing import Optional, Any, Dict, List, Union
 from dataclasses import dataclass, field, fields
 from logging import getLogger
-from idmtools.entities.iplatform import IPlatform
 from idmtools.core import ItemType
+from idmtools.core import EntityStatus
+from idmtools.core.interfaces.ientity import IEntity
+from idmtools.entities.suite import Suite
+from idmtools.entities.experiment import Experiment
+from idmtools.entities.simulation import Simulation
+from idmtools.entities.iplatform import IPlatform, ITEM_TYPE_TO_OBJECT_INTERFACE
 from idmtools_platform_slurm.platform_operations.json_metadata_operations import JSONMetadataOperations
 from idmtools_platform_slurm.platform_operations.asset_collection_operations import \
     SlurmPlatformAssetCollectionOperations
 from idmtools_platform_slurm.platform_operations.experiment_operations import SlurmPlatformExperimentOperations
 from idmtools_platform_slurm.platform_operations.simulation_operations import SlurmPlatformSimulationOperations
-from idmtools_platform_slurm.slurm_operations import SlurmOperations, SlurmOperationalMode, RemoteSlurmOperations, \
-    LocalSlurmOperations
+from idmtools_platform_slurm.slurm_operations import SlurmOperations, SlurmOperationalMode, LocalSlurmOperations
 from idmtools_platform_slurm.platform_operations.suite_operations import SlurmPlatformSuiteOperations
+from idmtools_platform_slurm.platform_operations.utils import SlurmSuite, SlurmExperiment, SlurmSimulation
 
 logger = getLogger(__name__)
 
 op_defaults = dict(default=None, compare=False, metadata={"pickle_ignore": True})
-CONFIG_PARAMETERS = ['ntasks', 'partition', 'nodes', 'mail_type', 'mail_user', 'ntasks_per_core', 'cpus_per_task', 'mem_per_cpu', 'time',
-                     'account', 'mem', 'exclusive', 'requeue', 'sbatch_custom', 'max_running_jobs']
+CONFIG_PARAMETERS = ['ntasks', 'partition', 'nodes', 'mail_type', 'mail_user', 'ntasks_per_core', 'cpus_per_task',
+                     'mem_per_cpu', 'time', 'account', 'mem', 'exclusive', 'requeue', 'sbatch_custom',
+                     'max_running_jobs']
 
 
 @dataclass(repr=False)
@@ -135,3 +141,91 @@ class SlurmPlatform(IPlatform):
         config_dict = {k: getattr(self, k) for k in self.slurm_fields}
         config_dict.update(kwargs)
         return config_dict
+
+    def flatten_item(self, item: IEntity, raw=False, **kwargs) -> List[object]:
+        """
+        Flatten an item: resolve the children until getting to the leaves.
+
+        For example, for an experiment, will return all the simulations.
+        For a suite, will return all the simulations contained in the suites experiments.
+
+        Args:
+            item: Which item to flatten
+            raw: bool
+            kwargs: extra parameters
+
+        Returns:
+            List of leaves
+        """
+        if not raw:
+            interface = ITEM_TYPE_TO_OBJECT_INTERFACE[item.item_type]
+            idm_item = getattr(self, interface).to_entity(item)
+            return super().flatten_item(idm_item)
+
+        if isinstance(item, SlurmSuite):
+            experiments = self._suites.get_children(item, parent=item, raw=True)
+            children = list()
+            for slurm_exp in experiments:
+                children += self.flatten_item(item=slurm_exp, raw=raw)
+        elif isinstance(item, SlurmExperiment):
+            children = self._experiments.get_children(item, parent=item, raw=True)
+            exp = Experiment()
+            exp.uid = item.id
+            exp.platform = self
+            exp._platform_object = item
+            exp.tags = item.tags
+
+            for slurm_sim in children:
+                slurm_sim.experiment = exp
+                slurm_sim.platform = self
+        elif isinstance(item, SlurmSimulation):
+            if raw:
+                children = [item]
+            else:
+                exp = Experiment()
+                exp.uid = item.id
+                exp.platform = self
+                exp._platform_object = item
+                exp.tags = item.tags
+                sim = self._simulations.to_entity(item, parent=exp)
+                sim.experiment = exp
+                children = [sim]
+        elif isinstance(item, Suite):
+            slurm_suite = item.get_platform_object()
+            slurm_suite.platform = self
+            children = self.flatten_item(item=slurm_suite)
+        elif isinstance(item, Experiment):
+            children = item.simulations.items
+        elif isinstance(item, Simulation):
+            children = [item]
+        else:
+            raise Exception(f'Item Type: {type(item)} is not supported!')
+
+        return children
+
+    def validate_item_for_analysis(self, item: Union[Simulation, SlurmSimulation], analyze_failed_items=False):
+        """
+        Check if item is valid for analysis.
+
+        Args:
+            item: which item to verify status
+            analyze_failed_items: bool
+
+        Returns: bool
+        """
+        result = False
+
+        # TODO: we may consolidate two cases into one
+        if isinstance(item, SlurmSimulation):
+            if item.status == EntityStatus.SUCCEEDED:
+                result = True
+            else:
+                if analyze_failed_items and item.status == EntityStatus.FAILED:
+                    result = True
+        elif isinstance(item, Simulation):
+            if item.succeeded:
+                result = True
+            else:
+                if analyze_failed_items and item.status == EntityStatus.FAILED:
+                    result = True
+        return result
