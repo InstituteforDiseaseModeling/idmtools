b7bfabb601f5119121eed659b29121e0b28b3faf
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2022-10-26 14:20:58 -0700

    add rocky and almalinux sif build files

--- examples/singularity/rocky/create_dtk_rocky_sif.py
+++ examples/singularity/rocky/create_dtk_rocky_sif.py
@@ -2,23 +2,20 @@
 import os
 import sys
 
+from COMPS.utils.get_output_files_for_workitem import get_files
+
 from idmtools.core.platform_factory import Platform
-from idmtools_platform_comps.utils.download.download import DownloadWorkItem
 from idmtools_platform_comps.utils.singularity_build import SingularityBuildWorkItem
 
 if __name__ == '__main__':
-    platform = Platform("CALCULON")
-    sbi = SingularityBuildWorkItem(name="Create dtk rocky sif", definition_file="dtk_build_rocky.def", image_name="dtk_build_rocky_39.sif", force=True)
+    platform = Platform("Calculon")
+    sbi = SingularityBuildWorkItem(name="Create dtk rocky sif", definition_file="dtk_run_rocky_py39.def", image_name="dtk_run_rocky_py39.sif", force=True)
     sbi.tags = dict(python="3.9")
-    sbi.run(wait_until_done=True, platform=platform)
-    if sbi.succeeded:
-        # Write ID file
-        sbi.asset_collection.to_id_file("dtk_rock_prod.id")
+    ac_obj = sbi.run(wait_until_done=True, platform=platform)
 
-        dl_wi = DownloadWorkItem(name="download sif file",
-                                 related_work_items=[sbi.id],
-                                 file_patterns=["dtk_build_rocky_39.sif"], delete_after_download=False,
-                                 verbose=True,
-                                 output_path=os.path.join(os.getcwd(), "output_sif"),
-                                 )
-        dl_wi.run(wait_on_done=True, platform=platform)
+    if sbi.succeeded:
+        print("sbi.id: ", sbi.id)
+        # Write ID file
+        sbi.asset_collection.to_id_file(f"{platform._config_block}_dtk_run_rocky_py39_prod.id")
+        print("ac_obj: ", ac_obj.id)
+        get_files(sbi.id, "dtk_run_rocky_py39.sif")
