9981bbc282bf010ba225a209bfc2e5f98b7c5032
zdu <zdu@idmod.org>
2022-11-09 11:28:40 -0800

    Add clear_linux_mounts method

--- idmtools_platform_comps/idmtools_platform_comps/utils/linux_mounts.py
+++ idmtools_platform_comps/idmtools_platform_comps/utils/linux_mounts.py
@@ -35,6 +35,28 @@ def set_linux_mounts(platform: 'IPlatform', linux_environment: str = None) -> No
             os.environ[k] = ';'.join([v[0], v[2]])
 
 
+def clear_linux_mounts(platform: 'IPlatform', linux_environment: str = None) -> None:
+    """
+    For COMPS Platform, check and clear linux mounts.
+    Args:
+        platform: idmtools COMPS Platform
+        linux_environment: platform environment
+
+    Returns:
+        None
+    """
+    linux_envs = DEFAULT_ENVIRONMENTS
+    if linux_environment is not None:
+        linux_envs.append(linux_environment.upper())
+
+    if platform.environment.upper() in linux_envs:
+        mounts = AuthManager.get_environment_macros(platform.environment)['DOCKER_MOUNTS']
+        mounts = {v[0]: v[1:4] for v in [m.split(';') for m in mounts.split('|')]}
+        # pretend I'm on Linux and clear the Linux mapping environment variables
+        for k, v in mounts.items():
+            os.environ.pop(k)
+
+
 def get_workdir_from_simulations(platform: 'IPlatform', comps_simulations: List[Simulation]) -> Dict[str, str]:
     """
     Get COMPS simulations working directory.
@@ -47,5 +69,5 @@ def get_workdir_from_simulations(platform: 'IPlatform', comps_simulations: List[
     """
     set_linux_mounts(platform)
     sim_work_dir = {str(sim.id): sim.hpc_jobs[-1].working_directory for sim in comps_simulations if sim.hpc_jobs}
-
+    clear_linux_mounts(platform)
     return sim_work_dir
