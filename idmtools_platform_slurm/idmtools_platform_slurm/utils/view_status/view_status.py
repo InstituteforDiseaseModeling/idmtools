986d996849237a07a997243c2bb467913b12c317
zdu <zdu@idmod.org>
2023-01-11 12:21:38 -0800

    Remove all print

--- idmtools_platform_slurm/idmtools_platform_slurm/utils/view_status/view_status.py
+++ idmtools_platform_slurm/idmtools_platform_slurm/utils/view_status/view_status.py
@@ -19,7 +19,7 @@ from logging import getLogger
 if TYPE_CHECKING:  # pragma: no cover
     from idmtools.entities.iplatform import IPlatform
 
-logger = getLogger(__name__)
+user_logger = getLogger('user')
 
 
 @dataclass(repr=False)
@@ -74,10 +74,10 @@ class status_viewer:
             exp_id = exp_dir.name
             self._exp = platform.get_item(exp_id, ItemType.EXPERIMENT)
 
-            print('------------------------------')
-            print('last_suite_dir: ', last_suite_dir)
-            print('last_experiment_dir: ', exp_id)
-            print('------------------------------')
+            user_logger.info('------------------------------')
+            user_logger.info(f'last_suite_dir: {last_suite_dir}')
+            user_logger.info(f'last_experiment_dir: {exp_id}')
+            user_logger.info('------------------------------')
 
         job_id_path = platform.get_directory(self._exp).joinpath('job_id.txt')
         job_id = open(job_id_path).read().strip()
@@ -152,12 +152,12 @@ class status_viewer:
         """
         slurm_map = copy.deepcopy(SLURM_MAPS)
         slurm_map.pop('None', None)
-        print('------------------------------')
-        print("STATUS DEFINITION")
-        print("0: ".ljust(20), slurm_map['0'].name)
-        print("-1: ".ljust(20), slurm_map['-1'].name)
-        print("100: ".ljust(20), slurm_map['100'].name)
-        print('------------------------------')
+        user_logger.info('------------------------------')
+        user_logger.info("STATUS DEFINITION")
+        user_logger.info(f"{'0: '.ljust(20)} {slurm_map['0'].name}")
+        user_logger.info(f"{'-1: '.ljust(20)} {slurm_map['-1'].name}")
+        user_logger.info(f"{'100: '.ljust(20)} {slurm_map['100'].name}")
+        user_logger.info('------------------------------')
 
     def output_summary(self) -> None:
         """
@@ -166,10 +166,10 @@ class status_viewer:
             None
         """
         if self._summary:
-            print("job_id: ".ljust(20), self._summary["job_id"])
-            print("suite: ".ljust(20), self._summary["suite"])
-            print("experiment: ".ljust(20), self._summary["experiment"])
-            print("job_directory: ".ljust(20), self._summary["job_directory"])
+            user_logger.info(f"{'job_id: '.ljust(20)} {self._summary['job_id']}")
+            user_logger.info(f"{'suite: '.ljust(20)} {self._summary['suite']}")
+            user_logger.info(f"{'experiment: '.ljust(20)} {self._summary['experiment']}")
+            user_logger.info(f"{'job_directory: '.ljust(20)} {self._summary['job_directory']}")
 
     def output_status(self, platform: 'IPlatform', status_filter: List = None, job_ids: Union[str, List[str]] = None,
                       verbose: bool = True, root: str = 'job', display: bool = True, display_count: int = 20) -> None:
@@ -198,25 +198,25 @@ class status_viewer:
                 show_job_list = self._job_list[0: display_count]
                 show_job_dict = {key: job for key, job in self._job_dict.items() if key in show_job_list}
 
-            print(json.dumps(show_job_dict, indent=3))
+            user_logger.info(json.dumps(show_job_dict, indent=3))
         self.output_definition()
         if display and len(self._job_dict) > display_count:
             print(f"ONLY DISPLAY {display_count} ITEMS")
 
         # print report
         _simulation_count = len(self._exp.simulations)
-        print("status_filter: ".ljust(20), status_filter)
-        print("verbose: ".ljust(20), verbose)
-        print("display: ".ljust(20), display)
-        print("Simulation Count: ".ljust(20), _simulation_count)
-        print("Match Count: ".ljust(20), len(self._job_dict), f' ({dict(Counter(self._status_list))})')
-        print("Not Match Count: ".ljust(20), _simulation_count - len(self._job_dict))
-        print("No Status Count: ".ljust(20), len(self._sim_no_status))
+        user_logger.info(f"{'status_filter: '.ljust(20)} {status_filter}")
+        user_logger.info(f"{'verbose: '.ljust(20)} {verbose}")
+        user_logger.info(f"{'display: '.ljust(20)} {display}")
+        user_logger.info(f"{'Simulation Count: '.ljust(20)} {_simulation_count}")
+        user_logger.info(f"{'Match Count: '.ljust(20)} {len(self._job_dict)} ({dict(Counter(self._status_list))})")
+        user_logger.info(f"{'Not Match Count: '.ljust(20)} {_simulation_count - len(self._job_dict)}")
+        user_logger.info(f"{'No Status Count: '.ljust(20)} {len(self._sim_no_status)}")
 
         if self._sim:
-            print('\nSimulation Status: ', self._sim.status.name)
+            user_logger.info(f'\nSimulation Status: {self._sim.status.name}')
         elif self._exp:
-            print('\nExperiment Status: ', self._exp.status.name)
+            user_logger.info(f'\nExperiment Status: {self._exp.status.name}')
 
 
 def check_slurm_job(platform: 'IPlatform', scope: Tuple[str, ItemType] = None, status_filter: List = None,
