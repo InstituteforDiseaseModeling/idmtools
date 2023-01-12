5667cd7332fa76a9f9aa34d5ed5dd48ae67ab3ea
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2023-01-12 14:15:08 -0800

    Bump version: 1.7.3 → 1.7.3+nightly.0

--- idmtools_test/idmtools_test/__init__.py
+++ idmtools_test/idmtools_test/__init__.py
@@ -1,5 +1,5 @@
 import os
-__version__ = '1.7.3'
+__version__ = '1.7.3+nightly'
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 COMMON_INPUT_PATH = os.path.join(current_directory, "inputs")
