3a89d9b21ab40ad8daf8e26f77837089bccae041
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2023-01-25 14:20:41 -0800

    update more

--- idmtools_platform_local/idmtools_platform_local/__init__.py
+++ idmtools_platform_local/idmtools_platform_local/__init__.py
@@ -4,8 +4,4 @@ The local platform allows running experiments locally using docker.
 
 Copyright 2021, Bill & Melinda Gates Foundation. All rights reserved.
 """
-<<<<<<< HEAD
 __version__ = "1.7.4"
-=======
-__version__ = "1.7.3+nightly"
->>>>>>> 238019a9d4ed53e5148516516d5b536c1eafd6cf
