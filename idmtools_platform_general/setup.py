e1dafd3700c4d22f4d3c3f8cb47b61a50dab8b23
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2023-07-12 14:56:37 -0700

    remove extra comma

--- idmtools_platform_general/setup.py
+++ idmtools_platform_general/setup.py
@@ -40,7 +40,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Framework:: IDM-Tools :: Platform',
+        'Framework:: IDM-Tools :: Platform'
     ],
     description="General platform for IDM-Tools",
     install_requires=requirements,
--- idmtools_platform_slurm/setup.py
+++ idmtools_platform_slurm/setup.py
@@ -36,7 +36,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Framework:: IDM-Tools :: Platform',
+        'Framework:: IDM-Tools :: Platform'
     ],
     description="Provides ability to run against Slurm",
     install_requires=requirements,
