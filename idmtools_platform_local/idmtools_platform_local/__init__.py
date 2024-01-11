dffad5228d6f9e05dd51e82a5b95b27c90360182
shchen-idmod <shchen@idmod.org>
2024-01-11 11:28:37 -0800

    Bump version: 1.7.8+nightly.0 → 1.7.9

--- idmtools_platform_local/idmtools_platform_local/__init__.py
+++ idmtools_platform_local/idmtools_platform_local/__init__.py
@@ -4,4 +4,4 @@ The local platform allows running experiments locally using docker.
 
 Copyright 2021, Bill & Melinda Gates Foundation. All rights reserved.
 """
-__version__ = "1.7.8+nightly"
+__version__ = "1.7.9"
