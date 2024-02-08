1dee6bf87d67c59b6aa867b90966828a078b3f3e
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-02-08 15:28:54 -0800

    Adding more cases for simulation_builder

--- idmtools_core/tests/test_simulation_builder.py
+++ idmtools_core/tests/test_simulation_builder.py
@@ -164,62 +164,130 @@ class TestArmBuilder(ITestWithPersistence):
             expected_dict = {"a": value[0], "b": value[1]}
             self.assertEqual(simulation.task.parameters, expected_dict)
 
+    # Test case with parameters listed out in callback function:
+    # Case1: parameters in callback are all list
+    #   def update_parameter_callback(simulation, a, b, c)
+    #   builder.add_multiple_parameter_sweep_definition(update_parameter_callback, a, b, c)
+    # Case2: parameters in callback are not all list, can be single string
+    #   def update_parameter_callback(simulation, a, b, c)
+    #   builder.add_multiple_parameter_sweep_definition(update_parameter_callback, a, b, c)
     def test_add_multiple_parameter_sweep_definition(self):
         a = [True, False]
         b = [1, 2, 3, 4, 5]
-        c = ["test"]
-        self.builder.add_multiple_parameter_sweep_definition(update_parameter_callback, a, b, c)
-        templated_sim = self.get_templated_sim_builder()
-        # convert template to a fully realized list
-        simulations = list(templated_sim)
-        # Test if we have correct number of simulations
-        expected_values = list(itertools.product(a, b, c))
-        self.assertEqual(len(simulations), len(expected_values))
+        with self.subTest("test_with_list"):
+            c = ["test"]
+            self.builder.add_multiple_parameter_sweep_definition(update_parameter_callback, a, b, c)
+            templated_sim = self.get_templated_sim_builder()
+            # convert template to a fully realized list
+            simulations = list(templated_sim)
+            # Test if we have correct number of simulations
+            expected_values = list(itertools.product(a, b, c))
+            self.assertEqual(len(simulations), len(expected_values))
 
-        # Verify simulations individually
-        for simulation, value in zip(simulations, expected_values):
-            expected_dict = {"param_a": value[0], "param_b": value[1], "param_c": value[2]}
-            self.assertEqual(simulation.task.parameters, expected_dict)
+            # Verify simulations individually
+            for simulation, value in zip(simulations, expected_values):
+                expected_dict = {"param_a": value[0], "param_b": value[1], "param_c": value[2]}
+                self.assertEqual(simulation.task.parameters, expected_dict)
 
         # test with single string for c instead of list of string as ["test"]
-        c = "test"
-        builder2 = SimulationBuilder()
-        builder2.add_multiple_parameter_sweep_definition(update_parameter_callback, a, b, c)
-        templated_sim2 = self.get_templated_sim_builder()
-        simulations2 = list(templated_sim2)
-        expected_values2 = list(itertools.product(a, b, [c]))
-        # Test if we have correct number of simulations
-        self.assertEqual(len(simulations2), len(expected_values2))
-        # Verify simulations individually
-        for simulation, value in zip(simulations2, expected_values2):
-            expected_dict = {"param_a": value[0], "param_b": value[1], "param_c": value[2]}
-            self.assertEqual(simulation.task.parameters, expected_dict)
+        with self.subTest("test_with_single_parameter"):
+            self.builder = SimulationBuilder()
+            c = "test"
+            self.builder.add_multiple_parameter_sweep_definition(update_parameter_callback, a, b, c)
+            templated_sim2 = self.get_templated_sim_builder()
+            simulations2 = list(templated_sim2)
+            expected_values2 = list(itertools.product(a, b, [c]))
+            # Test if we have correct number of simulations
+            self.assertEqual(len(simulations2), len(expected_values2))
+            # Verify simulations individually
+            for simulation, value in zip(simulations2, expected_values2):
+                expected_dict = {"param_a": value[0], "param_b": value[1], "param_c": value[2]}
+                self.assertEqual(simulation.task.parameters, expected_dict)
 
+    # Test cases to pass dict to add_multiple_parameter_sweep_definition:
+    # Case1:
+    #   def update_parameter_callback(simulation, d: dict)
+    #   builder.add_multiple_parameter_sweep_definition(update_parameter_callback, dict)
+    # Case2:
+    #   def update_parameter_callback(simulation, a, b, c)
+    #   builder.add_multiple_parameter_sweep_definition(update_parameter_callback, dict)
+    # Case3: # len(args)==1 for add_multiple_parameter_sweep_definition
+    #   def update_parameter_callback(simulation, a: dict)  # len(args)==1
+    #   builder.add_multiple_parameter_sweep_definition(update_parameter_callback, dict)
+    # Case4: # len(kwargs)==1 for add_multiple_parameter_sweep_definition
+    #   def update_parameter_callback(simulation, a: dict)  # len(args)==1
+    #   builder.add_multiple_parameter_sweep_definition(update_parameter_callback, dict)
     def test_add_multiple_parameter_sweep_definition_dict(self):
-        test_dict = {"a": [True, False], "b": [1, 2, 3, 4, 5], "c": ["test"]}
-        self.builder.add_multiple_parameter_sweep_definition(update_parameter_callback, test_dict)
-        templated_sim = self.get_templated_sim_builder()
-        # convert template to a fully realized list
-        simulations = list(templated_sim)
-        # Test if we have correct number of simulations
-        expected_values = list(itertools.product(test_dict['a'], test_dict['b'], test_dict['c']))
-        self.assertEqual(len(simulations), len(expected_values))
+        # Case1:
+        with self.subTest("callback_func_with_dict"):
+            def update_parameter_callback1(simulation, d: dict):
+                simulation.task.set_parameter("param_a", d['a'])
+                simulation.task.set_parameter("param_b", d['b'])
+                simulation.task.set_parameter("param_c", d['c'])
+                return {"a": d['a'], "b": d['b'], "c": d['c']}
 
-        # Verify simulations individually
-        for simulation, value in zip(simulations, expected_values):
-            expected_dict = {"param_a": value[0], "param_b": value[1], "param_c": value[2]}
-            self.assertEqual(simulation.task.parameters, expected_dict)
+            test_dict = {"a": [True, False], "b": [1, 2, 3, 4, 5], "c": ["test"]}
+            self.builder.add_multiple_parameter_sweep_definition(update_parameter_callback1, test_dict)
+            templated_sim = self.get_templated_sim_builder()
+            # convert template to a fully realized list
+            simulations = list(templated_sim)
+            # Test if we have correct number of simulations
+            expected_values = list(itertools.product(test_dict['a'], test_dict['b'], test_dict['c']))
+            self.assertEqual(len(simulations), len(expected_values))
 
-        # test with single string for c instead of list of string as ["test"]
-        test_dict2 = {"a": [True, False], "b": [1, 2, 3, 4, 5], "c": "test"}
-        builder2 = SimulationBuilder()
-        builder2.add_multiple_parameter_sweep_definition(update_parameter_callback, test_dict2)
-        templated_sim2 = self.get_templated_sim_builder()
-        simulations2 = list(templated_sim2)
-        expected_values2 = list(itertools.product(test_dict2['a'], test_dict2['b'], [test_dict2['c']]))
-        # Test if we have correct number of simulations
-        self.assertEqual(len(simulations2), len(expected_values2))
-        # Verify simulations individually
-        for simulation, value in zip(simulations2, expected_values2):
-            expected_dict = {"param_a": value[0], "param_b": value[1], "param_c": value[2]}
-            self.assertEqual(simulation.task.parameters, expected_dict)
+            # Verify simulations individually
+            for simulation, value in zip(simulations, expected_values):
+                expected_dict = {"param_a": value[0], "param_b": value[1], "param_c": value[2]}
+                self.assertEqual(simulation.task.parameters, expected_dict)
+
+        # Case2: test with different callback function signature than case1
+        with self.subTest("callback_func_with_dict_key_parameters"):
+            self.builder = SimulationBuilder()
+            test_dict2 = {"a": [True, False], "b": [1, 2, 3, 4, 5], "c": "test"}
+            self.builder.add_multiple_parameter_sweep_definition(update_parameter_callback, test_dict2)
+            templated_sim2 = self.get_templated_sim_builder()
+            simulations2 = list(templated_sim2)
+            expected_values2 = list(itertools.product(test_dict2['a'], test_dict2['b'], [test_dict2['c']]))
+            # Test if we have correct number of simulations
+            self.assertEqual(len(simulations2), len(expected_values2))
+            # Verify simulations individually
+            for simulation, value in zip(simulations2, expected_values2):
+                expected_dict = {"param_a": value[0], "param_b": value[1], "param_c": value[2]}
+                self.assertEqual(simulation.task.parameters, expected_dict)
+
+        # Case3: len(args) = 1
+        with self.subTest("test_args_len_eqs_1"):
+            def update_parameter_callback3(simulation, d):
+                simulation.task.set_parameter("param_a", d['a'])
+                return {"a": d['a']}
+
+            test_dict = {"a": "test"}
+            self.builder = SimulationBuilder()
+            self.builder.add_multiple_parameter_sweep_definition(update_parameter_callback3, test_dict)
+            templated_sim3 = self.get_templated_sim_builder()
+            # convert template to a fully realized list
+            simulations3 = list(templated_sim3)
+            # Test if we have correct number of simulations
+            expected_values = list(itertools.product([test_dict['a']]))
+            self.assertEqual(len(simulations3), len(expected_values))
+
+            # Verify simulations individually
+            for simulation, value in zip(simulations3, expected_values):
+                expected_dict = {"param_a": value[0]}
+                self.assertEqual(simulation.task.parameters, expected_dict)
+
+        # Case4: len(kwargs) = 1
+        with self.subTest("test_args_len_eqs_1"):
+            def update_parameter_callback4(simulation, d):
+                simulation.task.set_parameter("param_a", d)
+                return {"a": d}
+
+            self.builder = SimulationBuilder()
+            self.builder.add_multiple_parameter_sweep_definition(update_parameter_callback4, test_dict="a")
+            templated_sim4 = self.get_templated_sim_builder()
+            # convert template to a fully realized list
+            simulations4 = list(templated_sim4)
+            # Test if we have correct number of simulations
+            self.assertEqual(len(simulations4), 1)
+            expected_dict = {"param_a": {'test_dict': 'a'}}
+            self.assertEqual(simulations4[0].task.parameters, expected_dict)
