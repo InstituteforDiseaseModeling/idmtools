66828967c309ac7e262cb43cedc81343f2f6884b
zdu <zdu@idmod.org>
2024-03-07 09:50:14 -0800

    update builder import

--- idmtools_core/idmtools/builders/__init__.py
+++ idmtools_core/idmtools/builders/__init__.py
@@ -5,6 +5,7 @@ Copyright 2021, Bill & Melinda Gates Foundation. All rights reserved.
 """
 # flake8: noqa F821
 from idmtools.builders.simulation_builder import SimulationBuilder
-from idmtools.builders.arm_simulation_builder import ArmSimulationBuilder, SweepArm, ArmType
+from idmtools.builders.sweep_arm import SweepArm, ArmType, TSweepFunction
+from idmtools.builders.arm_simulation_builder import ArmSimulationBuilder
 from idmtools.builders.csv_simulation_builder import CsvExperimentBuilder
 from idmtools.builders.yaml_simulation_builder import YamlSimulationBuilder
