8ff2fd49e635bd0dd72bdc791bf7038a6249a9fa
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-06-28 13:29:10 -0700

    update

--- idmtools_cli/setup.py
+++ idmtools_cli/setup.py
@@ -39,8 +39,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Framework:: IDM-Tools :: CLI'
+        'Programming Language :: Python :: 3.12'
     ],
     description="CLI for IDM-Tools",
     install_requires=requirements,
