0a218dc574ab4a3580f6c25573c9a692170a8759
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-07-23 11:12:18 -0700

    refactor code for get rich table from cli result

--- idmtools_platform_container/tests/test_container_cli/test_jobs.py
+++ idmtools_platform_container/tests/test_container_cli/test_jobs.py
@@ -1,25 +1,31 @@
 import os
 import sys
 import unittest
+from unittest.mock import patch
 import pytest
 from idmtools.entities.command_task import CommandTask
 from idmtools.entities.experiment import Experiment
 import idmtools_platform_container.cli.container as container_cli
 from idmtools_platform_container.utils.job_history import JobHistory
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from helper import get_jobs_from_cli
+from helper import get_actual_rich_table_values
 from test_base import TestContainerPlatformCliBase
+
 
 @pytest.mark.serial
 class TestContainerPlatformJobCli(TestContainerPlatformCliBase):
-    def test_jobs(self):
+
+    @patch('rich.console.Console.print')
+    def test_jobs(self, mock_console):
         command = "sleep 100"
         task = CommandTask(command=command)
         experiment = Experiment.from_task(task, name="run_command")
         experiment.run(wait_until_done=False)
         job = JobHistory.get_job(experiment.id)
         # test jobs
-        actual_table = get_jobs_from_cli(self.runner)
+        result = self.runner.invoke(container_cli.container, ['jobs'])
+        self.assertEqual(result.exit_code, 0)
+        actual_table = get_actual_rich_table_values(mock_console)
         expected_job = ['EXPERIMENT', experiment.id, job['CONTAINER']]
         found = False
         for row in actual_table:
@@ -30,14 +36,17 @@ class TestContainerPlatformJobCli(TestContainerPlatformCliBase):
         result = self.runner.invoke(container_cli.container, ['cancel', experiment.id])
         self.assertEqual(result.exit_code, 0)
 
-    def test_jobs_with_container_id(self):
+    @patch('rich.console.Console.print')
+    def test_jobs_with_container_id(self, mock_console):
         command = "sleep 100"
         task = CommandTask(command=command)
         experiment = Experiment.from_task(task, name="run_command")
         experiment.run(wait_until_done=False)
         job = JobHistory.get_job(experiment.id)
         # test jobs with container id
-        actual_table = get_jobs_from_cli(self.runner, job['CONTAINER'])
+        result = self.runner.invoke(container_cli.container, ['jobs'], self.platform.container_id)
+        self.assertEqual(result.exit_code, 0)
+        actual_table = get_actual_rich_table_values(mock_console)
         expected_job = ['EXPERIMENT', experiment.id, job['CONTAINER']]
         found = False
         for row in actual_table:
