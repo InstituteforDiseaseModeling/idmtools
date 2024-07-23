544cd297bd5b984b97d0a39c2ef0097507303c6c
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-07-23 11:47:25 -0700

    Added cli ps test

--- idmtools_platform_container/tests/test_container_cli/test_ps.py
+++ idmtools_platform_container/tests/test_container_cli/test_ps.py
@@ -5,39 +5,41 @@ import pytest
 import idmtools_platform_container.cli.container as container_cli
 from idmtools.entities.command_task import CommandTask
 from idmtools.entities.experiment import Experiment
-from idmtools_platform_container.utils.general import normalize_path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from test_base import TestContainerPlatformCliBase
 
 
 @pytest.mark.serial
-class TestContainerPlatformPathCli(TestContainerPlatformCliBase):
+class TestContainerPlatformProcessCli(TestContainerPlatformCliBase):
 
     @patch('rich.console.Console.print')
-    def test_path(self, mock_console):
+    def test_ps(self, mock_console):
         command = "sleep 100"
         task = CommandTask(command=command)
         experiment = Experiment.from_task(task, name="run_command")
         experiment.run(wait_until_done=False)
-        result = self.runner.invoke(container_cli.container, ['path', experiment.id])
+        result = self.runner.invoke(container_cli.container, ['ps', self.platform.container_id])
         self.assertEqual(result.exit_code, 0)
-        # check path
-
-        self.assertIn(normalize_path(f'{self.platform.job_directory}/{experiment.parent_id}/{experiment.id}'),
-                      normalize_path(mock_console.call_args.args[0].split(' ')[1]))
+        # check ps
+        self.assertIn("UID        PID  PPID  PGID   SID  C STIME TTY          TIME CMD",
+                      mock_console.call_args_list[0].args[0])
+        self.assertIn(f"EXPERIMENT:{experiment.id} batch.sh", mock_console.call_args_list[0].args[0])
+        self.assertIn(
+            "xargs -d \\n -P 4 -I% bash -c cd $(pwd) && $(pwd)/run_simulation.sh %  1>> stdout.txt 2>> stderr.txt",
+            mock_console.call_args_list[0].args[0])
         # clean up by stop the job
         result = self.runner.invoke(container_cli.container, ['stop-container', self.platform.container_id], '--remove')
         self.assertEqual(result.exit_code, 0)
 
-    def test_path_help(self):
-        result = self.runner.invoke(container_cli.container, ['path', "--help"])
-        expected_help = ('Usage: container path [OPTIONS] ITEM_ID\n'
+    def test_ps_help(self):
+        result = self.runner.invoke(container_cli.container, ['ps', "--help"])
+        expected_help = ('Usage: container ps [OPTIONS] CONTAINER_ID\n'
                          '\n'
-                         '  Locate Suite/Experiment/Simulation file directory.\n'
+                         '  List running processes in a container.\n'
                          '\n'
                          '  Arguments:\n'
                          '\n'
-                         '    ITEM_ID: Suite/Experiment/Simulation ID\n'
+                         '    CONTAINER_ID: Container ID\n'
                          '\n'
                          'Options:\n'
                          '  --help  Show this message and exit.\n')
