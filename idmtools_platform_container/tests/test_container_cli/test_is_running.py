46c7bcae3b5d269802d9b2b9d3133bfb5fb9b71d
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-07-26 09:59:47 -0700

    Add few more testcases to is_running cli test

--- idmtools_platform_container/tests/test_container_cli/test_is_running.py
+++ idmtools_platform_container/tests/test_container_cli/test_is_running.py
@@ -7,6 +7,7 @@ import pytest
 from idmtools.entities.command_task import CommandTask
 from idmtools.entities.experiment import Experiment
 import idmtools_platform_container.cli.container as container_cli
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from test_base import TestContainerPlatformCliBase
 
@@ -14,17 +15,39 @@ from test_base import TestContainerPlatformCliBase
 @pytest.mark.serial
 class TestContainerPlatformIsRunningCli(TestContainerPlatformCliBase):
 
-    @patch('rich.console.Console.print')
-    def test_is_running(self, mock_console):
-        command = "sleep 100"
+    def test_is_running(self):
+        command = "python3 Assets/sleep.py"
         task = CommandTask(command=command)
+        task.common_assets.add_asset("../inputs/sleep.py")
         experiment = Experiment.from_task(task, name="run_command")
         experiment.run(wait_until_done=False)
         sleep(10)
-        result = self.runner.invoke(container_cli.container, ['is-running', experiment.id])
-        self.assertEqual(result.exit_code, 0)
-        self.assertEqual(f'EXPERIMENT {experiment.id} is running on container {self.platform.container_id}.',
-                         mock_console.call_args_list[0][0][0])
+        # test is-running with experiment id
+        with patch('rich.console.Console.print') as mock_console_exp:
+            result = self.runner.invoke(container_cli.container, ['is-running', experiment.id])
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(f'EXPERIMENT {experiment.id} is running on container {self.platform.container_id}.',
+                             mock_console_exp.call_args_list[0][0][0])
+        # test is-running with simulation id
+        with patch('rich.console.Console.print') as mock_console_sim:
+            result = self.runner.invoke(container_cli.container, ['is-running', experiment.simulations[0].id])
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(
+                f'SIMULATION {experiment.simulations[0].id} is running on container {self.platform.container_id}.',
+                mock_console_sim.call_args_list[0][0][0])
+        # test is-running with invalid id
+        with patch('rich.console.Console.print') as mock_console_invalid:
+            result = self.runner.invoke(container_cli.container, ['is-running', "abc"])
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(f'Job abc is not found.', mock_console_invalid.call_args_list[0][0][0])
+
+        # test is-running with suite id
+        with patch('rich.console.Console.print') as mock_console_suite:
+            result = self.runner.invoke(container_cli.container, ['is-running', experiment.parent_id])
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(f'{experiment.parent_id} is not a valid Experiment/Simulation ID.',
+                             mock_console_suite.call_args_list[0][0][0])
+
         # clean up container
         result = self.runner.invoke(container_cli.container, ['stop-container', self.platform.container_id], '--remove')
         self.assertEqual(result.exit_code, 0)
