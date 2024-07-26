c46183004a94a2bb7be51db54f3eab9a09d68a85
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-07-26 10:02:14 -0700

    Add verbose and limit testcases to status cli test

--- idmtools_platform_container/tests/test_container_cli/test_status.py
+++ idmtools_platform_container/tests/test_container_cli/test_status.py
@@ -2,41 +2,126 @@ import os
 import sys
 import unittest
 from unittest import mock
-from unittest.mock import patch
 import pytest
+from idmtools.builders import SimulationBuilder
+from idmtools.core import ItemType
 from idmtools.entities.command_task import CommandTask
 from idmtools.entities.experiment import Experiment
 import idmtools_platform_container.cli.container as container_cli
+from idmtools.entities.templated_simulation import TemplatedSimulations
+from idmtools_platform_container.container_platform import ContainerPlatform
 from idmtools_platform_container.utils.general import normalize_path
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from test_base import TestContainerPlatformCliBase
+from helper import get_actual_rich_table_values, found_job_id_by_experiment, cleaned_str
 
 
 @pytest.mark.serial
 class TestContainerPlatformStatusCli(TestContainerPlatformCliBase):
-    @patch('rich.console.Console.print')
-    def test_status_with_experiment_id(self, mock_console):
-        command = "sleep 100"
+    def test_status(self):
+        command = "python3 Assets/sleep.py"
         task = CommandTask(command=command)
+        task.common_assets.add_asset("../inputs/sleep.py")
         experiment = Experiment.from_task(task, name="run_command")
         experiment.run(wait_until_done=False)
+        exp_dir = self.platform.get_directory_by_id(experiment.id, ItemType.EXPERIMENT)
+
         # test status with experiment id
-        result = self.runner.invoke(container_cli.container, ['status', experiment.id])
-        self.assertEqual(result.exit_code, 0)
-        self.assertIn(normalize_path(f"{self.platform.job_directory}/{experiment.parent_id}/{experiment.id}"),
-                      mock_console.call_args_list[0].args[0])
-        self.assertIn('Simulation Count', mock_console.call_args_list[1][0][0])
-        self.assertIn('SUCCEEDED', mock_console.call_args_list[2][0][0])
-        self.assertIn('FAILED', mock_console.call_args_list[3][0][0])
-        self.assertIn('RUNNING', mock_console.call_args_list[4][0][0])
-        self.assertIn('PENDING', mock_console.call_args_list[5][0][0])
+        with mock.patch('rich.console.Console.print') as mock_console:
+            result = self.runner.invoke(container_cli.container, ['status', experiment.id])
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn(normalize_path(exp_dir), mock_console.call_args_list[0].args[0])
+            self.assertIn('Simulation Count', cleaned_str(mock_console.call_args_list[1][0][0]))
+            self.assertIn('SUCCEEDED', cleaned_str(mock_console.call_args_list[2][0][0]))
+            self.assertIn('FAILED', cleaned_str(mock_console.call_args_list[3][0][0]))
+            self.assertIn('RUNNING', cleaned_str(mock_console.call_args_list[4][0][0]))
+            self.assertIn('PENDING', cleaned_str(mock_console.call_args_list[5][0][0]))
+
+        # test status with experiment id and container id
+        with mock.patch('rich.console.Console.print') as mock_console_container:
+            result = self.runner.invoke(container_cli.container,
+                                        ['status', experiment.id, '-c', self.platform.container_id])
+            self.assertEqual(result.exit_code, 0)
+            self.assertIn(normalize_path(exp_dir), mock_console_container.call_args_list[0].args[0])
+            self.assertIn('Simulation Count', cleaned_str(mock_console_container.call_args_list[1][0][0]))
+            self.assertIn('SUCCEEDED', cleaned_str(mock_console_container.call_args_list[2][0][0]))
+            self.assertIn('FAILED', cleaned_str(mock_console_container.call_args_list[3][0][0]))
+            self.assertIn('RUNNING', cleaned_str(mock_console_container.call_args_list[4][0][0]))
+            self.assertIn('PENDING', cleaned_str(mock_console_container.call_args_list[5][0][0]))
+
         # test status with simulation id
         with mock.patch('rich.console.Console.print') as mock_console_sim:
+            print("simulations", experiment.simulations[0].id)
             result = self.runner.invoke(container_cli.container, ['status', experiment.simulations[0].id])
+            self.assertEqual(result.exit_code, 0)
             self.assertIn(f'SIMULATION {experiment.simulations[0].id} is ',
                           mock_console_sim.call_args_list[0][0][0])
+
+        # test status with wrong id
+        with mock.patch('idmtools_platform_container.cli.container.user_logger') as user_logger:
+            result = self.runner.invoke(container_cli.container, ['status', "sim_id"])
+            self.assertEqual(result.exit_code, 0)
+            user_logger.warning.assert_called_with("Job sim_id not found.")
+
+        # test status with job id
+        with mock.patch('rich.console.Console.print') as mock_console_jobs:
+            # first find job id:
+            result = self.runner.invoke(container_cli.container, ['jobs'])
+            self.assertEqual(result.exit_code, 0)
+            actual_table = get_actual_rich_table_values(mock_console_jobs)
+            job_id, container_id = found_job_id_by_experiment(actual_table, experiment.id)
+            # test status with job id
+            with mock.patch('rich.console.Console.print') as mock_console_job:
+                result = self.runner.invoke(container_cli.container, ['status', job_id])
+                self.assertEqual(result.exit_code, 0)
+                self.assertIn(normalize_path(exp_dir), mock_console_job.call_args_list[0].args[0])
+                self.assertIn('Simulation Count', cleaned_str(mock_console_job.call_args_list[1][0][0]))
+                self.assertIn('SUCCEEDED', cleaned_str(mock_console_job.call_args_list[2][0][0]))
+                self.assertIn('FAILED', cleaned_str(mock_console_job.call_args_list[3][0][0]))
+                self.assertIn('RUNNING', cleaned_str(mock_console_job.call_args_list[4][0][0]))
+                self.assertIn('PENDING', cleaned_str(mock_console_job.call_args_list[5][0][0]))
+
         # clean up container
         result = self.runner.invoke(container_cli.container, ['stop-container', self.platform.container_id], '--remove')
+        self.assertEqual(result.exit_code, 0)
+
+    # test status with verbose and limit. For this test, we generate 9 simulations, but only show 2 in the output
+    @mock.patch('rich.console.Console.print')
+    def test_status_with_verbose_and_limit(self, mock_console):
+        platform = ContainerPlatform(job_directory=self.job_directory, new_container=True)
+        command = "python3 Assets/sleep.py"
+        task = CommandTask(command=command)
+        task.common_assets.add_asset("../inputs/sleep.py")
+        ts = TemplatedSimulations(base_task=task)
+        sb = SimulationBuilder()
+
+        def update_parameter_callback(simulation, sleep_time):
+            simulation.task.command.add_argument(sleep_time)
+            return {'sleep_time': sleep_time}
+
+        sb.add_sweep_definition(update_parameter_callback, sleep_time=range(1, 10))
+        ts.add_builder(sb)
+        experiment = Experiment.from_template(ts, name="run_sweep_sleep")
+        experiment.run(wait_until_done=True, platform=platform)
+        exp_dir = platform.get_directory_by_id(experiment.id, ItemType.EXPERIMENT)
+        # we only show 2 simulation details in the output
+        result = self.runner.invoke(container_cli.container,
+                                    ['status', experiment.id, '-l', 2, '--verbose'])
+        self.assertEqual(result.exit_code, 0)
+        self.assertIn(normalize_path(exp_dir), mock_console.call_args_list[0].args[0])
+        self.assertEqual('Simulation Count: 9', cleaned_str(mock_console.call_args_list[1][0][0]))
+        self.assertEqual('SUCCEEDED (9)', cleaned_str(mock_console.call_args_list[2][0][0]))
+        self.assertEqual('...', cleaned_str(mock_console.call_args_list[5][0][0]))
+        self.assertEqual('FAILED (0)', cleaned_str(mock_console.call_args_list[6][0][0]))
+        self.assertIn('RUNNING', cleaned_str(mock_console.call_args_list[7][0][0]))
+        self.assertIn('PENDING', cleaned_str(mock_console.call_args_list[8][0][0]))
+        # verify there are 2 lines with simulations
+        simulation_ids = [simulation.id for simulation in experiment.simulations]
+        self.assertTrue(cleaned_str(mock_console.call_args_list[3][0][0]) in simulation_ids)
+        self.assertTrue(cleaned_str(mock_console.call_args_list[4][0][0]) in simulation_ids)
+        # clean up container
+        result = self.runner.invoke(container_cli.container, ['stop-container', platform.container_id], '--remove')
         self.assertEqual(result.exit_code, 0)
 
     def test_status_help(self):
