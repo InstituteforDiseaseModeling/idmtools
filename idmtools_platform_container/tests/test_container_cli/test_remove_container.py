514d0374fc61e129788d0c7553e10d96679b11bb
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-07-26 10:02:59 -0700

    Add few more testcases to remove-container and stop-container cli tests

--- idmtools_platform_container/tests/test_container_cli/test_remove_container.py
+++ idmtools_platform_container/tests/test_container_cli/test_remove_container.py
@@ -6,6 +6,8 @@ import pytest
 import idmtools_platform_container.cli.container as container_cli
 from idmtools.entities.command_task import CommandTask
 from idmtools.entities.experiment import Experiment
+from idmtools_platform_container.container_platform import ContainerPlatform
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from test_base import TestContainerPlatformCliBase
 
@@ -18,16 +20,46 @@ class TestContainerPlatformRemoveContainerCli(TestContainerPlatformCliBase):
         command = "sleep 100"
         task = CommandTask(command=command)
         experiment = Experiment.from_task(task, name="run_command")
-        experiment.run(wait_until_done=False)
-        result = self.runner.invoke(container_cli.container, ['stop-container', self.platform.container_id])
+        platform = ContainerPlatform(job_directory=self.job_directory, new_container=True)
+        experiment.run(wait_until_done=False, platform=platform)
+        result = self.runner.invoke(container_cli.container, ['stop-container', platform.container_id])
         self.assertEqual(result.exit_code, 0)
         sleep(1)
-        result = self.runner.invoke(container_cli.container, ['remove-container', self.platform.container_id])
+        result = self.runner.invoke(container_cli.container, ['remove-container', platform.container_id])
         self.assertEqual(result.exit_code, 0)
         with patch('rich.console.Console.print') as mock_console:
-            result = self.runner.invoke(container_cli.container, ['inspect', self.platform.container_id])
+            result = self.runner.invoke(container_cli.container, ['inspect', platform.container_id])
             self.assertEqual(result.exit_code, 0)
-            self.assertTrue(f"Container {self.platform.container_id} not found.", mock_console.call_args_list[0].args[0])
+            self.assertTrue(f"Container {platform.container_id} not found.", mock_console.call_args_list[0].args[0])
+
+    @patch('idmtools_platform_container.cli.container.user_logger')
+    def test_remove_container_running_container(self, mock_user_logger):
+        command = "sleep 100"
+        task = CommandTask(command=command)
+        experiment = Experiment.from_task(task, name="run_command")
+        platform = ContainerPlatform(job_directory=self.job_directory, new_container=True)
+        experiment.run(wait_until_done=False, platform=platform)
+        result = self.runner.invoke(container_cli.container, ['remove-container', platform.container_id])
+        self.assertEqual(result.exit_code, 0)
+        mock_user_logger.warning.assert_called_with(
+            f"Container {platform.container_id} is running, need to stop first.")
+
+        result = self.runner.invoke(container_cli.container, ['stop-container', platform.container_id, '--remove'])
+        self.assertEqual(result.exit_code, 0)
+
+    @patch('idmtools_platform_container.cli.container.user_logger')
+    def test_remove_container_invalid(self, mock_user_logger):
+        command = "sleep 100"
+        platform = ContainerPlatform(job_directory=self.job_directory, new_container=True)
+        task = CommandTask(command=command)
+        experiment = Experiment.from_task(task, name="run_command")
+        experiment.run(wait_until_done=False, platform=platform)
+        sleep(1)
+        result = self.runner.invoke(container_cli.container, ['remove-container', "abcd"])
+        sleep(1)
+        mock_user_logger.warning.assert_called_with("Container abcd not found.")
+        result = self.runner.invoke(container_cli.container, ['stop-container', platform.container_id, '--remove'])
+        self.assertEqual(result.exit_code, 0)
 
     def test_remove_container_help(self):
         result = self.runner.invoke(container_cli.container, ['remove-container', "--help"])
