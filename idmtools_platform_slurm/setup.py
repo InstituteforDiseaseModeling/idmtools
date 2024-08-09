13dc17eccd2ea7a7c03e8f3e3c211c1de556a907
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-08-08 19:58:18 -0700

    Bump version: 1.7.10+nightly.0 → 1.7.11

--- idmtools_platform_slurm/setup.py
+++ idmtools_platform_slurm/setup.py
@@ -53,5 +53,5 @@ setup(
     test_suite='tests',
     extras_require=extras,
     url='https://github.com/InstituteforDiseaseModeling/idmtools',
-    version='1.7.10+nightly'
+    version='1.7.11'
 )
