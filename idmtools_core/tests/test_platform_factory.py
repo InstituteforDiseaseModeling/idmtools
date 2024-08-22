b7c4ff8b597fc324f7790f02e0c192eed5c5497f
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-08-21 22:47:52 -0700

    Added testcases with new platform_factory changes

--- idmtools_core/tests/test_platform_factory.py
+++ idmtools_core/tests/test_platform_factory.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import allure
 import os
 import pytest
@@ -29,32 +31,32 @@ class TestPlatformFactory(ITestWithPersistence):
     def test_block_not_exits(self):
         with self.assertRaises(ValueError) as context:
             Platform('NOTEXISTS')  # noqa:F841
-        self.assertEqual("Block 'NOTEXISTS' doesn't exist!", str(context.exception.args[0]))
+        self.assertTrue("NOTEXISTS is an unknown Platform Type. Supported platforms are " in str(context.exception.args[0]))
 
     @allure.story("Plugins")
     def test_bad_type(self):
         with self.assertRaises(ValueError) as context:
             Platform('BADTYPE')  # noqa:F841
-        self.assertTrue("Bad is an unknown Platform Type. Supported platforms are" in str(context.exception.args[0]))
+        self.assertIn("Bad is an unknown Platform Type. Supported platforms are", str(context.exception.args[0]))
 
     @allure.story("Plugins")
     def test_no_type(self):
         with self.assertRaises(ValueError) as context:
             Platform('NOTYPE')
-        self.assertIn('When creating a Platform you must specify the type in the block.', context.exception.args[0])
+        self.assertIn('NOTYPE is an unknown Platform Type. Supported platforms are ',  context.exception.args[0])
 
     @allure.story("Configuration")
     def test_block_is_none(self):
-        with self.assertRaises(ValueError) as context:
+        try:
             Platform(None)
-        self.assertIn('Must have a valid Block name to create a Platform!', context.exception.args[0])
-
+        except Exception as ex:
+            self.assertIn("Type must be specified in Platform constructor", ex.args[0])
     @allure.story("Configuration")
     def test_no_block(self):
         try:
             Platform()
         except Exception as ex:
-            self.assertIn("__new__() missing 1 required positional argument: 'block'", ex.args[0])
+            self.assertIn("Type must be specified in Platform constructor", ex.args[0])
 
     @allure.story("Configuration")
     def test_create_from_block(self):
@@ -76,3 +78,135 @@ class TestPlatformFactory(ITestWithPersistence):
         platform2.uid = None
         self.assertEqual(platform, platform2)
         platform2.cleanup()
+
+    @patch("idmtools.config.idm_config_parser.user_logger")
+    def test_create_platform_with_valid_block(self, mock_user_logger):
+        block = 'My_container'
+        platform = Platform(block)
+        self.assertEqual(platform._config_block, block)
+        self.assertEqual(platform.__class__.__name__, 'ContainerPlatform')
+        self.assertEqual(platform._kwargs, {})
+        self.assertIn('MY_JOB_DIRECTORY', platform.job_directory)
+        mock_user_logger.log.call_args_list[0].assert_called_with('\n[My_container]')
+        mock_user_logger.log.call_args_list[1].assert_called_with('"job_directory": "MY_JOB_DIRECTORY"')
+
+    @patch("idmtools.core.platform_factory.user_logger")
+    def test_create_platform_with_valid_block_new_job_directory_from_platform(self, mock_user_logger):
+        block = 'My_container'
+        platform = Platform(block, job_directory="my_directory")
+        self.assertEqual(platform._config_block, block)
+        self.assertEqual(platform.__class__.__name__, 'ContainerPlatform')
+        self.assertEqual(platform._kwargs, {'job_directory': 'my_directory'})
+        # job_directory from block should be used
+        self.assertIn('my_directory', platform.job_directory)
+        mock_user_logger.log.call_args_list[0].assert_called_with('\n[My_container]')
+        mock_user_logger.log.call_args_list[1].assert_called_with('"job_directory": "my_directory"')
+
+    @patch("idmtools.core.platform_factory.user_logger")
+    def test_create_platform_with_valid_block_other_kwargs(self, mock_user_logger):
+        block = 'My_container'
+        platform = Platform(block, docker_image="my_image")
+        self.assertEqual(platform._config_block, block)
+        self.assertEqual(platform.__class__.__name__, 'ContainerPlatform')
+        self.assertEqual(platform._kwargs, {'docker_image': 'my_image'})
+        # job_directory from block should be used
+        self.assertIn('MY_JOB_DIRECTORY', platform.job_directory)
+        mock_user_logger.log.call_args_list[0].assert_called_with('\n[My_container]')
+        mock_user_logger.log.call_args_list[1].assert_called_with('"job_directory": "MY_JOB_DIRECTORY"')
+
+    @patch("idmtools.core.platform_factory.user_logger")
+    def test_create_platform_with_alias(self, mock_user_logger):
+        kwargs = {'job_directory': 'destination_directory'}
+        platform = Platform('Container', **kwargs)
+        self.assertEqual(platform.__class__.__name__, 'ContainerPlatform')
+        self.assertEqual(platform._config_block, 'Container')
+        self.assertEqual(platform._kwargs, kwargs)
+        self.assertIn('destination_directory', platform.job_directory)
+        mock_user_logger.log.call_args_list[0].assert_called_with('\n[Container]')
+        mock_user_logger.log.call_args_list[1].assert_called_with('"job_directory": "destination_directory"')
+
+    def test_create_platform_with_invalid_block(self):
+        kwargs = {'job_directory': 'destination_directory'}
+        with self.assertRaises(Exception) as context:
+            platform = Platform("Container1", **kwargs)
+        self.assertTrue('Container1 is an unknown Platform Type. Supported platforms are ' in str(context.exception))
+
+    @patch('idmtools.core.platform_factory.user_logger')
+    @patch('idmtools.core.platform_factory.logger')
+    def test_create_platform_with_type(self, mock_logger, mock_user_logger):
+        with self.subTest("with_alias_block_and_non_used_type"):
+            kwargs = {'job_directory': 'destination_directory', 'type': 'Container1'}
+            platform = Platform("Container", **kwargs)
+            self.assertEqual(platform.__class__.__name__, 'ContainerPlatform')
+            self.assertEqual(platform._config_block, 'Container')
+            self.assertEqual(platform._kwargs, kwargs)
+            self.assertIn('destination_directory', platform.job_directory)
+            mock_logger.warning.call_args_list[0].assert_called_with(f"call('\n/!\\ WARNING: The following User Inputs are not used:")
+            mock_logger.warning.call_args_list[1].assert_called_with("- type = Container1")
+            mock_user_logger.log.call_args_list[0].assert_called_with('\n[Container]')
+            mock_user_logger.log.call_args_list[1].assert_called_with('"job_directory": "destination_directory"')
+        mock_logger.reset_mock()
+        mock_user_logger.reset_mock()
+        with self.subTest("with_random_block_and_valid_type"):
+            kwargs = {'job_directory': 'destination_directory', 'type': 'Container'}
+            platform = Platform("Container", **kwargs)
+            self.assertEqual(platform.__class__.__name__, 'ContainerPlatform')
+            self.assertEqual(platform._config_block, 'Container')
+            self.assertEqual(platform._kwargs, kwargs)
+            self.assertIn('destination_directory', platform.job_directory)
+            mock_logger.warning.assert_not_called()
+            mock_user_logger.log.call_args_list[0].assert_called_with('\n[Container]')
+            mock_user_logger.log.call_args_list[1].assert_called_with('"job_directory": "destination_directory"')
+        mock_logger.reset_mock()
+        mock_user_logger.reset_mock()
+        with self.subTest("with_no_block_and_invalid_type"):
+            with self.assertRaises(Exception) as context:
+                platform = Platform(type="Container1", job_directory="destination_directory")
+            self.assertTrue(
+                'Container1 is an unknown Platform Type. Supported platforms are ' in str(context.exception))
+        with self.subTest("with_no_block_and_valid_type"):
+            kwargs = {'job_directory': 'destination_directory', 'type': 'Container'}
+            platform = Platform(**kwargs)
+            self.assertEqual(platform.__class__.__name__, 'ContainerPlatform')
+            self.assertEqual(platform._config_block, None)
+            self.assertEqual(platform._kwargs, kwargs)
+            self.assertIn('destination_directory', platform.job_directory)
+            mock_logger.warning.assert_not_called()
+            mock_user_logger.log.assert_not_called()
+        mock_user_logger.reset_mock()
+        with patch("idmtools.config.idm_config_parser.user_logger") as mock_config_user_logger:
+            with self.subTest("with_ini_block_update_type_from_ini"):
+                block = 'My_container'
+                kwargs = {'type': 'Container1'}
+                platform = Platform(block, **kwargs)
+                self.assertEqual(platform._config_block, block)
+                self.assertEqual(platform.__class__.__name__, 'ContainerPlatform')
+                self.assertEqual(platform._kwargs, kwargs)
+                self.assertIn('MY_JOB_DIRECTORY', platform.job_directory)
+                mock_logger.warning.call_args_list[0].assert_called_with(f"call('\n/!\\ WARNING: The following User Inputs are not used:")
+                mock_logger.warning.call_args_list[1].assert_called_with("- type = Container1")
+                mock_config_user_logger.log.call_args_list[0].assert_called_with('\n[My_container]')
+                mock_config_user_logger.log.call_args_list[1].assert_called_with('"job_directory": "MY_JOB_DIRECTORY"')
+                mock_user_logger.log.assert_not_called()
+        mock_logger.reset_mock()
+        with self.subTest("with_non_exist_block_and_valid_type"):
+            kwargs = {'job_directory': 'destination_directory', 'type': 'Container'}
+            platform = Platform('block', **kwargs)
+            self.assertEqual(platform.__class__.__name__, 'ContainerPlatform')
+            self.assertEqual(platform._config_block, 'block')
+            self.assertEqual(platform._kwargs, kwargs)
+            self.assertIn('destination_directory', platform.job_directory)
+            mock_logger.warning.call_args_list[0].assert_called_with(
+                f"call('\n/!\\ WARNING: the following Config Settings are not used when creating Platform:")
+            mock_logger.warning.call_args_list[1].assert_called_with("- block = block")
+            mock_user_logger.log.call_args_list[0].assert_called_with('\n[block]')
+            mock_user_logger.log.call_args_list[1].assert_called_with('"job_directory": "destination_directory"')
+        mock_user_logger.reset_mock()
+        with self.subTest("with_non_exist_block_and_valid_type"):
+            kwargs = {'job_directory': 'destination_directory', 'any': 'something'}
+            with self.assertRaises(ValueError) as context:
+                platform = Platform('block', **kwargs)
+            self.assertIn('You are specifying a platform without a configuration file or configuration block',
+                          mock_user_logger.method_calls[0].args[0])
+            self.assertTrue(
+                "block is an unknown Platform Type. Supported platforms are " in str(context.exception.args[0]))
