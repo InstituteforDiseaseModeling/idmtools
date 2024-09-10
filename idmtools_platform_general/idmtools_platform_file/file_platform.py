c6ad9c6224243fc67d06686e0ab4718a860edd85
ZDu-IDM <zdu@idmod.org>
2024-09-10 15:13:57 -0700

    fix import

--- idmtools_platform_general/idmtools_platform_file/file_platform.py
+++ idmtools_platform_general/idmtools_platform_file/file_platform.py
@@ -18,7 +18,7 @@ from idmtools.entities.experiment import Experiment
 from idmtools.entities.simulation import Simulation
 from idmtools.entities.iplatform import IPlatform, ITEM_TYPE_TO_OBJECT_INTERFACE
 from idmtools.utils.decorators import check_symlink_capabilities
-from idmtools_platform_file.platform_operations.utils import FILE_MAPS, clean_experiment_name, validate_file_path_length
+from idmtools_platform_file.platform_operations.utils import FILE_MAPS, clean_experiment_name
 from idmtools_platform_file.assets import generate_script, generate_simulation_script
 from idmtools_platform_file.platform_operations.asset_collection_operations import FilePlatformAssetCollectionOperations
 from idmtools_platform_file.platform_operations.experiment_operations import FilePlatformExperimentOperations
