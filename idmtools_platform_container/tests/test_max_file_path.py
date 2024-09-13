693c45e7dd678db92c7c2e194e12b2904c2d383f
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-09-13 14:18:33 -0700

    Fix failed test in linux

--- idmtools_platform_container/tests/test_max_file_path.py
+++ idmtools_platform_container/tests/test_max_file_path.py
@@ -9,7 +9,7 @@ from idmtools.entities.experiment import Experiment
 @pytest.mark.serial
 class TestPlatformExperiment(unittest.TestCase):
     @patch('idmtools_platform_file.platform_operations.utils.user_logger')
-    def test_max_file_path(self, mock_user_logger):
+    def test_max_file_path_too_long(self, mock_user_logger):
         platform = Platform("Container", job_directory="dest")
 
         # Define task
@@ -17,8 +17,27 @@ class TestPlatformExperiment(unittest.TestCase):
         task = CommandTask(command=command)
         task.transient_assets.add_asset("inputs/Assets/MyLib/functions.py")
         # Run an experiment
-        experiment = Experiment.from_task(task, name='a' * 240)
-        with self.assertRaises(SystemExit):
+        experiment = Experiment.from_task(task, name='a' * 210)
+        import platform as p
+        if p.system() in ["Windows"]:
+            with self.assertRaises(SystemExit):
+                experiment.run(wait_until_done=False)  # Only in Windows file path is too long, raise an exception
+                mock_user_logger.warning.call_args_list[0].assert_called_with("File path length too long")
+                mock_user_logger.warning.call_args_list[1].assert_called_with("You may want to adjust your job_directory location")
+        else:
             experiment.run(wait_until_done=False)
-        mock_user_logger.warning.call_args_list[0].assert_called_with("File path length too long")
-        mock_user_logger.warning.call_args_list[1].assert_called_with("You may want to adjust your job_directory location")
+            mock_user_logger.warning.assert_not_called()
+
+    @patch('idmtools_platform_file.platform_operations.utils.user_logger')
+    def test_max_file_path_not_too_long(self, mock_user_logger):
+        platform = Platform("Container", job_directory="dest")
+
+        # Define task
+        command = "echo 'Hello, World!'"
+        task = CommandTask(command=command)
+        task.transient_assets.add_asset("inputs/Assets/MyLib/functions.py")
+        # Run an experiment
+        experiment = Experiment.from_task(task, name='a' * 24)
+        experiment.run(wait_until_done=False)
+        mock_user_logger.warning.assert_not_called()
+
