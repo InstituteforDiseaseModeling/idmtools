9664723a30eb0b25b6efa594c652cb789fb5946c
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-10-15 15:32:40 -0700

    Bump version: 2.0.0 → 2.0.1

--- idmtools_platform_comps/setup.py
+++ idmtools_platform_comps/setup.py
@@ -58,5 +58,5 @@ comps-cli=idmtools_platform_comps.cli.comps:comps
 """,
     extras_require=extras,
     url='https://github.com/InstituteforDiseaseModeling/idmtools',
-    version='2.0.0'
+    version='2.0.1'
 )
