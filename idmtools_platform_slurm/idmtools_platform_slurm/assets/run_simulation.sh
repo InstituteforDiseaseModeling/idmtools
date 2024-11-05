562abab553d8c0aee5885af3971ee1279cc7747c
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-11-05 08:42:07 -0800

    Update echo messages

--- idmtools_platform_slurm/idmtools_platform_slurm/assets/run_simulation.sh
+++ idmtools_platform_slurm/idmtools_platform_slurm/assets/run_simulation.sh
@@ -10,13 +10,13 @@ echo "The script is running from: $current_dir"
 
 # Run the simulation based on whether MPI is required
 if [ "$mpi_type" == "no-mpi" ]; then
-    echo "run without mpi"
+    echo "Run without MPI"
     srun _run.sh 1> stdout.txt 2> stderr.txt
 elif [ "$mpi_type" == "mpirun" ]; then
-    echo "run mpirun"
+    echo "Run mpirun"
     mpirun "$current_dir"/_run.sh 1> stdout.txt 2> stderr.txt
 elif [ "$mpi_type" == "pmi2" ] || [ "$mpi_type" == "pmix" ]; then # pmi2 or pmix
-    echo "run mpi with $mpi_type"
+    echo "Run MPI with $mpi_type"
     srun --mpi=$mpi_type _run.sh 1> stdout.txt 2> stderr.txt
 else
     echo "Invalid MPI type: $mpi_type"
