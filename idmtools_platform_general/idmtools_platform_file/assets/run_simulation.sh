b190568e2a21d2ff283615f5e7e07546e0fa9c77
ZDu-IDM <zdu@idmod.org>
2024-11-15 11:00:35 -0800

    rollback symlink changes and will do symlink in _run.sh

--- idmtools_platform_general/idmtools_platform_file/assets/run_simulation.sh
+++ idmtools_platform_general/idmtools_platform_file/assets/run_simulation.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 JOB_DIRECTORY=$1
-
-# Create the symbolic link
-ln -s "$(dirname "${BASH_SOURCE[0]}")/Assets" "${JOB_DIRECTORY}/Assets"
-
 echo "enter directory: '$JOB_DIRECTORY'"
 cd $JOB_DIRECTORY
 sed -i 's/\r//g' _run.sh
