37bdf53dddbf334217049699424c599f7fca7a13
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2025-03-19 12:38:12 -0700

    Fix few typos and change validation from comps experiment/simulations

--- idmtools_platform_comps/tests/test_simulation_builder.py
+++ idmtools_platform_comps/tests/test_simulation_builder.py
@@ -2,7 +2,10 @@ import allure
 import os
 from functools import partial
 import pytest
+from COMPS.Data import QueryCriteria
+
 from idmtools.builders import SimulationBuilder, SweepArm, ArmType, ArmSimulationBuilder
+from idmtools.core import ItemType
 from idmtools.core.platform_factory import Platform
 from idmtools.entities.experiment import Experiment
 from idmtools.entities.templated_simulation import TemplatedSimulations
@@ -45,20 +48,20 @@ class TestSimulationBuilder(ITestWithPersistence):
                 sim2: {a:1, b:2, c:5}
                 sim3: {a:1, b:3, c:4}
                 sim4: {a:1, b:3, c:5}
-                sim5: {a:1, b:2, c:4, a:6, b:2}
-                sim6: {a:1, b:2, c:4, a:7, b:2}
-                sim7: {a:1, b:2, c:5, a:6, b:2}
-                sim8: {a:1, b:2, c:5, a:7, b:2}
-                sim9: {a:1, b:3, c:4, a:6, b:2}
-                sim10: {a:1, b:3, c:4, a:7, b:2}
-                sim11: {a:1, b:3, c:5, a:6, b:2}
-                sim12: {a:1, b:3, c:5, a:7, b:2}
+                sim5: {a:1, b:2, c:4, d:6, e:2}
+                sim6: {a:1, b:2, c:4, d:7, e:2}
+                sim7: {a:1, b:2, c:5, d:6, e:2}
+                sim8: {a:1, b:2, c:5, d:7, e:2}
+                sim9: {a:1, b:3, c:4, d:6, be:2}
+                sim10: {a:1, b:3, c:4, d:7, e:2}
+                sim11: {a:1, b:3, c:5, d:6, e:2}
+                sim12: {a:1, b:3, c:5, d:7, e:2}
         """
         base_task = JSONConfiguredPythonTask(script_path=os.path.join(COMMON_INPUT_PATH, "python", "model1.py"))
         # define that we are going to create multiple simulations from this task
         ts = TemplatedSimulations(base_task=base_task)
 
-        # define our first sweep Sweep Arm
+        # define our first SweepArm
         arm = SweepArm(type=ArmType.cross)
         builder = ArmSimulationBuilder()
         arm.add_sweep_definition(setA, 1)
@@ -77,11 +80,13 @@ class TestSimulationBuilder(ITestWithPersistence):
         experiment = Experiment.from_template(ts, name=self.case_name)
         experiment.run(wait_until_done=True)
         self.assertTrue(experiment.succeeded)
-        self.assertEqual(experiment.simulations.__len__(), 12)
+        comps_exp = self.platform.get_item(experiment.id, item_type=ItemType.EXPERIMENT, raw=True)
+        comps_simulations = comps_exp.get_simulations(query_criteria=QueryCriteria().select_children("tags"))
+        self.assertEqual(comps_simulations.__len__(), 12)
         exclude_key = "task_type"
         real_tags = [
-            {key: value for key, value in simulation.tags.items() if key != exclude_key}
-            for simulation in experiment.simulations
+            {key: int(value) for key, value in simulation.tags.items() if key != exclude_key}
+            for simulation in comps_simulations
         ]
         expected_tags = [
             {'a': 1, 'b': 2, 'c': 4},
@@ -117,8 +122,8 @@ class TestSimulationBuilder(ITestWithPersistence):
                 sim2: {a:1, b:2, c:5}
                 sim3: {a:1, b:3, c:4}
                 sim4: {a:1, b:3, c:5}
-                sim5: {a:6, b:2}
-                sim6: {a:7, b:2}
+                sim5: {d:6, e:2}
+                sim6: {d:7, e:2}
             Note:
                 arm1 and arm2 are adding to total simulations
         """
@@ -126,7 +131,7 @@ class TestSimulationBuilder(ITestWithPersistence):
         # define that we are going to create multiple simulations from this task
         ts = TemplatedSimulations(base_task=base_task)
 
-        # define our first sweep Sweep Arm
+        # define our first SweepArm
         arm = SweepArm(type=ArmType.cross)
         builder = ArmSimulationBuilder()
         arm.add_sweep_definition(setA, 1)
@@ -134,7 +139,7 @@ class TestSimulationBuilder(ITestWithPersistence):
         arm.add_sweep_definition(setC, [4, 5])
         builder.add_arm(arm)
 
-        # define our second sweep Sweep Arm
+        # define our second SweepArm
         arm2 = SweepArm(type=ArmType.cross)
         arm2.add_sweep_definition(setD, [6, 7])
         arm2.add_sweep_definition(setE, [2])
@@ -158,7 +163,7 @@ class TestSimulationBuilder(ITestWithPersistence):
         # define that we are going to create multiple simulations from this task
         ts = TemplatedSimulations(base_task=base_task)
 
-        # define our first sweep Sweep Arm
+        # define our first SweepArm
         arm1 = SweepArm(type=ArmType.cross)
         arm2 = SweepArm(type=ArmType.cross)
 
@@ -177,7 +182,7 @@ class TestSimulationBuilder(ITestWithPersistence):
 
         ts.add_builder(builder)
         experiment = Experiment.from_template(ts, name=self.case_name)
-        # run the experiment
+        # run experiment
         experiment.run(wait_until_done=True)
         self.assertTrue(experiment.succeeded)
         self.assertEqual(experiment.simulations.__len__(), 12)
@@ -186,11 +191,11 @@ class TestSimulationBuilder(ITestWithPersistence):
         """
             This file demonstrates how to use SimulationBuilder.
 
-            |__simulationbuilder1
+            |__SimulationBuilder1
                 |_ a = 1
                 |_ b = [2,3]
                 |_ c = [4,5]
-             --simulationbuilder2
+             --SimulationBuilder2
                 |_ d = [6,7]
                 |_ e = 2
             Expect sims with parameters:
@@ -198,8 +203,8 @@ class TestSimulationBuilder(ITestWithPersistence):
                 sim2: {a:1, b:2, c:5}
                 sim3: {a:1, b:3, c:4}
                 sim4: {a:1, b:3, c:5}
-                sim5: {a:6, b:2}
-                sim6: {a:7, b:2}
+                sim5: {d:6, e:2}
+                sim6: {d:7, e:2}
             Note:
                 builder1 and builder2 are adding to total simulations
         """
@@ -207,7 +212,7 @@ class TestSimulationBuilder(ITestWithPersistence):
         # define that we are going to create multiple simulations from this task
         ts = TemplatedSimulations(base_task=base_task)
 
-        # define our first Simulationbuilder
+        # define our first SimulationBuilder
         builder1 = SimulationBuilder()
         builder1.add_sweep_definition(setA, 1)
         builder1.add_sweep_definition(setB, [2, 3])
@@ -222,11 +227,13 @@ class TestSimulationBuilder(ITestWithPersistence):
         experiment = Experiment.from_template(ts, name=self.case_name)
         experiment.run(wait_until_done=True)
         self.assertTrue(experiment.succeeded)
-        self.assertEqual(experiment.simulations.__len__(), 6)
+        comps_exp = self.platform.get_item(experiment.id, item_type=ItemType.EXPERIMENT, raw=True)
+        comps_simulations = comps_exp.get_simulations(query_criteria=QueryCriteria().select_children("tags"))
+        self.assertEqual(comps_simulations.__len__(), 6)
         exclude_key = "task_type"
         real_tags = [
-            {key: value for key, value in simulation.tags.items() if key != exclude_key}
-            for simulation in experiment.simulations
+            {key: int(value) for key, value in simulation.tags.items() if key != exclude_key}
+            for simulation in comps_simulations
         ]
         expected_tags = [
             {'a': 1, 'b': 2, 'c': 4},
