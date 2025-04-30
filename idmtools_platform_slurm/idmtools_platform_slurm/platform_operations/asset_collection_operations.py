e819508a9dbca7c3caed8831e6958f447ec3e13f
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2025-04-30 15:31:42 -0700

    Simplify asset_collection_operations.py methods with inherit from FilePlatform

--- idmtools_platform_slurm/idmtools_platform_slurm/platform_operations/asset_collection_operations.py
+++ idmtools_platform_slurm/idmtools_platform_slurm/platform_operations/asset_collection_operations.py
@@ -3,13 +3,11 @@ Here we implement the SlurmPlatform asset collection operations.
 
 Copyright 2025, Gates Foundation. All rights reserved.
 """
-from dataclasses import field, dataclass
+from dataclasses import dataclass
 from logging import getLogger
-from typing import TYPE_CHECKING, Type, List, Dict, Union
-from idmtools.core import ItemType
-from idmtools.entities.simulation import Simulation
+from typing import TYPE_CHECKING
 from idmtools_platform_file.platform_operations.asset_collection_operations import FilePlatformAssetCollectionOperations
-from idmtools_platform_file.platform_operations.utils import FileSimulation
+
 
 if TYPE_CHECKING:
     from idmtools_platform_slurm.slurm_platform import SlurmPlatform
@@ -26,22 +24,3 @@ class SlurmPlatformAssetCollectionOperations(FilePlatformAssetCollectionOperatio
     Provides AssetCollection Operations to SlurmPlatform.
     """
     platform: 'SlurmPlatform'  # noqa F821
-    platform_type: Type = field(default=None)
-
-    def get_assets(self, simulation: Union[Simulation, FileSimulation], files: List[str], **kwargs) -> Dict[
-        str, bytearray]:
-        """
-        Get assets for simulation.
-        Args:
-            simulation: Simulation or SlurmSimulation
-            files: files to be retrieved
-            kwargs: keyword arguments used to expand functionality.
-        Returns:
-            Dict[str, bytearray]
-        """
-        if isinstance(simulation, (Simulation, FileSimulation)):
-            sim_dir = self.platform.get_directory_by_id(simulation.id, ItemType.SIMULATION)
-            return self._get_assets_from_dir(sim_dir, files)
-        else:
-            raise NotImplementedError(
-                f"get_assets() for items of type {type(simulation)} is not supported on SlurmPlatform.")
