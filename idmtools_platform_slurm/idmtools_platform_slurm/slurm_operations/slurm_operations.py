0d8d909e5a2fe78ec576226754ebd1e45141ed37
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2025-04-30 15:35:10 -0700

    removed submit_job to slurm_platform.py and remove duplicate methods for link_file and link_dir with file platform

--- idmtools_platform_slurm/idmtools_platform_slurm/slurm_operations/slurm_operations.py
+++ idmtools_platform_slurm/idmtools_platform_slurm/slurm_operations/slurm_operations.py
@@ -6,12 +6,10 @@ Copyright 2025, Gates Foundation. All rights reserved.
 import subprocess
 from dataclasses import dataclass, field
 from logging import getLogger
-from pathlib import Path
 from typing import Union, List, Any, Type
 
 from idmtools.entities.experiment import Experiment
 from idmtools.entities.simulation import Simulation
-from idmtools.utils.decorators import check_symlink_capabilities
 from idmtools_platform_file.file_operations.file_operations import FileOperations
 from idmtools_platform_slurm.assets import generate_batch, generate_script, generate_simulation_script
 
@@ -42,51 +40,6 @@ class SlurmOperations(FileOperations):
             generate_simulation_script(self.platform, item, retries)
         else:
             raise NotImplementedError(f"{item.__class__.__name__} is not supported for batch creation.")
-
-    @check_symlink_capabilities
-    def link_file(self, target: Union[Path, str], link: Union[Path, str]) -> None:
-        """
-        Link files.
-        Args:
-            target: the source file path
-            link: the file path
-        Returns:
-            None
-        """
-        target = Path(target).absolute()
-        link = Path(link).absolute()
-        link.symlink_to(target)
-
-    @check_symlink_capabilities
-    def link_dir(self, target: Union[Path, str], link: Union[Path, str]) -> None:
-        """
-        Link directory/files.
-        Args:
-            target: the source folder path.
-            link: the folder path
-        Returns:
-            None
-        """
-        target = Path(target).absolute()
-        link = Path(link).absolute()
-        link.symlink_to(target)
-
-    def submit_job(self, item: Union[Experiment, Simulation], **kwargs) -> None:
-        """
-        Submit a Slurm job.
-        Args:
-            item: idmtools Experiment or Simulation
-            kwargs: keyword arguments used to expand functionality
-        Returns:
-            None
-        """
-        if isinstance(item, Experiment):
-            working_directory = self.get_directory(item)
-            subprocess.run(['bash', 'batch.sh'], stdout=subprocess.PIPE, cwd=str(working_directory))
-        elif isinstance(item, Simulation):
-            pass
-        else:
-            raise NotImplementedError(f"Submit job is not implemented on SlurmPlatform.")
 
     @staticmethod
     def cancel_job(job_ids: Union[str, List[str]]) -> Any:
