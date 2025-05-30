2b05b68ed48291484b9fc4611be324d26011352d
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2025-05-30 14:44:46 -0700

    Fix lint in analyze_manager.py

--- idmtools_core/idmtools/analysis/analyze_manager.py
+++ idmtools_core/idmtools/analysis/analyze_manager.py
@@ -13,16 +13,11 @@ from typing import NoReturn, List, Dict, Tuple, Optional, TYPE_CHECKING
 from tqdm import tqdm
 from idmtools import IdmConfigParser
 from idmtools.analysis.map_worker_entry import map_item
-from idmtools.assets import AssetCollection
 from idmtools.core import NoPlatformException
 from idmtools.core.enums import ItemType
 from idmtools.core.interfaces.ientity import IEntity
 from idmtools.core.logging import VERBOSE, SUCCESS
-from idmtools.entities import Suite
-from idmtools.entities.experiment import Experiment
 from idmtools.entities.ianalyzer import IAnalyzer
-from idmtools.entities.iworkflow_item import IWorkflowItem
-from idmtools.entities.simulation import Simulation
 from idmtools.utils.language import on_off, verbose_timedelta
 
 if TYPE_CHECKING:  # pragma: no cover
