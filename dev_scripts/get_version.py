6bc31c47089e104db64f9f2f95bd1a18a21a761a
Sharon Chen <shchen@idmod.org>
2025-07-01 15:23:41 -0700

    roll back version bump since it is not built yet

--- dev_scripts/get_version.py
+++ dev_scripts/get_version.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 """Utility script to update references to idmtools_core in requirements.txt files when versions change."""
 import os
-import glob
-import re
 import tomllib  # use 'tomli' if you're on Python <3.11
-import tomli_w  # write support; install with `pip install tomli-w`
 
 REPO_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 CORE_PATH = os.path.join(REPO_PATH, 'idmtools_core')
