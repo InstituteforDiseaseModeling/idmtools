a63098a8a87d9600faca1a902d7593ec3d1f8e30
Sharon Chen <shchen@idmod.org>
2025-07-14 12:43:30 -0700

    Fix bug in previous example for rerun_failed_simulation.py

--- examples/python_model/rerun_failed_simulation.py
+++ examples/python_model/rerun_failed_simulation.py
@@ -1,5 +1,6 @@
 """
-This is the example of how to get failed simulation and rerun them with an existing experiment
+This is the example of how to rerun failed simulations with an existing experiment.
+Note, we can only rerun failed simulations due to server random failure.
 """
 import copy
 
@@ -10,25 +11,26 @@ from idmtools.entities.command_task import CommandTask
 
 platform = Platform('Calculon')
 # Get experiment by id
-experiment = platform.get_item("90ba4f1e-c45d-f011-aa23-b88303911bc1", ItemType.EXPERIMENT)
+experiment = platform.get_item("fdc32be7-e960-f011-aa23-b88303911bc1", ItemType.EXPERIMENT)
 # Get all failed simulations
 filter_simulation_ids = experiment.get_simulations_by_tags(status=EntityStatus.FAILED)
 print(filter_simulation_ids)
 
-# rerun failed simulations, and deepcopy each simulation to avoid changing the original simulation
+# rerun failed simulations. Note we can only rerun failed simulations due to server random failure.
 failed_simulations = []
 for sim_id in filter_simulation_ids:
-    sim = platform.get_item(filter_simulation_ids[0], ItemType.SIMULATION)
-    # set status to None so that we can rerun
+    sim = platform.get_item(sim_id, ItemType.SIMULATION)
+    # set the status to None so that we can rerun
     sim.status = None
-    sim.tags.update({"rerun": "true"})
+    sim.tags.update({"rerun": sim.id})
+    sim.assets.platform = None
     # TODO: modify failed simulation here:
     # reset task
     # cli=sim.task.command.cmd
     # sim.task =CommandTask(CommandLine.from_string(cli))
-    failed_simulations.append(copy.deepcopy(sim))
+    failed_simulations.append(sim)
 # Add failed simulations to the experiment
-experiment.simulations.extend(failed_simulations)
+experiment.add_simulations(failed_simulations)
 # run experiment again
 experiment.run(wait_until_done=True)
 
