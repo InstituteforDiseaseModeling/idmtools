778b3ba08af447f22b7dd3ea3f650bb6b737662b
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2026-04-22 10:31:12 -0700

    Change node_group to idm_abcd

--- idmtools_platform_comps/tests/test_workorder.py
+++ idmtools_platform_comps/tests/test_workorder.py
@@ -37,7 +37,7 @@ class TestWorkOrder(ITestWithPersistence):
     def setUp(self) -> None:
         self.case_name = get_case_name(os.path.basename(__file__) + "--" + self._testMethodName)
         print(self.case_name)
-        self.platform = Platform('SlurmStage', node_group='idm_48cores')
+        self.platform = Platform('SlurmStage', node_group='idm_abcd')
 
     def test_workorder_pythontask(self):
         """
